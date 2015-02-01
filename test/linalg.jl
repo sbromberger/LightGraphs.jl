6651d39d99d22b05f11210a06827cf4066645166
Seth Bromberger <github@bromberger.com>
2015-01-31 23:38:13 -0800

    linalg test bugfix

--- test/linalg.jl
+++ test/linalg.jl
@@ -1,6 +1,6 @@
-@test adjacency_matrix(g3)[3,2]
-@test !adjacency_matrix(g3)[2,4]
-@test laplacian_matrix(g3)[3,2]
-@test !laplacian_matrix(g3)[1,3]
-@test laplacian_spectrum(g3)[5] == 2.7320508075688767
+@test adjacency_matrix(g3)[3,2] == 1
+@test adjacency_matrix(g3)[2,4] == 0
+@test laplacian_matrix(g3)[3,2] == -1
+@test laplacian_matrix(g3)[1,3] == 0
+@test laplacian_spectrum(g3)[5] == 3.6180339887498945
 @test adjacency_spectrum(g3)[1] == -1.732050807568878
