1d722bd8f829b362d38e8d4fc53903f577723c42
Seth Bromberger <github@bromberger.com>
2015-04-06 01:41:40 -0700

    fix bug in shortest path algorithms with use_dists; abstract that function into a (non-exported) has_distance() function in distance.jl
    forgot about dijkstra_with_pred; added tests

--- test/dijkstra.jl
+++ test/dijkstra.jl
@@ -1,11 +1,17 @@
-d = float([ 0 1 2 3 4; 5 0 6 7 8; 9 10 0 11 12; 13 14 15 0 16; 17 18 19 20 0])
-z = dijkstra_shortest_paths(g4, 2; edge_dists=d)
+d1 = float([ 0 1 2 3 4; 5 0 6 7 8; 9 10 0 11 12; 13 14 15 0 16; 17 18 19 20 0])
+d2 = sparse(float([ 0 1 2 3 4; 5 0 6 7 8; 9 10 0 11 12; 13 14 15 0 16; 17 18 19 20 0]))
 
-@test z.parents == [0, 0, 2, 3, 4]
-@test z.dists == [Inf, 0, 6, 17, 33]
+y = dijkstra_shortest_paths(g4, 2; edge_dists=d1)
+z = dijkstra_shortest_paths(g4, 2; edge_dists=d2)
 
-z = dijkstra_predecessor_and_distance(g4, 2; edge_dists=d)
-@test z.predecessors[3] == [2]
+@test y.parents == z.parents == [0, 0, 2, 3, 4]
+@test y.dists == z.dists == [Inf, 0, 6, 17, 33]
 
-@test enumerate_paths(z)[2] == []
-@test enumerate_paths(z)[4] == enumerate_paths(z,4) == [2,3,4]
+y = dijkstra_predecessor_and_distance(g4, 2; edge_dists=d1)
+z = dijkstra_predecessor_and_distance(g4, 2; edge_dists=d2)
+@test z.predecessors[3] == y.predecessors[3] == [2]
+
+@test enumerate_paths(z) == enumerate_paths(y)
+@test enumerate_paths(z)[4] ==
+    enumerate_paths(z,4) ==
+    enumerate_paths(y,4) == [2,3,4]
