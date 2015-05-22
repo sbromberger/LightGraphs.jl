73d06b179dfe8a7d4c25f4fda1f8d484c78bd81a
Remi Louf <remilouf@gmail.com>
2015-05-22 21:30:33 +0100

    Clean code, accumulation and rescaling made in-place functions

--- src/centrality/betweenness.jl
+++ src/centrality/betweenness.jl
@@ -61,27 +61,25 @@ function betweenness_centrality(
     end
     for si in nodes
         s = vertices(g)[si]
-        # if length(weights) == 0
-        #     state = dijkstra_predecessor_and_distance(g, s)
-        # else
-            state = dijkstra_predecessor_and_distance(g, s)
-        # end
-        if endpoints            # 120
-            betweenness = _accumulate_endpoints(betweenness, state, g, si)
+        state = dijkstra_predecessor_and_distance(g, s)
+        if endpoints
+            _accumulate_endpoints!(betweenness, state, g, si)
         else
-            betweenness = _accumulate_basic(betweenness, state, g, si)
+            _accumulate_basic!(betweenness, state, g, si)
         end
     end
-    betweenness = _rescale(betweenness, n_v,
-                           normalize,
-                           is_directed,
-                           k)
+
+    _rescale!(betweenness, 
+              n_v,
+              normalize,
+              is_directed,
+              k)
 
     return betweenness
 end
 
 
-function _accumulate_basic(
+function _accumulate_basic!(
     betweenness::Vector{Float64},
     state::DijkstraStateWithPred,
     g::AbstractGraph,
@@ -99,22 +97,18 @@ function _accumulate_basic(
     S = sortperm(state.dists, rev=true)
     for w in S
         coeff = (1.0 + δ[w]) / σ[w]
-        # println("coeff of $w = $coeff, δ[w] = $(δ[w])")
-        # println("*** P[w] = P[$w] = $(P[w])")
         for v in P[w]
             if v > 0
                 δ[v] += (σ[v] * coeff)
-                # println("setting δ[$vi] to $(δ[vi]), δ = $δ")
             end
         end
         if w != si
             betweenness[w] += δ[w]
         end
     end
-    return betweenness
 end
 
-function _accumulate_endpoints(
+function _accumulate_endpoints!(
     betweenness::Vector{Float64},
     state::DijkstraStateWithPred,
     g::AbstractGraph,
@@ -127,8 +121,6 @@ function _accumulate_endpoints(
     P = state.predecessors
     v1 = [1:nv;]
     v2 = state.dists
-    # v1 = [1:nv][state.hasparent] # the state.hasparent will fix P[si] = [] when it's merged
-    # v2 = state.dists[state.hasparent]
     S = Int[x[2] for x in sort(collect(zip(v2,v1)), rev=true)]
     s = g.vertices[si]
     betweenness[s] += length(S) - 1    # 289
@@ -142,10 +134,9 @@ function _accumulate_endpoints(
             betweenness[w] += (δ[w] + 1)
         end
     end
-    return betweenness
 end
 
-function _rescale(betweenness::Vector{Float64}, n::Int, normalize::Bool, directed::Bool, k::Int)
+function _rescale!(betweenness::Vector{Float64}, n::Int, normalize::Bool, directed::Bool, k::Int)
     if normalize
         if n <= 2
             do_scale = false
@@ -158,16 +149,15 @@ function _rescale(betweenness::Vector{Float64}, n::Int, normalize::Bool, directe
             do_scale = true
             scale = 1.0 / 2.0
         else
-            do_scale = false    # 328
+            do_scale = false
         end
     end
     if do_scale
         if k > 0
-            scale = scale * n / k   #331
+            scale = scale * n / k 
         end
         for v = 1:length(betweenness)
             betweenness[v] *= scale
         end
     end
-    return betweenness      # 334
 end
