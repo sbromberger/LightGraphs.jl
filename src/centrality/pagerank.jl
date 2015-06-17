ea2ff66518cec9f17af7d1dbabfe0dd1cfb757b5
Seth Bromberger <github@bromberger.com>
2015-06-17 16:13:38 -0500

    added missed pagerank files

---
+++ src/centrality/pagerank.jl
@@ -0,0 +1,25 @@
+# Parts of this code were taken / derived from NetworkX. See LICENSE for
+# licensing details.
+
+function pagerank(g::DiGraph, α=0.85, n=100, ϵ = 1.0e-6)
+    M = adjacency_matrix(g)
+    S = vec(sum(M,1))
+    S = 1./S
+    S[find(S .== Inf)]=0.0
+    M = scale(S, M')
+    N = nv(g)
+    x = repmat([1.0/N], N)
+    p = repmat([1.0/N], N)
+    dangling_weights = p
+    is_dangling = find(S .== 0)
+
+    for _ in 1:n
+        xlast = x
+        x = α * (x * M + sum(x[is_dangling]) * dangling_weights) + (1 - α) * p
+        err = sum(abs(x - xlast))
+        if (err < N * ϵ)
+            return x
+        end
+    end
+    error("Pagerank did not converge after $n iterations.")
+end
