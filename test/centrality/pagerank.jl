0baf5331bcec1601ceac67199a5d7bb1d5da2189
Seth Bromberger <github@bromberger.com>
2015-06-30 10:51:27 -0500

    fixed pagerank test

--- test/centrality/pagerank.jl
+++ test/centrality/pagerank.jl
@@ -1,3 +1,3 @@
-@test round(pagerank(g5)[3],3) == 0.318
+@test_approx_eq_eps(pagerank(g5)[3], 0.318, 0.001)
 @test_throws ErrorException pagerank(g5, 2)
 @test_throws ErrorException pagerank(g5, 0.85, 2)
