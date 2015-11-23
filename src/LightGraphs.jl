d7ab90e5476315b8a62317f8f781379dcf968803
afternone <afternone@163.com>
2015-11-24 01:10:19 +0800

    compute nmi more efficient

--- src/LightGraphs.jl
+++ src/LightGraphs.jl
@@ -110,7 +110,7 @@ erdos_renyi, watts_strogatz, random_regular_graph, random_regular_digraph, rando
 stochastic_block_model,
 
 #community
-modularity, mutual_information, variation_of_information, community_detection_nback, core_periphery_deg,
+modularity, nmi, nvoi, nminvoi, community_detection_nback, core_periphery_deg,
 local_clustering,local_clustering_coefficient, global_clustering_coefficient,
 label_propagation
 
@@ -166,7 +166,7 @@ include("core.jl")
         include("community/core-periphery.jl")
         include("community/clustering.jl")
 		include("community/label_propagation.jl")
-		include("community/mutual_information.jl")
+		include("community/nmi.jl")
         include("flow/maximum_flow.jl")
             include("flow/edmonds_karp.jl")
             include("flow/dinic.jl")
---
+++ src/community/nmi.jl
@@ -0,0 +1,188 @@
+"normalised mutual information"
+function nmi(pa::Vector{Int}, pb::Vector{Int})
+    length(pa) == length(pb) || error("Two partitions have different number of nodes !")
+    n = length(pa)
+    qa = maximum(pa)
+    qb = maximum(pb)
+    if qa==1 && qb==1
+        return 0.0
+    end
+    ga = zeros(Int, qa)
+    gb = zeros(Int, qb)
+    A = Array(Vector{Int}, qa)
+    B = Array(Vector{Int}, qa)
+    for i=1:qa
+        A[i] = Int[]
+        B[i] = Int[]
+    end
+    for i=1:n
+        q = pa[i]
+        t = pb[i]
+        ga[q] += 1
+        gb[t] += 1
+        idx = 0
+        for j=1:length(A[q])
+            if A[q][j] == t
+                idx = j
+                break
+            end
+        end
+        if idx == 0
+            push!(A[q], t)
+            push!(B[q], 1)
+        else
+            B[q][idx] += 1
+        end
+    end
+    Ha = 0.0
+    for q=1:qa
+        if ga[q] == 0
+            continue
+        end
+        prob = ga[q]/n
+        Ha += prob*log(prob)
+    end
+    Hb = 0.0
+    for q=1:qb
+        if gb[q] == 0
+            continue
+        end
+        prob = gb[q]/n
+        Hb += prob*log(prob)
+    end
+    Iab = 0.0
+    for q=1:qa
+        for idx=1:length(A[q])
+            prob = B[q][idx]/n
+            t = A[q][idx]
+            Iab += prob*log(prob/(ga[q]/n*gb[t]/n))
+        end
+    end
+    -2.0*Iab/(Ha+Hb)
+end
+
+"normalised variation of information"
+function nvoi(pa::Vector{Int}, pb::Vector{Int})
+    length(pa) == length(pb) || error("Two partitions have different number of nodes !")
+    n = length(pa)
+    qa = maximum(pa)
+    qb = maximum(pb)
+    if qa==1 && qb==1
+        return 0.0
+    end
+    ga = zeros(Int, qa)
+    gb = zeros(Int, qb)
+    A = Array(Vector{Int}, qa)
+    B = Array(Vector{Int}, qa)
+    for i=1:qa
+        A[i] = Int[]
+        B[i] = Int[]
+    end
+    for i=1:n
+        q = pa[i]
+        t = pb[i]
+        ga[q] += 1
+        gb[t] += 1
+        idx = 0
+        for j=1:length(A[q])
+            if A[q][j] == t
+                idx = j
+                break
+            end
+        end
+        if idx == 0
+            push!(A[q], t)
+            push!(B[q], 1)
+        else
+            B[q][idx] += 1
+        end
+    end
+    Ha = 0.0
+    for q=1:qa
+        if ga[q] == 0
+            continue
+        end
+        prob = ga[q]/n
+        Ha += prob*log(prob)
+    end
+    Hb = 0.0
+    for q=1:qb
+        if gb[q] == 0
+            continue
+        end
+        prob = gb[q]/n
+        Hb += prob*log(prob)
+    end
+    Iab = 0.0
+    for q=1:qa
+        for idx=1:length(A[q])
+            prob = B[q][idx]/n
+            t = A[q][idx]
+            Iab += prob*log(prob/(ga[q]/n*gb[t]/n))
+        end
+    end
+	-(Ha + Hb - 2Iab)/log(length(pa))
+end
+
+"normalised mutual information and normalised variation of information"
+function nminvoi(pa::Vector{Int}, pb::Vector{Int})
+    length(pa) == length(pb) || error("Two partitions have different number of nodes !")
+    n = length(pa)
+    qa = maximum(pa)
+    qb = maximum(pb)
+    if qa==1 && qb==1
+        return 0.0
+    end
+    ga = zeros(Int, qa)
+    gb = zeros(Int, qb)
+    A = Array(Vector{Int}, qa)
+    B = Array(Vector{Int}, qa)
+    for i=1:qa
+        A[i] = Int[]
+        B[i] = Int[]
+    end
+    for i=1:n
+        q = pa[i]
+        t = pb[i]
+        ga[q] += 1
+        gb[t] += 1
+        idx = 0
+        for j=1:length(A[q])
+            if A[q][j] == t
+                idx = j
+                break
+            end
+        end
+        if idx == 0
+            push!(A[q], t)
+            push!(B[q], 1)
+        else
+            B[q][idx] += 1
+        end
+    end
+    Ha = 0.0
+    for q=1:qa
+        if ga[q] == 0
+            continue
+        end
+        prob = ga[q]/n
+        Ha += prob*log(prob)
+    end
+    Hb = 0.0
+    for q=1:qb
+        if gb[q] == 0
+            continue
+        end
+        prob = gb[q]/n
+        Hb += prob*log(prob)
+    end
+    Iab = 0.0
+    for q=1:qa
+        for idx=1:length(A[q])
+            prob = B[q][idx]/n
+            t = A[q][idx]
+            Iab += prob*log(prob/(ga[q]/n*gb[t]/n))
+        end
+    end
+	-2.0*Iab/(Ha+Hb), -(Ha + Hb - 2Iab)/log(length(pa))
+end
