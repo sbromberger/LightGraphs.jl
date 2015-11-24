d1e533373e399b849cc91ede803c94fd6454104a
afternone <afternone@163.com>
2015-11-24 17:05:46 +0800

    update label_propagation.jl

--- src/community/label_propagation.jl
+++ src/community/label_propagation.jl
@@ -29,35 +29,33 @@ function label_count(g::SimpleGraph, membership::Vector{Int}, u::Int)
     label_cnt = Dict{Int, Int}()
     for v in out_neighbors(g,u)
         v_lbl = membership[v]
-        if haskey(label_cnt, v_lbl)
-            label_cnt[v_lbl] += 1
-        else
-            label_cnt[v_lbl] = 1
-        end
+        label_cnt[v_lbl] = get(label_cnt, v_lbl, 0) + 1
     end
     label_cnt
 end
 
 function update_label!(g::SimpleGraph, membership::Vector{Int}, u::Int)
-    old_lbl = membership[u]
-    label_cnt = label_count(g, membership, u)
-    neigh_lbls = collect(keys(label_cnt))
-    neigh_lbls_cnt = collect(values(label_cnt))
-    order = collect(1:length(label_cnt))
-    shuffle!(order)
-    max_cnt = neigh_lbls_cnt[order[1]]
-    max_lbl = neigh_lbls[order[1]]
-    running = false
-    for i=2:length(order)
-        if neigh_lbls_cnt[order[i]] > max_cnt
-            max_cnt = neigh_lbls_cnt[order[i]]
-            max_lbl = neigh_lbls[order[i]]
-        end
-    end
-    if max_lbl != old_lbl
-        membership[u] = max_lbl
-        running = true
-    end
+	running = false
+	if degree(g, u) > 0
+	    old_lbl = membership[u]
+	    label_cnt = label_count(g, membership, u)
+	    neigh_lbls = collect(keys(label_cnt))
+	    neigh_lbls_cnt = collect(values(label_cnt))
+	    order = collect(1:length(label_cnt))
+	    shuffle!(order)
+	    max_cnt = neigh_lbls_cnt[order[1]]
+	    max_lbl = neigh_lbls[order[1]]
+	    for i=2:length(order)
+	        if neigh_lbls_cnt[order[i]] > max_cnt
+	            max_cnt = neigh_lbls_cnt[order[i]]
+	            max_lbl = neigh_lbls[order[i]]
+	        end
+	    end
+	    if max_lbl != old_lbl
+	        membership[u] = max_lbl
+	        running = true
+	    end
+	end
     running
 end
 
