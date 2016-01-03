348b8376cf7e988112c7a5ea48c2d3faac44d52c
Seth Bromberger <github@bromberger.com>
2016-01-03 09:17:31 -0800

    no need for dijkstra if degree = 0

--- src/centrality/closeness.jl
+++ src/centrality/closeness.jl
@@ -9,16 +9,20 @@ function closeness_centrality(
     closeness = zeros(n_v)
 
     for u = 1:n_v
-        d = dijkstra_shortest_paths(g,u).dists
-        δ = filter(x->x != typemax(x), d)
-        σ = sum(δ)
-        l = length(δ) - 1
-        if σ > 0
-            closeness[u] = l / σ
+        if degree(g, u) == 0     # no need to do Dijkstra here
+            closeness[u] = 0.0
+        else
+            d = dijkstra_shortest_paths(g,u).dists
+            δ = filter(x->x != typemax(x), d)
+            σ = sum(δ)
+            l = length(δ) - 1
+            if σ > 0
+                closeness[u] = l / σ
 
-            if normalize
-                n = l / (n_v-1)
-                closeness[u] *= n
+                if normalize
+                    n = l / (n_v-1)
+                    closeness[u] *= n
+                end
             end
         end
     end
