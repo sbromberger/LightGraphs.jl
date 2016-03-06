85eb27a565665af497d8c3aa2985288e9eebde27
Júlio Hoffimann <julio.hoffimann@gmail.com>
2016-03-06 08:36:39 -0800

    Add test for find_path!() from Boykov-Kolmogorov algorithm

--- test/flow/boykov_kolmogorov.jl
+++ test/flow/boykov_kolmogorov.jl
@@ -1,4 +1,27 @@
-using LightGraphs
-using Base.Test
+# construct graph
+G = DiGraph(3)
+add_edge!(G,1,2)
+add_edge!(G,2,3)
 
-# TODO
+# source and sink terminals
+source, target = 1, 3
+
+# default capacity
+capacity_matrix = LightGraphs.DefaultCapacity(G)
+
+# state variables
+flow_matrix = zeros(Int, 3, 3)
+
+TREE = zeros(Int, 3)
+TREE[source] = 1
+TREE[target] = 2
+
+PARENT = zeros(Int, 3)
+
+A = [target,source]
+
+residual_graph = LightGraphs.residual(G)
+
+path = LightGraphs.find_path!(residual_graph, source, target, flow_matrix, capacity_matrix, PARENT, TREE, A)
+
+@test path == [1,2,3]
