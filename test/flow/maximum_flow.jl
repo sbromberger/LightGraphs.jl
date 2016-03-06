1cdd27b17e4a4bfa136a69da6e28ca807ccd5857
Júlio Hoffimann <julio.hoffimann@gmail.com>
2016-03-05 22:46:54 -0800

    Test maximum_flow on multiple graphs

--- test/flow/maximum_flow.jl
+++ test/flow/maximum_flow.jl
@@ -1,35 +1,52 @@
 using LightGraphs
 using Base.Test
 
-# Construct DiGraph
-flow_graph = DiGraph(8)
+#### Graphs for testing
+graphs = [
+  # Graph with 8 vertices
+  (8,
+   [
+     (1,2,10),(1,3,5),(1,4,15),(2,3,4),(2,5,9),
+     (2,6,15),(3,4,4),(3,6,8),(4,7,16),(5,6,15),
+     (5,8,10),(6,7,15),(6,8,10),(7,3,6),(7,8,10)
+   ],
+   1,8, # source/target
+   3,   # answer for default capacity
+   28   # answer for custom capacity
+  ),
 
-# Load custom dataset
-flow_edges = [
-    (1,2,10),(1,3,5),(1,4,15),(2,3,4),(2,5,9),
-    (2,6,15),(3,4,4),(3,6,8),(4,7,16),(5,6,15),
-    (5,8,10),(6,7,15),(6,8,10),(7,3,6),(7,8,10)
+  # Graph with 6 vertices
+  (6,
+   [
+     (1,2,9),(1,3,9),(2,3,10),(2,4,8),(3,4,1),
+     (3,5,3),(5,4,8),(4,6,10),(5,6,7)
+   ],
+   1,6, # source/target
+   2,   # answer for default capacity
+   12   # answer for custom capacity
+  )
 ]
 
-capacity_matrix = zeros(Int,8,8)
+for (nvertices,flow_edges,s,t,fdefault,fcustom) in graphs
+    flow_graph = DiGraph(nvertices)
+    capacity_matrix = zeros(Int,nvertices,nvertices)
+    for e in flow_edges
+        u,v,f = e
+        add_edge!(flow_graph,u,v)
+        capacity_matrix[u,v] = f
+    end
 
-for e in flow_edges
-    u,v,f = e
-    add_edge!(flow_graph,u,v)
-    capacity_matrix[u,v] = f
-end
+    # Test DefaultCapacity
+    d = LightGraphs.DefaultCapacity(flow_graph)
+    @test typeof(d) <: AbstractArray{Int, 2}
+    @test d[s,t] == 0
+    @test size(d) == (nvertices,nvertices)
+    @test typeof(transpose(d)) == LightGraphs.DefaultCapacity
+    @test typeof(ctranspose(d)) == LightGraphs.DefaultCapacity
 
-# Test DefaultCapacity
-d = LightGraphs.DefaultCapacity(flow_graph)
-@test typeof(d) <: AbstractArray{Int, 2}
-@test d[1,8] == 0
-@test d[1,2] == 1
-@test size(d) == (8,8)
-@test typeof(transpose(d)) == LightGraphs.DefaultCapacity
-@test typeof(ctranspose(d)) == LightGraphs.DefaultCapacity
-
-# Test all algorithms
-for ALGO in [EdmondsKarpAlgorithm, DinicAlgorithm, BoykovKolmogorovAlgorithm, PushRelabelAlgorithm]
-  @test maximum_flow(flow_graph,1,8,algorithm=ALGO())[1] == 3
-  @test maximum_flow(flow_graph,1,8,capacity_matrix,algorithm=ALGO())[1] == 28
+    # Test all algorithms
+    for ALGO in [EdmondsKarpAlgorithm, DinicAlgorithm, BoykovKolmogorovAlgorithm, PushRelabelAlgorithm]
+      @test maximum_flow(flow_graph,s,t,algorithm=ALGO())[1] == fdefault
+      @test maximum_flow(flow_graph,s,t,capacity_matrix,algorithm=ALGO())[1] == fcustom
+    end
 end
