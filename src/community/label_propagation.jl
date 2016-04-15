26146f739dc39ae34a93dea7dad046adc58a915b
afternone <afternone@163.com>
2016-04-15 23:56:32 +0800

    better variables' name

--- src/community/label_propagation.jl
+++ src/community/label_propagation.jl
@@ -1,76 +1,77 @@
 """
 Community detection using the label propagation algorithm (see [Raghavan et al.](http://arxiv.org/abs/0709.2938)).
 `g`: imput Graph
+`maxiter`: optional, maximum iterations
 return : array containing vertex assignments, the range of the output as 1:countdistinct(membership)
 """
 function label_propagation(g::SimpleGraph; maxiter=1000)
     n = nv(g)
-    label = collect(1:n)
-    runing_nodes = Set(vertices(g))
-    c = NeighComm(collect(1:n), fill(-1,n), 1)
+    membership = collect(1:n)
+    active_nodes = Set(vertices(g))
+    nc = NeighComm(collect(1:n), fill(-1,n), 1)
     i = 0
-    while !isempty(runing_nodes) && i < maxiter
+    while !isempty(active_nodes) && i < maxiter
     	i += 1
-        order = shuffle(collect(runing_nodes))
-        for u in order
-          old_comm = label[u]
-          label[u] = vote!(g, label, c, u)
-            if old_comm != label[u]
+        random_order = shuffle(collect(active_nodes))
+        for u in random_order
+          old_comm = membership[u]
+          membership[u] = vote!(g, membership, nc, u)
+            if old_comm != membership[u]
                 for v in out_neighbors(g, u)
-                    push!(runing_nodes, v)
+                    push!(active_nodes, v)
                 end
             else
-                delete!(runing_nodes, u)
+                delete!(active_nodes, u)
             end
         end
     end
-    renumber_labels!(label)
-    label
+    renumber_labels!(membership)
+    membership
 end
 
 """Fast shuffle Array `a` in UnitRange `r` inplace."""
 function range_shuffle!(r::UnitRange, a::AbstractVector)
-  for i=length(r):-1:2
-    j = rand(1:i)
-    ii = i + r.start - 1
-    jj = j + r.start - 1
-    a[ii],a[jj] = a[jj],a[ii]
-  end
+	for i=length(r):-1:2
+		j = rand(1:i)
+		ii = i + r.start - 1
+		jj = j + r.start - 1
+		a[ii],a[jj] = a[jj],a[ii]
+	end
 end
 
-"""Type to record neighbor labels and their count."""
+"""Type to record neighbor labels and their counts."""
 type NeighComm
-  neigh_pos::Vector{Int}
-  neigh_cnt::Vector{Int}
-  neigh_last::Int
+	neigh_pos::Vector{Int}
+	neigh_cnt::Vector{Int}
+	neigh_last::Int
 end
 
 """Return the most frequency label."""
-function vote!(g, m, c, u)
-    for i=1:c.neigh_last-1
-        c.neigh_cnt[c.neigh_pos[i]] = -1
+function vote!(g::SimpleGraph, membership::Vector{Int}, nc::NeighComm, u::Int)
+    for i=1:nc.neigh_last-1
+        nc.neigh_cnt[nc.neigh_pos[i]] = -1
     end
-    c.neigh_last = 1
-    c.neigh_pos[1] = m[u]
-    c.neigh_cnt[c.neigh_pos[1]] = 0
-    c.neigh_last = 2
+    nc.neigh_last = 1
+    nc.neigh_pos[1] = membership[u]
+    nc.neigh_cnt[nc.neigh_pos[1]] = 0
+    nc.neigh_last = 2
     max_cnt = 0
     for neigh in out_neighbors(g,u)
-        neigh_comm = m[neigh]
-        if c.neigh_cnt[neigh_comm] < 0
-            c.neigh_cnt[neigh_comm] = 0
-            c.neigh_pos[c.neigh_last] = neigh_comm
-            c.neigh_last += 1
+        neigh_comm = membership[neigh]
+        if nc.neigh_cnt[neigh_comm] < 0
+            nc.neigh_cnt[neigh_comm] = 0
+            nc.neigh_pos[nc.neigh_last] = neigh_comm
+            nc.neigh_last += 1
         end
-        c.neigh_cnt[neigh_comm] += 1
-        if c.neigh_cnt[neigh_comm] > max_cnt
-          max_cnt = c.neigh_cnt[neigh_comm]
+        nc.neigh_cnt[neigh_comm] += 1
+        if nc.neigh_cnt[neigh_comm] > max_cnt
+          max_cnt = nc.neigh_cnt[neigh_comm]
         end
     end
     # ties breaking randomly
-    range_shuffle!(1:c.neigh_last-1, c.neigh_pos)
-    for lbl in c.neigh_pos
-      if c.neigh_cnt[lbl] == max_cnt
+    range_shuffle!(1:nc.neigh_last-1, nc.neigh_pos)
+    for lbl in nc.neigh_pos
+      if nc.neigh_cnt[lbl] == max_cnt
         return lbl
       end
     end
