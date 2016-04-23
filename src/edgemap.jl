3f0de5311b2cd36aecb7802d9303a6c80ad51ed6
CarloLucibello <carlo.lucibello@gmail.com>
2016-04-23 18:28:50 +0200

    add ConstEdgeMap and fix bug

--- src/edgemap.jl
+++ src/edgemap.jl
@@ -3,12 +3,20 @@ type EdgeMap{T, D, G <: SimpleGraph}
     data::D
 end
 
-getindex(em::EdgeMap, e::Edge) = setindex(em, data[e])
+getindex(em::EdgeMap, e::Edge) = getindex(em.data, e)
 setindex!(t::EdgeMap, v, e::Edge) = setindex!(em.data, v, e)
 
+typealias DefaultEdgeMap{T, G} EdgeMap{T, Dict{Edge,T}, G}
 # typealias EdgeMap{T, G} EdgeMap{T, Dict{Edge, T}, G}
 
 EdgeMap{T, G<:SimpleGraph}(::Type{T}, g::G) =
-        EdgeMap{T, Dict{Edge, T}, G}(g, Dict{Edge, T}())
+    DefaultEdgeMap{T, G}(g, Dict{Edge, T}())
 
-show(io::IO, em::EdgeMap) = print(io, "EdgeMap($(em.data) on $(em.g))") 
+show(io::IO, em::EdgeMap) = print(io, "EdgeMap($(em.data) on $(em.g))")
+
+typealias ConstEdgeMap{T, G} EdgeMap{T, T, G}
+
+ConstEdgeMap{G,T}(g::SimpleGraph, x::T) = EdgeMap{T, T, G}(g, x)
+
+getindex(em::ConstEdgeMap, e::Edge) = em.data
+setindex!(t::ConstEdgeMap, v, e::Edge) = error("Cannot assign to ConstEdgeMap.")
