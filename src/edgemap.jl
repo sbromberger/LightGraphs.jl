8a9dae274a7a69473a139938f94adac24c2a0849
CarloLucibello <carlo.lucibello@gmail.com>
2016-04-23 18:35:45 +0200

    [edgemap] add iterable interface

--- src/edgemap.jl
+++ src/edgemap.jl
@@ -3,8 +3,6 @@ type EdgeMap{T, D, G <: SimpleGraph}
     data::D
 end
 
-getindex(em::EdgeMap, e::Edge) = getindex(em.data, e)
-setindex!(t::EdgeMap, v, e::Edge) = setindex!(em.data, v, e)
 
 typealias DefaultEdgeMap{T, G} EdgeMap{T, Dict{Edge,T}, G}
 # typealias EdgeMap{T, G} EdgeMap{T, Dict{Edge, T}, G}
@@ -18,5 +16,16 @@ typealias ConstEdgeMap{T, G} EdgeMap{T, T, G}
 
 ConstEdgeMap{G,T}(g::SimpleGraph, x::T) = EdgeMap{T, T, G}(g, x)
 
+# Associative interface
+getindex(em::EdgeMap, e::Edge) = getindex(em.data, e)
+setindex!(em::EdgeMap, v, e::Edge) = setindex!(em.data, v, e)
+eltype(::Type{EdgeMap}) = eltype(typeof(em.data))
+length(em::EdgeMap) = length(em.data)
+
 getindex(em::ConstEdgeMap, e::Edge) = em.data
-setindex!(t::ConstEdgeMap, v, e::Edge) = error("Cannot assign to ConstEdgeMap.")
+setindex!(em::ConstEdgeMap, v, e::Edge) = error("Cannot assign to ConstEdgeMap.")
+
+# Iterable interface
+start(em::EdgeMap) = start(em.data)
+next(em::EdgeMap, state)  = start(em.data)
+done(em::EdgeMap, state) = done(em.data)
