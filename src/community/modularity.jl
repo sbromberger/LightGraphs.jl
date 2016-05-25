683d408ed55fd695504966c3f99732e89398cb97
afternone <afternone@163.com>
2016-05-25 17:40:11 +0800

    more readable

--- src/community/modularity.jl
+++ src/community/modularity.jl
@@ -5,8 +5,10 @@ Computes Newman's modularity `Q`
 for graph `g` given the partitioning `c`.
 """
 function modularity(g::Graph, c)
+    m = ne(g)
+    m == 0 && (return 0.0)
     labels = unique(c)
-    length(labels) == 1 && return 0.0
+    length(labels) == 1 && (return 0.0)
     e = Dict{Int,Int}()
     a = Dict{Int,Int}()
     for edge in edges(g)
@@ -18,14 +20,11 @@ function modularity(g::Graph, c)
         a[c1] = get(a,c1,0) + 1
         a[c2] = get(a,c2,0) + 1
     end
-    m = ne(g)
     Q = 0.0
-    if m > 0
-        for label in labels
-            tmp = get(a,label,0)/2/m
-            Q += get(e,label,0)/2/m
-            Q -= tmp*tmp
-        end
+    for label in labels
+        tmp = get(a,label,0)/2/m
+        Q += get(e,label,0)/2/m
+        Q -= tmp*tmp
     end
     return Q
 end
