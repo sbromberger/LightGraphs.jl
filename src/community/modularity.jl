3c57ee87b42cc0aac73d7665172344bd52530869
afternone <afternone@163.com>
2016-05-26 09:48:16 +0800

    less memory space

--- src/community/modularity.jl
+++ src/community/modularity.jl
@@ -6,27 +6,26 @@ for graph `g` given the partitioning `c`.
 """
 function modularity(g::Graph, c)
     n = nv(g)
-    m = 2 * ne(g)
-    m == 0 && return 0.
+    m = 2*ne(g)
+    m == 0 && return 0.0
     nc = maximum(c)
     a = zeros(Int, nc)
-    Q = 0
+    Q = 0.0
     for u in vertices(g)
         for v in neighbors(g, u)
             if u <= v
                 c1 = c[u]
                 c2 = c[v]
                 if c1 == c2
-                    Q += 2
+                    Q += 2/m
                 end
                 a[c1] += 1
                 a[c2] += 1
             end
         end
     end
-    Q = Q * m
     @inbounds for i=1:nc
-        Q -= a[i] * a[i]
+        Q -= a[i]*a[i]/m/m
     end
-    return Q/m/m
+    return Q
 end
