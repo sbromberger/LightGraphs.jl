341ac92d3f7ec6b0920f0fabcb34c9aba2054210
David Widmann <dev+git@devmotion.de>
2016-06-05 23:47:46 +0200

    Added line breaks

--- src/randgraphs.jl
+++ src/randgraphs.jl
@@ -150,9 +150,12 @@ end
 """
     barabasi_albert(n::Integer, k::Integer; is_directed::Bool = false, complete::Bool = false, seed::Int = -1)
 
-Creates a [Barabási–Albert model](https://en.wikipedia.org/wiki/Barab%C3%A1si%E2%80%93Albert_model) random graph with `n` vertices.
-It is grown by adding new vertices to an initial graph with `k` vertices. Each new vertex is attached with `k` edges to `k` different vertices already present in the system by preferential attachment.
-Initial graphs are undirected and consist of isolated vertices by default; use `is_directed=true` and `complete=true` for directed and complete initial graphs.
+Creates a [Barabási–Albert model](https://en.wikipedia.org/wiki/Barab%C3%A1si%E2%80%93Albert_model)
+random graph with `n` vertices. It is grown by adding new vertices to an initial
+graph with `k` vertices. Each new vertex is attached with `k` edges to `k`
+different vertices already present in the system by preferential attachment.
+Initial graphs are undirected and consist of isolated vertices by default; 
+use `is_directed=true` and `complete=true` for directed and complete initial graphs.
 """
 barabasi_albert(n::Integer, k::Integer; keyargs...) =
     barabasi_albert(n, k, k; keyargs...)
@@ -160,9 +163,12 @@ barabasi_albert(n::Integer, k::Integer; keyargs...) =
 """
     barabasi_albert(n::Integer, n0::Integer, k::Integer; is_directed::Bool = false, complete::Bool = false, seed::Int = -1)
 
-Creates a [Barabási–Albert model](https://en.wikipedia.org/wiki/Barab%C3%A1si%E2%80%93Albert_model) random graph with `n` vertices.
-It is grown by adding new vertices to an initial graph with `n0` vertices. Each new vertex is attached with `k` edges to `k` different vertices already present in the system by preferential attachment.
-Initial graphs are undirected and consist of isolated vertices by default; use `is_directed=true` and `complete=true` for directed and complete initial graphs.
+Creates a [Barabási–Albert model](https://en.wikipedia.org/wiki/Barab%C3%A1si%E2%80%93Albert_model)
+random graph with `n` vertices. It is grown by adding new vertices to an initial
+graph with `n0` vertices. Each new vertex is attached with `k` edges to `k`
+different vertices already present in the system by preferential attachment.
+Initial graphs are undirected and consist of isolated vertices by default;
+use `is_directed=true` and `complete=true` for directed and complete initial graphs.
 """
 function barabasi_albert(n::Integer, n0::Integer, k::Integer; is_directed::Bool = false, complete::Bool = false, seed::Int = -1)
     if complete
@@ -178,12 +184,16 @@ end
 """
     barabasi_albert!(g::SimpleGraph, n::Integer, k::Integer; seed::Int = -1)
 
-Creates a [Barabási–Albert model](https://en.wikipedia.org/wiki/Barab%C3%A1si%E2%80%93Albert_model) random graph with `n` vertices.
-It is grown by adding new vertices to an initial graph `g`. Each new vertex is attached with `k` edges to `k` different vertices already present in the system by preferential attachment.
+Creates a [Barabási–Albert model](https://en.wikipedia.org/wiki/Barab%C3%A1si%E2%80%93Albert_model)
+random graph with `n` vertices. It is grown by adding new vertices to an initial
+graph `g`. Each new vertex is attached with `k` edges to `k` different vertices
+already present in the system by preferential attachment.
 """
 function barabasi_albert!(g::SimpleGraph, n::Integer, k::Integer; seed::Int=-1)
     n0 = nv(g)
-    1 <= k <= n0 <= n || throw(ArgumentError("Barabási-Albert model requires 1 <= k <= n0 <= n where n0 is the number of nodes in graph g"))
+    1 <= k <= n0 <= n ||
+        throw(ArgumentError("Barabási-Albert model requires 1 <= k <= n0 <= n" *
+			    "where n0 is the number of nodes in graph g"))
     n0 == n && return g
     
     # seed random number generator
@@ -193,7 +203,8 @@ function barabasi_albert!(g::SimpleGraph, n::Integer, k::Integer; seed::Int=-1)
     sizehint!(g.fadjlist, n)
     add_vertices!(g, n - n0)
 
-    # if initial graph doesn't contain any edges expand it by one vertex and add k edges from this additional node
+    # if initial graph doesn't contain any edges
+    # expand it by one vertex and add k edges from this additional node
     if ne(g) == 0
         # expand initial graph
         n0 += 1
