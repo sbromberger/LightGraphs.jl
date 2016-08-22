9e5e533ade81cedbbb294907438c76e9f1d7c3ff
CarloLucibello <carlo.lucibello@gmail.com>
2016-08-22 23:37:17 +0200

    improve pagerank

--- src/centrality/pagerank.jl
+++ src/centrality/pagerank.jl
@@ -11,8 +11,8 @@ function pagerank(g::DiGraph, α=0.85, n=100, ϵ = 1.0e-6)
     S = vec(sum(A,1))
     S = 1./S
     S[find(S .== Inf)]=0.0
-    M = A' # need a separate line due to a bug in julia
-    M = Diagonal(S) * M
+    M = A' # need a separate line due to bug #17456 in julia
+    M = (Diagonal(S) * M)'
     N = nv(g)
     x = repmat([1.0/N], N)
     p = repmat([1.0/N], N)
@@ -21,7 +21,7 @@ function pagerank(g::DiGraph, α=0.85, n=100, ϵ = 1.0e-6)
 
     for _ in 1:n
         xlast = x
-        x = α * (M.' * x + sum(x[is_dangling]) * dangling_weights) + (1 - α) * p
+        x = α * (M * x + sum(x[is_dangling]) * dangling_weights) + (1 - α) * p
         err = sum(abs(x - xlast))
         if (err < N * ϵ)
             return x
