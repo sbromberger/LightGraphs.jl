a9c6887d8522c13584f6e8c74c430a4d89158cad
athulappadan <a4athulappadan@gmail.com>
2016-10-27 11:21:21 +0530

    test data modified

--- test/spanningtrees/prim.jl
+++ test/spanningtrees/prim.jl
@@ -17,42 +17,18 @@ vec_mst = Vector{Edge}([Edge(1, 2), Edge(2, 3), Edge(3, 4)])
 @test mst == vec_mst
 
 #second test
-g = Graph(8)
-add_edge!(g, 2,8)
-add_edge!(g, 1,3)
-add_edge!(g, 6,8)
-add_edge!(g, 1,8)
-add_edge!(g, 3,4)
-add_edge!(g, 2,4)
-add_edge!(g, 2,6)
-add_edge!(g, 3,8)
-add_edge!(g, 6,5)
-add_edge!(g, 2,3)
-add_edge!(g, 5,7)
-add_edge!(g, 1,7)
-add_edge!(g, 4,7)
-add_edge!(g, 7,3)
-add_edge!(g, 1,5)
-add_edge!(g, 5,8)
-
-distmx_sec = zeros(8, 8)
-distmx_sec[2,8] = distmx_sec[8,2] = 0.19
-distmx_sec[1,3] = distmx_sec[3,1] = 0.26
-distmx_sec[6,8] = distmx_sec[8,6] = 0.28
-distmx_sec[1,8] = distmx_sec[8,1] = 0.16
-distmx_sec[3,4] = distmx_sec[4,3] = 0.17
-distmx_sec[2,4] = distmx_sec[4,2] = 0.29
-distmx_sec[2,6] = distmx_sec[6,2] = 0.32
-distmx_sec[3,8] = distmx_sec[8,3] = 0.34
-distmx_sec[5,6] = distmx_sec[6,5] = 0.35
-distmx_sec[2,3] = distmx_sec[3,2] = 0.36
-distmx_sec[5,7] = distmx_sec[7,5] = 0.93
-distmx_sec[1,7] = distmx_sec[7,1] = 0.58
-distmx_sec[4,7] = distmx_sec[7,4] = 0.52
-distmx_sec[3,7] = distmx_sec[7,3] = 0.40
-distmx_sec[1,5] = distmx_sec[5,1] = 0.38
-distmx_sec[5,8] = distmx_sec[8,5] = 0.37
-
+distmx_sec = [
+                0   0   0.26    0   0.38    0   0.58    0.16
+                0   0   0.36    0.29    0   0.32    0   0.19
+                0.26    0.36    0   0.17    0   0   0.4 0.34
+                0   0.29    0.17    0   0   0   0.52    0
+                0.38    0   0   0   0   0.35    0.93    0.37
+                0   0.32    0   0   0.35    0   0   0.28
+                0.58    0   0.4 0.52    0.93    0   0   0
+                0.16    0.19    0.34    0   0.37    0.28    0   0
+                ]
+                
+g = Graph(distmx_sec)
 mst2 = prim_mst(g, distmx_sec)
 vec2 = Vector{Edge}([Edge(1, 8),Edge(8, 2),Edge(1, 3),Edge(3, 4),Edge(8, 6),Edge(6, 5),Edge(3, 7)])
 
