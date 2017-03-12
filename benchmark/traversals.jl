6124998293a2e145801df87583b5d51622dce60f
Seth Bromberger <github@bromberger.com>
2017-03-12 14:19:54 -0700

    pkgbenchmarks

---
+++ benchmark/traversals.jl
@@ -0,0 +1,14 @@
+@benchgroup "traversals" begin
+  @benchgroup "graphs" begin
+    for (name, g) in GRAPHS
+        @bench "$(name): bfs_tree" LightGraphs.bfs_tree($g, 1)
+        @bench "$(name): dfs_tree" LightGraphs.dfs_tree($g, 1)
+    end
+  end # graphs
+  @benchgroup "digraphs" begin
+    for (name, g) in DIGRAPHS
+        @bench "$(name): bfs_tree" LightGraphs.bfs_tree($g, 1)
+        @bench "$(name): dfs_tree" LightGraphs.dfs_tree($g, 1)
+    end
+  end # digraphs
+end # traversals
