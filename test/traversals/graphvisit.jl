4c920302d4ef7b88fd47fbd206bb9821220f233b
Júlio Hoffimann <julio.hoffimann@gmail.com>
2017-03-12 18:52:32 -0700

    Create test set for graphvisist.jl and maxadjvisit.jl

--- test/traversals/graphvisit.jl
+++ test/traversals/graphvisit.jl
@@ -1,28 +1,30 @@
-# stub tests for coverage; disregards output.
+@testset "Graph visit" begin
+    # stub tests for coverage; disregards output.
 
-f = IOBuffer()
+    f = IOBuffer()
 
-@test traverse_graph_withlog(g6, BreadthFirst(), [1;], f) == nothing
+    @test traverse_graph_withlog(g6, BreadthFirst(), [1;], f) == nothing
 
-@test visited_vertices(g6, BreadthFirst(), [1;]) == [1, 2, 3, 4, 5]
+    @test visited_vertices(g6, BreadthFirst(), [1;]) == [1, 2, 3, 4, 5]
 
 
-function trivialgraphvisit(
-    g::AbstractGraph,
-    alg::LightGraphs.AbstractGraphVisitAlgorithm,
-    sources
-)
-    visitor = TrivialGraphVisitor()
-    traverse_graph!(g, alg, sources, visitor)
+    function trivialgraphvisit(
+        g::AbstractGraph,
+        alg::LightGraphs.AbstractGraphVisitAlgorithm,
+        sources
+    )
+        visitor = TrivialGraphVisitor()
+        traverse_graph!(g, alg, sources, visitor)
+    end
+
+    @test trivialgraphvisit(g, BreadthFirst(), 1) == nothing
+
+    # this just exercises some graph visitors
+    @test traverse_graph!(g, BreadthFirst(), 1, TrivialGraphVisitor()) == nothing
+    @test traverse_graph!(g, BreadthFirst(), 1, LogGraphVisitor(IOBuffer())) == nothing
+
+    # dummy edge map test
+    d = LightGraphs.DummyEdgeMap()
+    e = Edge(1,2)
+    @test d[e] == 0
 end
-
-@test trivialgraphvisit(g, BreadthFirst(), 1) == nothing
-
-# this just exercises some graph visitors
-@test traverse_graph!(g, BreadthFirst(), 1, TrivialGraphVisitor()) == nothing
-@test traverse_graph!(g, BreadthFirst(), 1, LogGraphVisitor(IOBuffer())) == nothing
-
-# dummy edge map test
-d = LightGraphs.DummyEdgeMap()
-e = Edge(1,2)
-@test d[e] == 0
