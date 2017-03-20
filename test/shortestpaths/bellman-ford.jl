2a937bdf2efdce2aa662109800a3aa7992f56d1b
Seth Bromberger <github@bromberger.com>
2017-03-19 17:52:01 -0700

    cherry pick bellman-ford test

--- test/shortestpaths/bellman-ford.jl
+++ test/shortestpaths/bellman-ford.jl
@@ -8,7 +8,10 @@
     @test enumerate_paths(z)[2] == []
     @test enumerate_paths(z)[4] == enumerate_paths(z,4) == [2,3,4]
     @test !has_negative_edge_cycle(g4)
-
     z = bellman_ford_shortest_paths(g4, 2)
     @test z.dists == [typemax(Int), 0, 1, 2, 3]
+
+    gx = CompleteGraph(3)
+    d = [1 -3 1; -3 1 1; 1 1 1]
+    @test_throws LightGraphs.NegativeCycleError bellman_ford_shortest_paths(gx, 1, d)
 end
