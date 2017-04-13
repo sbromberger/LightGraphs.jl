951a3093a4976322ca25d7e0ac6e19ce739397ad
Elias Bouacicda <elias.bouacida@mailoo.org>
2017-04-13 14:45:25 +0200

    Update cyclicity.jl

--- test/cyclicity.jl
+++ test/cyclicity.jl
@@ -1,27 +1,31 @@
 complete = CompleteDiGraph(4)
+path = PathDiGraph(5)
+triangle = random_regular_graph(3,2)
+quadrangle = random_regular_graph(4,2)
+pentagon = random_regular_graph(5,2)
 
-@test maxsimplecycles(g4) == 0
+@test maxsimplecycles(path) == 0
 @test maxsimplecycles(complete) == 20
 @test maxsimplecycles(4) == 20
-@test maxsimplecycles(g4, false) == 84
+@test maxsimplecycles(path, false) == 84
 
 @test length(simplecycles(complete)) == 20
 @test simplecycles(complete) == simplecycles_iter(complete)
 @test simplecyclescount(complete) == 20
 @test simplecycleslength(complete) == ([0,6,8,6], 20)
 
-@test simplecyclescount(g4) == 0
-@test length(simplecycles(g4)) == 0
-@test isempty(simplecycles(g4)) == true
-@test isempty(simplecycles_iter(g4)) == true
-@test simplecycleslength(g4) == (zeros(5), 0)
+@test simplecyclescount(path) == 0
+@test length(simplecycles(path)) == 0
+@test isempty(simplecycles(path)) == true
+@test isempty(simplecycles_iter(path)) == true
+@test simplecycleslength(path) == (zeros(5), 0)
 
 @test simplecyclescount(complete, 10) == 10
 @test simplecycleslength(complete, 10)[2] == 10
 
-@test simplecyclescount(g4, 10) == 0
-@test isempty(simplecycles_iter(g4, 10)) == true
-@test simplecycleslength(g4, 10) == (zeros(5), 0)
+@test simplecyclescount(path, 10) == 0
+@test isempty(simplecycles_iter(path, 10)) == true
+@test simplecycleslength(path, 10) == (zeros(5), 0)
 
 trianglelengths, triangletotal = simplecycleslength(DiGraph(triangle))
 @test sum(trianglelengths) == triangletotal
@@ -32,4 +36,3 @@ quadranglelengths, quadrangletotal = simplecycleslength(DiGraph(quadrangle))
 
 pentagonlengths, pentagontotal = simplecycleslength(DiGraph(pentagon))
 @test sum(pentagonlengths) == pentagontotal
-
