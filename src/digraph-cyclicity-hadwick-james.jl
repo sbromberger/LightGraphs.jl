21bb1b831306da36ca0ef76e605e2f0d118d70ed
Jonathan Goldfarb <jgoldfar@gmail.com>
2017-04-24 16:34:32 -0400

    Update implementation according to reviews and improve code coverage with an additional test

--- src/digraph-cyclicity-hadwick-james.jl
+++ src/digraph-cyclicity-hadwick-james.jl
@@ -5,10 +5,14 @@ Find circuits (including self-loops) using the algorithm of Hadwick & James
 function simplecycles_hadwick_james end
 @traitfn function simplecycles_hadwick_james(g::::IsDirected)
     nvg = nv(g)
-    B = [Int[] for i in 1:nvg]
-    blocked = [false for i in 1:nvg]
-    stack = Int[]
-    cycles = Vector{Vector{Int}}()
+    T = eltype(g)
+    B = Vector{Vector{T}}()
+    for i in vertices(g)
+        push!(B, Vector{T}())
+    end
+    blocked = falses(nvg)
+    stack = T[]
+    cycles = Vector{Vector{T}}()
     for v in vertices(g)
         circuit_recursive!(g, v, v, blocked, B, stack, cycles)
         resetblocked!(blocked)
@@ -31,7 +35,7 @@ resetblocked!(blocked) = fill!(blocked, false)
 Find circuits starting from v1 recursively.
 """
 function circuit_recursive! end
-@traitfn function circuit_recursive!(g::::IsDirected, v1::Int, v2::Int, blocked::Vector{Bool}, B::Vector{Vector{Int}}, stack::Vector{Int}, cycles::Vector{Vector{Int}})
+@traitfn function circuit_recursive!{T}(g::::IsDirected, v1::T, v2::T, blocked::BitArray, B::Vector{Vector{T}}, stack::Vector{T}, cycles::Vector{Vector{T}})
     f = false
     push!(stack, v2)
     blocked[v2] = true
@@ -43,9 +47,7 @@ function circuit_recursive! end
             push!(cycles, copy(stack))
             f = true
         elseif !blocked[w]
-            if circuit_recursive!(g, v1, w, blocked, B, stack, cycles)
-                f = true
-            end
+            f = circuit_recursive!(g, v1, w, blocked, B, stack, cycles)
         end
     end
     if f
@@ -53,7 +55,7 @@ function circuit_recursive! end
     else
         for w in Av
             (w < v1) && continue
-            if !in(v2, B[w])
+            if !(v2 in B[w])
                 push!(B[w], v2)
             end
         end
@@ -65,7 +67,7 @@ end
 """
 Simultaneously count and remove occurences of a value `val` in the array `list`.
 """
-function countAndFilter!{T}(val::T, list::AbstractArray{T})
+function countAndFilter!(val::T, list::AbstractArray{T}) where T
     nocc = 0
     function doFilter(v)
         if v == val
@@ -82,7 +84,7 @@ end
 """
 Unblock the value `v` from the blocked list and remove from `B`.
 """
-function unblock!{T}(v::T, blocked::Vector{Bool}, B::Vector{Vector{T}})
+function unblock!(v::T, blocked::BitArray, B::Vector{Vector{T}}) where T
     blocked[v] = false
     wPos = 1
     Bv = B[v]
