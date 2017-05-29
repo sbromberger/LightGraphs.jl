cf5824bbbce2e058eb7bc7449d4f8017e9cf0db9
somil55 <somm14divi@gmail.com>
2017-05-30 01:09:00 +0530

    Modified betweeness.jl to add parallel_betweeness_centrality and added tests for this

--- test/centrality/betweenness.jl
+++ test/centrality/betweenness.jl
@@ -20,30 +20,50 @@
 
     c = readcentrality(joinpath(testdir,"testdata","graph-50-500-bc.txt"))
     for g in testdigraphs(gint)
-        z = @inferred(betweenness_centrality(g))
-        @test map(Float32, z) == map(Float32, c)
+        z  = @inferred(betweenness_centrality(g))
+        zp = @inferred(parallel_betweenness_centrality(g))
+        @test all(isapprox.(z,zp))
+        @test map(Float32, z)  == map(Float32, c)
 
-        y = @inferred(betweenness_centrality(g, endpoints=true, normalize=false))
+        y  = @inferred(betweenness_centrality(g, endpoints=true, normalize=false))
+        yp = parallel_betweenness_centrality(g, endpoints=true, normalize=false)
+        @test all(isapprox(y,yp))
         @test round.(y[1:3],4) ==
             round.([122.10760591498584, 159.0072453120582, 176.39547945994505], 4)
 
-        x = @inferred(betweenness_centrality(g,3))
+
+
+        x  = @inferred(betweenness_centrality(g,3))
+        x2  = @inferred(betweenness_centrality(g,collect(1:20)))
+        xp2 = parallel_betweenness_centrality(g,collect(1:20))
+        @test all(isapprox(x2,xp2))
         @test length(x) == 50
     end
 
     @test @inferred(betweenness_centrality(s1)) == [0, 1, 0]
+    @test parallel_betweenness_centrality(s1)   == [0, 1, 0]
+
     @test @inferred(betweenness_centrality(s2)) == [0, 0.5, 0]
+    @test parallel_betweenness_centrality(s2)   == [0, 0.5, 0]
 
     g = Graph(2)
     add_edge!(g,1,2)
-    z = @inferred(betweenness_centrality(g; normalize=true))
+    z  = @inferred(betweenness_centrality(g; normalize=true))
+    zp = parallel_betweenness_centrality(g; normalize=true)
+    all(isapprox(z,zp))
     @test z[1] == z[2] == 0.0
-    z2 = @inferred(betweenness_centrality(g, vertices(g)))
-    z3 = @inferred(betweenness_centrality(g, [vertices(g);]))
+    z2  = @inferred(betweenness_centrality(g, vertices(g)))
+    zp2 = parallel_betweenness_centrality(g, vertices(g))
+    all(isapprox(z2,zp2))
+    z3  = @inferred(betweenness_centrality(g, [vertices(g);]))
+    zp3 = parallel_betweenness_centrality(g, [vertices(g);])
+    all(isapprox(z3,zp3))
 
     @test z == z2 == z3
 
 
-    z = @inferred(betweenness_centrality(g3; normalize=false))
+    z  = @inferred(betweenness_centrality(g3; normalize=false))
+    zp = parallel_betweenness_centrality(g3; normalize=false)
+    all(isapprox(z,zp))
     @test z[1] == z[5] == 0.0
 end
