2ad036e594283d3bdefd4d0e20bab4291f4ab48e
Seth Bromberger <sbromberger@users.noreply.github.com>
2017-05-30 14:42:25 -0700

    Kronecker graph generator (#630)
    * Kronecker graph generator
       - Update randgraphs.jl

--- src/LightGraphs.jl
+++ src/LightGraphs.jl
@@ -94,6 +94,7 @@ multiroute_flow, KishimotoAlgorithm, ExtendedMultirouteFlowAlgorithm,
 erdos_renyi, watts_strogatz, random_regular_graph, random_regular_digraph, random_configuration_model,
 StochasticBlockModel, make_edgestream, nearbipartiteSBM, blockcounts, blockfractions,
 stochastic_block_model, barabasi_albert, barabasi_albert!, static_fitness_model, static_scale_free,
+kronecker,
 
 #community
 modularity, core_periphery_deg,
--- src/generators/randgraphs.jl
+++ src/generators/randgraphs.jl
@@ -843,3 +843,40 @@ function blockfractions(sbm::StochasticBlockModel, g::Union{AbstractGraph, Abstr
     bp = bc ./ sum(bc)
     return bp
 end
+
+"""
+    kronecker(SCALE, edgefactor, A=0.57, B=0.19, C=0.19)
+
+Generate a directed [Kronecker graph](https://en.wikipedia.org/wiki/Kronecker_graph)
+with the default Graph500 parameters.
+
+###
+References
+- http://www.graph500.org/specifications#alg:generator
+"""
+function kronecker(SCALE, edgefactor, A=0.57, B=0.19, C=0.19)
+    N = 2^SCALE
+    M = edgefactor * N
+    ij = ones(Int, M, 2)
+    ab = A + B
+    c_norm = C/(1 - (A + B))
+    a_norm = A/(A + B)
+
+    for ib = 1:SCALE
+        ii_bit = rand(M).>(ab)  # bitarray
+        jj_bit = rand(M).> ( c_norm.*(ii_bit) + a_norm.*.!(ii_bit) )
+        ij .+= 2^(ib-1).*(hcat(ii_bit, jj_bit))
+    end
+
+    p = randperm(N)
+    ij = p[ij]
+
+    p = randperm(M)
+    ij = ij[p, :]
+
+    g = DiGraph(N)
+    for (s,d) in zip(@view(ij[:,1]), @view(ij[:,2]))
+        add_edge!(g, s, d)
+    end
+    return g
+end
