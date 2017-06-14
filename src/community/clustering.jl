8e66bc97b1ffd6bef4229791f2962ac962eb3bd6
James Fairbanks <james.fairbanks@gtri.gatech.edu>
2017-06-14 13:27:17 -0400

    rewrite clustering coefficients for speed

--- src/community/clustering.jl
+++ src/community/clustering.jl
@@ -26,27 +26,50 @@ the maximum number of possible triangles, respectively, for each node in the lis
 
 This function is related to the local clustering coefficient `r` by ``r=\frac{a}{b}``.
 """
-function local_clustering(g::AbstractGraph, v::Integer)
+function local_clustering!(storage::AbstractVector{Bool}, g::AbstractGraph, v::Integer)
     k = degree(g, v)
     k <= 1 && return (0, 0)
     neighs = neighbors(g, v)
-    c = 0
-    for i in neighs, j in neighs
-        i == j && continue
-        if has_edge(g, i, j)
-            c += 1
+    tcount = 0
+    for i in neighs
+        storage[i] = true
+    end
+    for i in neighs
+        for j in neighbors(g, i)
+            i == j && continue
+            if storage[j]
+                tcount += 1
+            end
         end
     end
-    return is_directed(g) ? (c , k*(k-1)) : (div(c,2) , div(k*(k-1),2))
+    return is_directed(g) ? (tcount , k*(k-1)) : (div(tcount,2) , div(k*(k-1),2))
 end
-function local_clustering(g::AbstractGraph, vs = vertices(g))
-    ntriang = zeros(Int, length(vs))
-    nalltriang = zeros(Int, length(vs))
+
+function local_clustering!(storage::AbstractVector{Bool},
+                           ntriang::AbstractVector{Int},
+                           nalltriang::AbstractVector{Int},
+                           g::AbstractGraph,
+                           vs)
     i = 0
     for (i, v) in enumerate(vs)
-        ntriang[i], nalltriang[i] = local_clustering(g, v)
+        ntriang[i], nalltriang[i] = local_clustering!(storage, g, v)
+        for w in neighbors(g, v)
+            storage[w] = false
+        end
     end
     return ntriang, nalltriang
+end
+
+function local_clustering(g::AbstractGraph, v::Integer)
+    storage = zeros(Bool, nv(g))
+    return local_clustering!(storage, g, v)
+end
+
+function local_clustering(g::AbstractGraph, vs = vertices(g))
+    storage = zeros(Bool, nv(g))
+    ntriang = zeros(Int, length(vs))
+    nalltriang = zeros(Int, length(vs))
+    return local_clustering!(storage, ntriang, nalltriang, g, vs)
 end
 
 
