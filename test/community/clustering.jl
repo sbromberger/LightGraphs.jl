52295664a056fbfe438148d7191847cd0965f3e8
James <jpfairbanks@gmail.com>
2017-06-15 14:56:48 -0400

    rewrite clustering coefficients for speed (#654)
    * rewrite clustering coefficients for speed
    
    * improve local_clustering_coefficients, docs, benchmarks
    
    * Create clustering.jl
    
    * Create clustering.jl
    
    * codecov

--- test/community/clustering.jl
+++ test/community/clustering.jl
@@ -1,10 +1,11 @@
 @testset "Clustering" begin
     g10 = CompleteGraph(10)
     for g in testgraphs(g10)
-      @test @inferred(local_clustering_coefficient(g)) == ones(10)
-      @test @inferred(global_clustering_coefficient(g)) == 1
-      @test @inferred(local_clustering(g)) == (fill(36, 10), fill(36, 10))
-      @test @inferred(triangles(g)) == fill(36, 10)
-      @test @inferred(triangles(g, 1)) == 36
+        @test @inferred(local_clustering_coefficient(g, 1)) == 1.0
+        @test @inferred(local_clustering_coefficient(g)) == ones(10)
+        @test @inferred(global_clustering_coefficient(g)) == 1.0
+        @test @inferred(local_clustering(g)) == (fill(36, 10), fill(36, 10))
+        @test @inferred(triangles(g)) == fill(36, 10)
+        @test @inferred(triangles(g, 1)) == 36
     end
 end
