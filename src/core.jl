fd2c4350fda340d4e72f37523759c059c2499919
Seth Bromberger <github@bromberger.com>
2017-07-11 13:49:12 -0700

    update syntax for insert_and_dedup!()

--- src/core.jl
+++ src/core.jl
@@ -1,7 +1,8 @@
 abstract type AbstractPathState end
 # modified from http://stackoverflow.com/questions/25678112/insert-item-into-a-sorted-list-with-julia-with-and-without-duplicates
 # returns true if insert succeeded, false if it was a duplicate
-_insert_and_dedup!{T<:Integer}(v::Vector{T}, x::T) = isempty(splice!(v, searchsorted(v,x), x))
+_insert_and_dedup!(v::Vector{T}, x::T) where T <: Integer =
+    isempty(splice!(v, searchsorted(v, x), x))
 
 """
     is_ordered(e)
