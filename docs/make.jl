e5f34fc60843bf140fb58ec5b95fa626b7f5fbc9
Seth Bromberger <github@bromberger.com>
2017-07-20 17:59:08 -0700

    switch to new documentation system

--- docs/make.jl
+++ docs/make.jl
@@ -9,13 +9,38 @@ cp(normpath(@__FILE__, "../../README.md"), normpath(@__FILE__, "../src/index.md"
 cp(normpath(@__FILE__, "../../CONTRIBUTING.md"), normpath(@__FILE__, "../src/contributing.md"); remove_destination=true)
 cp(normpath(@__FILE__, "../../LICENSE.md"), normpath(@__FILE__, "../src/license.md"); remove_destination=true)
 
-makedocs(modules=[LightGraphs], doctest = false)
-
+makedocs(
+    modules     = [LightGraphs],
+    format      = :html,
+    sitename    = "LightGraphs",
+    doctest     = false,
+    pages       = Any[
+        "Getting Started"	            => "index.md",
+        "Basic Functions"	            => "basicmeasures.md",
+        "Operators"	                    => "operators.md",
+        "Path and Traversal"	            => "pathing.md",
+        "Distance"	                    => "distance.md",
+        "Centrality Measures"	            => "centrality.md",
+        "Linear Algebra"	            => "linalg.md",
+        "Matching"	                    => "matching.md",
+        "Community Structures"	            => "community.md",
+        "Degeneracy"	                    => "degeneracy.md",
+        "Flow and Cut"	                    => "flowcut.md",
+        "Graph Generators"	            => "generators.md",
+        "Reading / Writing Graphs"	    => "persistence.md",
+        "Integration with other packages"   => "integration.md",
+        "Contributing"	                    => "contributing.md",
+        "License Information"	            => "license.md"
+    ]
+)
 
 deploydocs(
-    deps = Deps.pip("pygments", "mkdocs", "mkdocs-material", "python-markdown-math"),
-    repo   = "github.com/JuliaGraphs/LightGraphs.jl.git",
-    julia  = "0.6"
+    deps        = nothing,
+    make        = nothing,
+    repo        = "github.com/JuliaGraphs/LightGraphs.jl.git",
+    target      = "build",
+    julia       = "0.6",
+    osname      = "linux"
 )
 
 rm(normpath(@__FILE__, "../src/index.md"))
