7c6bd7eaa90459e6519eb22ca77d85bb66befa88
Seth Bromberger <sbromberger@users.noreply.github.com>
2017-07-20 19:33:11 -0700

    Sbromberger/eigdocs (#696)
    * switch to new documentation system
    
    * doc fix

--- src/centrality/eigenvector.jl
+++ src/centrality/eigenvector.jl
@@ -1,17 +1,20 @@
-@doc_str """
+"""
     eigenvector_centrality(g)
 
 Compute the eigenvector centrality for the graph `g`.
 
 Eigenvector centrality computes the centrality for a node based on the
 centrality of its neighbors. The eigenvector centrality for node `i` is
+the \$i^{th}\$ element of \$\\mathbf{x}\$ in the equation
+``
+    \\mathbf{Ax} = λ \\mathbf{x}
+``
+where \$\\mathbf{A}\$ is the adjacency matrix of the graph `g` 
+with eigenvalue λ.
 
-    ``\\mathbf{Ax} = λ \\mathbf{x}``
-
-where `A` is the adjacency matrix of the graph `g` with eigenvalue `λ`.
 By virtue of the Perron–Frobenius theorem, there is a unique and positive
-solution if `λ` is the largest eigenvalue associated with the
-eigenvector of the adjacency matrix `A`.
+solution if λ is the largest eigenvalue associated with the
+eigenvector of the adjacency matrix \$\\mathbf{A}\$.
 
 ### References
 
