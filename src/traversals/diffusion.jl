5112d6970d9c15c365d49875ae85c79584739a36
Nick Eubank <nickeubank@gmail.com>
2017-07-25 11:03:00 -0600

    updated re sbromberger advice

--- src/traversals/diffusion.jl
+++ src/traversals/diffusion.jl
@@ -1,99 +1,82 @@
+"""
+    diffusion(g, p, n)
 
-struct Diffusion{T}
-  reached::Set{T}
-  newly_reached::Set{T}
-  to_watch::Set{T}
-  p::Float64
-  normalize_p::Bool
+Run diffusion simulation on `g` for `n` steps with spread
+probabilities based on `p`. Return a vector with the set of vertices
+reached at each step of the simulation.
+
+### Optional Arguments
+- `initial_infections=sample(vertices(g), 1)`: A list of vertices that
+are infected at the start of the simulation.
+- `watch=vertices(g)`: While simulation is always run on the full graph,
+specifying `watch` limits reporting to a specific set of vertices reached
+during the simulation.
+- `normalize=false`: if `false`, set the probability of spread from a vertex ``i`` to
+each of the out_neighbors of ``i`` to ``p``. If `true`, set the probability of spread
+from a vertex ``i`` to each of the `out_neighbors` of ``i`` to ``\\frac{p}{out_degreee(g, i)}``.
+"""
+
+function diffusion(g::AbstractGraph, p, n;
+    initial_infections=LightGraphs.sample(vertices(g), 1),
+    watch=vertices(g),
+    normalize::Bool=false
+    )
+
+    # Initialize
+    T = eltype(g)
+    watch_set = Set{T}(watch)
+    new_infections = Set{T}()
+    infected_vertices = IntSet(initial_infections)
+    vertices_per_step = [Vector{T}() for i in 1:n]
+
+    # Record initial infection
+    vertices_per_step[1] = initial_infections
+
+    # Run simulation
+    for step in 2:n
+        new_infections = Set{T}()
+        for i in infected_vertices
+            infect_neighbors(g, i, p, new_infections)
+        end
+
+        # Record only new infections
+        setdiff!(new_infections, infected_vertices)
+        vertices_per_step[step] = T.(collect(intersect(new_infections, watch_set)))
+
+        # Add new to master set of infected
+        union!(infected_vertices, new_infections)
+    end
+
+    return vertices_per_step
+end
+
+function infect_neighbors(g, i, p, new_infections)
+    if normalize
+        local_p = p / out_degree(g, i)
+    else
+        local_p = p
+    end
+
+    union!(new_infections, randsubseq(out_neighbors(g, i), local_p))
 end
 
 """
-
-    diffusion_simulation(g, p, num_steps;
-                         to_watch=Set(vertices(g))
-                         initial_at_risk=Set(vertices(g)),
-                         normalize_p=false
-                         )
-
-### Implementation Notes
-
-Runs diffusion simulation on `g` for `num_steps` with spread
-probabilities based on `p`.
-
-Returns an Array with number of vertices reached at each step of
-simulation.
-
-While simulation is always run on full graph, specifying `to_watch`
-allows for reporting of the number of vertices reached within
-a subpopulation.
-
-If `normalized_p` is `false`, the probability of spread from a vertex i to
-each of the out_neighbors of `i` is `p`.
-
-If `normalized_p` is `true`, the probability of spread from a vertex `i` to
-each of the out_neighbors of `i` is `p / degree(g, i)`.
-
+    diffusion_rate(results)
+    diffusion_rate(g, p, n; ...)
+Given the results of a `diffusion` output or the parameters
+to the `diffusion` simulation itself, (run and) return the rate of
+diffusion as a vector representing the cumulative number of vertices
+infected at each simulation step, restricted to vertices included
+in `watch`, if specified.
 """
-
-function diffusion_simulation(g::AbstractGraph,
-                              p::Float64,
-                              num_steps::Int64;
-                              to_watch::Set=Set(vertices(g)),
-                              initial_at_risk::Set=Set(vertices(g)),
-                              normalize_p::Bool=false)
-
-  # Check set types
-  graph_vertex_type = eltype(vertices(g))
-
-  to_watch::Set{T} where T <: graph_vertex_type
-  initial_at_risk::Set{T} where T <: graph_vertex_type
-
-  simulation = Diffusion{graph_vertex_type}(
-                         Set{graph_vertex_type}(),
-                         Set{graph_vertex_type}(),
-                         to_watch,
-                         p,
-                         normalize_p)
-
-  vertices_reached = zeros(Int64, num_steps)
-
-  # Initiate
-  initial_infection(simulation, initial_at_risk)
-
-  # Run for num_steps
-  for step in 1:num_steps
-    for i in simulation.reached
-      infect_neighbors(g, simulation, i)
-    end
-
-    # flip state vectors
-    union!(simulation.reached, simulation.newly_reached)
-
-    # Get infection rate
-    vertices_reached[step] = length( intersect(simulation.reached, simulation.to_watch) )
-  end
-
-  return vertices_reached
-
-end
-
-function initial_infection(simulation::Diffusion, initial_at_risk::Set)
-  i = rand(initial_at_risk)
-  push!(simulation.reached, i)
-end
-
-function infect_neighbors(g::AbstractGraph, simulation::Diffusion, i)
-
-  if simulation.normalize_p
-    p = simulation.p / degree(g, i)
-  else
-    p= simulation.p
-  end
-
-  for n in out_neighbors(g, i)
-    if rand(Float64) < p
-      push!(simulation.newly_reached, n)
-    end
-  end
-
-end
+diffusion_rate(x::Vector{Vector{T}}) where T <: Integer = cumsum(length.(x))
+diffusion_rate(g::AbstractGraph, p, n;
+    initial_infections=LightGraphs.sample(vertices(g), 1),
+    watch=vertices(g),
+    normalize::Bool=false
+    ) = diffusion_rate(
+           diffusion(g, p, n,
+           initial_infections=initial_infections,
+           watch=watch, normalize=normalize
+           )
+)
