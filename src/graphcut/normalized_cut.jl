aba678c88a93821c7038c63e1df31bf066787d16
tejus-gupta <tejusgupta14@gmail.com>
2017-08-31 19:17:38 +0530

    requested changes

--- src/graphcut/normalized_cut.jl
+++ src/graphcut/normalized_cut.jl
@@ -1,65 +1,130 @@
-"""
-```
-labels                = normalized_cut(W, [num_cuts], [thres])
-```
-
-Performs recursive two-way normalized graph cut as described in this paper - "Normalized Cuts and Image Segmentation" by Shi and Malik
-
-Parameters:  
--    W              = Weighted adjacency matrix for graph.
--    thres          = Subgraphs aren't split if best normalized cut is above this threshold
--    num_cuts       = Number of cuts performed to determine optimal cut
-
-# Example
-```julia
-using LightGraphs, SimpleWeightedGraphs
-g = SimpleWeightedGraph(6)
-add_edge!(g, 1, 2, 1)
-add_edge!(g, 2, 3, 1)
-add_edge!(g, 1, 3, 1)
-add_edge!(g, 4, 5, 1)
-add_edge!(g, 4, 6, 1)
-add_edge!(g, 5, 6, 1)
-add_edge!(g, 1, 6, 0.1)
-add_edge!(g, 3, 4, 0.2)
-
-labels = normalized_cut(g.weights, 0.1)
-```
-"""
-function ncut{T<:Real}(W::SparseMatrixCSC{T,Int}, thres::Real = 0.01 ,num_cuts::Int = 10)
-
-    #returns normalized cut score
-    function ncut_cost(mask)
-        cut_cost = 0
-        rows = rowvals(W)
-        vals = nonzeros(W)
-        for i = 1:n
-            for j in nzrange(W, i)
-                row = rows[j]
-                if mask[i] != mask[row]
-                    cut_cost += vals[j]/2
-                end
+#computes normalized cut cost for partition `cut`
+function _normalized_cut_cost(cut, W::AbstractMatrix, D)
+    cut_cost = 0
+    for j in indices(W, 2)
+        for i in indices(W, 1)
+            if cut[i] != cut[j]
+                cut_cost += W[i, j]
             end
         end
-        return cut_cost/sum(D*mask) + cut_cost/sum(D*(.~mask))
+    end
+    cut_cost/=2
+    return cut_cost/sum(D*cut) + cut_cost/sum(D*(.~cut))
+end
+
+function _normalized_cut_cost(cut, W::SparseMatrixCSC, D)
+    cut_cost = 0
+    rows = rowvals(W)
+    vals = nonzeros(W)
+    n = size(W, 2)
+    for i = 1:n
+        for j in nzrange(W, i)
+            row = rows[j]
+            if cut[i] != cut[row]
+                cut_cost += vals[j]/2
+            end
+        end
+    end
+    return cut_cost/sum(D*cut) + cut_cost/sum(D*(.~cut))
+end
+
+function _partition_weightmx(cut, W::AbstractMatrix)
+    nv = length(cut)
+    nv1 = sum(cut)
+    nv2 = nv - nv1
+    newvid = Vector{Int}(nv)
+    vmap1 = Vector{Int}(nv1)
+    vmap2 = Vector{Int}(nv2)
+    j1 = 1
+    j2 = 1
+    for i in eachindex(cut)
+        if cut[i] == true
+            newvid[i] = j1
+            vmap1[j1] = i
+            j1+=1
+        else
+            newvid[i] = j2
+            vmap2[j2] = i
+            j2+=1
+        end
     end
 
+    W1 = similar(W, (nv1, nv1))
+    W2 = similar(W, (nv2, nv2))
+
+    for j in indices(W, 2)
+        for i in indices(W, 1)
+            if cut[i] == cut[j] == true
+                W1[newvid[i], newvid[j]] = W[i, j]
+            elseif cut[i] == cut[j] == false
+                W2[newvid[i], newvid[j]] = W[i, j]
+            end
+        end
+    end
+
+    return (W1, W2, vmap1, vmap2)
+end
+
+function _partition_weightmx(cut, W::SparseMatrixCSC)
+    nv = length(cut)
+    nv1 = sum(cut)
+    nv2 = nv - nv1
+    newvid = Vector{Int}(nv)
+    vmap1 = Vector{Int}(nv1)
+    vmap2 = Vector{Int}(nv2)
+    j1 = 1
+    j2 = 1
+    for i in eachindex(cut)
+        if cut[i] == true
+            newvid[i] = j1
+            vmap1[j1] = i
+            j1+=1
+        else
+            newvid[i] = j2
+            vmap2[j2] = i
+            j2+=1
+        end
+    end
+
+    rows = rowvals(W)
+    vals = nonzeros(W)
+    I1 = Vector{Int}(); I2 = Vector{Int}()
+    J1 = Vector{Int}(); J2 = Vector{Int}()
+    V1 = Vector{Float64}(); V2 = Vector{Float64}()
+    for i = 1:nv
+        for j in nzrange(W, i)
+            row = rows[j]
+            if cut[i] == cut[row] == false
+                push!(I1, newvid[i])
+                push!(J1, newvid[row])
+                push!(V1, vals[j])
+            elseif cut[i] == cut[row] == true
+                push!(I2, newvid[i])
+                push!(J2, newvid[row])
+                push!(V2, vals[j])
+            end
+        end
+    end
+    W1 = sparse(I1, J1, V1)
+    W2 = sparse(I2, J2, V2)
+    return (W1, W2, vmap1, vmap2)
+end
+
+function _recursive_normalized_cut(W, thres=thres, num_cuts=num_cuts)
     m, n = size(W)
     D = Diagonal(vec(sum(W, 2)))
 
-    if m == 1
-        return [1]
-    end
+    m == 1 && return [1]
 
     #get eigenvector corresponding to second smallest eigenvalue
-    v = eigvecs(full(D-W), full(D))[:, 2]
+    v = eigs(D-W, D, nev=2, which=:SR)[2][:,2]
 
     #perform n-cuts with different partitions of v and find best one
     min_cost = Inf
     best_thres = -1
     for t in linspace(minimum(v), maximum(v), num_cuts)
-        mask = v.>t
-        cost = ncut_cost(mask)
+        cut = v.>t
+        cost = _normalized_cut_cost(cut, W, D)
         if cost < min_cost
             min_cost = cost
             best_thres = t
@@ -67,66 +132,53 @@ function ncut{T<:Real}(W::SparseMatrixCSC{T,Int}, thres::Real = 0.01 ,num_cuts::
     end
 
     if min_cost < thres
-        #split graph, compute ncut for each subgraph recursively and merge index labels
-        mask = v.>best_thres
+        #split graph, compute normalized_cut for each subgraph recursively and merge indices.
+        cut = v.>best_thres
 
-        if sum(mask) == 0 || sum(mask) == m
+        if sum(cut) == 0 || sum(cut) == m
             return ones(Int, m)
         end
 
-        inds = Vector{Int}(m)
-        rev_inds1 = Vector{Int}(m - sum(mask))
-        rev_inds2 = Vector{Int}(sum(mask))
-        i1 = 1
-        i2 = 1
-        for j in 1:m
-            if !mask[j]
-                inds[j] = i1
-                rev_inds1[i1] = j
-                i1 += 1
-            else
-                inds[j] = i2
-                rev_inds2[i2] = j
-                i2 += 1
-            end
-        end
-
-        rows = rowvals(W)
-        vals = nonzeros(W)
-        I1 = Vector{Int}(); I2 = Vector{Int}()
-        J1 = Vector{Int}(); J2 = Vector{Int}()
-        V1 = Vector{Float64}(); V2 = Vector{Float64}()
-        for i = 1:n
-            for j in nzrange(W, i)
-                row = rows[j]
-                if mask[i] == mask[row] == false
-                    push!(I1, inds[i])
-                    push!(J1, inds[row])
-                    push!(V1, vals[j])
-                elseif mask[i] == mask[row] == true
-                    push!(I2, inds[i])
-                    push!(J2, inds[row])
-                    push!(V2, vals[j])
-                end
-            end
-        end
-        W1 = sparse(I1, J1, V1)
-        W2 = sparse(I2, J2, V2)
-        labels1 = ncut(W1, thres, num_cuts)
-        labels2 = ncut(W2, thres, num_cuts)
+        W1, W2, vmap1, vmap2 = _partition_weightmx(cut, W)
+        labels1 = _recursive_normalized_cut(W1, thres, num_cuts)
+        labels2 = _recursive_normalized_cut(W2, thres, num_cuts)
 
         labels = Vector{Int}(m)
-        n1 = maximum(labels1)
+        offset = maximum(labels1)
 
-        for i in 1:(m - sum(mask))
-            labels[rev_inds1[i]] = labels1[i]
+        for i in eachindex(labels1)
+            labels[vmap1[i]] = labels1[i]
         end
-        for i in 1:sum(mask)
-            labels[rev_inds2[i]] = labels2[i] + n1
+        for i in eachindex(labels2)
+            labels[vmap2[i]] = labels2[i] + offset
         end
+
         return labels
     else
-        #don't cut graph further
         return ones(Int, m)
     end
+end
+
+"""
+    normalized_cut(g, distmx=weights(g), [thres=0.01], [num_cuts=10]);
+
+Perform [recursive two-way normalized graph-cut](https://en.wikipedia.org/wiki/Segmentation-based_object_categorization#Normalized_cuts)
+on a graph, partitioning the vertices into disjoint sets.
+Return a vector that contains the set index for each vertex.
+
+### Keyword Arguments
+- `thres`: Subgraphs aren't split if best normalized cut is above this threshold
+- `num_cuts`: Number of cuts performed to determine optimal cut
+
+### References
+"Normalized Cuts and Image Segmentation" - Jianbo Shi and Jitendra Malik
+"""
+function normalized_cut(
+    g::AbstractGraph,
+    W::AbstractMatrix{T}=weights(g);
+    thres = 0.01,
+    num_cuts::Int = 10
+    ) where T <: Real
+
+    return _recursive_normalized_cut(W, thres, num_cuts)
 end
--- test/graphcut/normalized_cut.jl
+++ test/graphcut/normalized_cut.jl
@@ -1,24 +1,61 @@
 @testset "Normalized Cut" begin
 
-    g = SimpleWeightedGraph(4)
-    add_edge!(g, 1, 2, 1)
-    add_edge!(g, 3, 4, 1)
+    gx = Graph(6)
+    add_edge!(gx, 1, 2)
+    add_edge!(gx, 2, 3)
+    add_edge!(gx, 1, 3)
+    add_edge!(gx, 4, 5)
+    add_edge!(gx, 4, 6)
+    add_edge!(gx, 5, 6)
+    add_edge!(gx, 1, 6)
+    add_edge!(gx, 3, 4)
 
-    labels = ncut(g.weights, 0.1)
+    w = zeros(6, 6)
+    w[2, 1] = 1.0
+    w[3, 1] = 1.0
+    w[6, 1] = 0.1
+    w[1, 2] = 1.0
+    w[3, 2] = 1.0
+    w[1, 3] = 1.0
+    w[2, 3] = 1.0
+    w[4, 3] = 0.2
+    w[3, 4] = 0.2
+    w[5, 4] = 1.0
+    w[6, 4] = 1.0
+    w[4, 5] = 1.0
+    w[6, 5] = 1.0
+    w[1, 6] = 0.1
+    w[4, 6] = 1.0
+    w[5, 6] = 1.0
 
-    @test labels == [1, 1, 2, 2] || labels == [2, 2, 1, 1]
+    for g in testgraphs(gx)
+      labels = @inferred(normalized_cut(g, w, thres=1))
+      @test labels == [1, 1, 1, 2, 2, 2] || labels == [2, 2, 2, 1, 1, 1]
+    end
 
-    g = SimpleWeightedGraph(6)
-    add_edge!(g, 1, 2, 1)
-    add_edge!(g, 2, 3, 1)
-    add_edge!(g, 1, 3, 1)
-    add_edge!(g, 4, 5, 1)
-    add_edge!(g, 4, 6, 1)
-    add_edge!(g, 5, 6, 1)
-    add_edge!(g, 1, 6, 0.1)
-    add_edge!(g, 3, 4, 0.2)
+    w = SparseMatrixCSC(w)
+    for g in testgraphs(gx)
+      labels = @inferred(normalized_cut(g, w, thres=1))
+      @test labels == [1, 1, 1, 2, 2, 2] || labels == [2, 2, 2, 1, 1, 1]
+    end
 
-    labels = ncut(g.weights, 0.1)
+    gx = Graph(4)
+    add_edge!(gx, 1, 2)
+    add_edge!(gx, 3, 4)
 
-    @test labels == [1, 1, 1, 2, 2, 2] || labels == [2, 2, 2, 1, 1, 1]
+    w = zeros(4, 4)
+    w[2, 1] = 1.0
+    w[1, 2] = 1.0
+    w[4, 3] = 1.0
+    w[3, 4] = 1.0
+    for g in testgraphs(gx)
+      labels = @inferred(normalized_cut(g, w, thres=0.1))
+      @test labels == [1, 1, 2, 2] || labels == [2, 2, 1, 1]
+    end
+
+    w = SparseMatrixCSC(w)
+    for g in testgraphs(gx)
+      labels = @inferred(normalized_cut(g, w, thres=0.1))
+      @test labels == [1, 1, 2, 2] || labels == [2, 2, 1, 1]
+    end
 end
