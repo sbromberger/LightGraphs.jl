5887833142b0e14b196f7f16000cbba23b7816c6
Mr Su <suzhu1988@163.com>
2018-01-19 16:35:54 +0800

    add clique_percolation.jl
    clique percolation is a method of uncovering the overlapping community structure of complex networks in nature and society

---
+++ src/community/clique_percolation.jl
@@ -0,0 +1,36 @@
+using LightGraphs
+
+function clique_percolation(g::SimpleGraph; k=3)
+  kcliques = filter(x->length(x)>=k, maximal_cliques(g))
+  nc = length(kcliques)
+  # graph with nodes represent k-cliques
+  h = Graph(nc)
+  # vector for counting common nodes between two cliques efficiently
+  x = fill(false, nv(g))
+  for i = 1:nc
+    for u in kcliques[i]
+      x[u] = true
+    end
+    for j = i+1:nc
+        common_nodes = 0
+        for v in kcliques[j]
+            if x[v]
+                common_nodes += 1
+            end
+        end
+        if common_nodes >= k-1
+            add_edge!(h, i, j)
+        end
+    end
+    # reset status
+    for u in kcliques[i]
+      x[u] = false
+    end
+  end
+  components = connected_components(h)
+  communities = [IntSet() for i=1:length(components)]
+  for (i,component) in enumerate(components), u in component, v in kcliques[u]
+    push!(communities[i], v)
+   end
+   communities
+end
