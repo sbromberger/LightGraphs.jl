d5550cd40f2fdd8d4811c2b3883273aeb1c1cb97
Seth Bromberger <sbromberger@users.noreply.github.com>
2018-03-07 20:47:05 -0800

    remove add/remove vertex/edge from core, minor bug fix (#862)
    * remove add/remove vertex/edge from core, minor bug fix
    
    * fix tests
    
    * export add/rem vertex

--- src/LightGraphs.jl
+++ src/LightGraphs.jl
@@ -28,7 +28,7 @@ export
 # Interface
 AbstractGraph, AbstractEdge, AbstractEdgeIter,
 Edge, Graph, SimpleGraph, DiGraph, SimpleDiGraph, vertices, edges, edgetype, nv, ne, src, dst,
-is_directed, add_vertex!, add_edge!, rem_vertex!, rem_edge!,
+is_directed,
 has_vertex, has_edge, inneighbors, outneighbors,
 
 # core
@@ -36,6 +36,9 @@ is_ordered, add_vertices!, indegree, outdegree, degree,
 Δout, Δin, δout, δin, Δ, δ, degree_histogram,
 neighbors, all_neighbors, common_neighbors,
 has_self_loops, num_self_loops, density, squash, weights,
+
+# simplegraphs
+add_edge!, add_vertex!, add_vertices!, rem_edge!, rem_vertex!,
 
 # decomposition
 core_number, k_core, k_shell, k_crust, k_corona,
