2bc6d6e4b2e2be590cf92bbca4f57775e11a4a7a
Simon Schoelly <>
2018-04-25 19:09:11 +0200

    Fixed some tests && added testdigraphs for all tests

--- test/digraph/transitivity.jl
+++ test/digraph/transitivity.jl
@@ -30,61 +30,76 @@
 
     # transitivereduction
     let
+        # transitive reduction of the nullgraph is again a nullgraph
         nullgraph = SimpleDiGraph(0)
-        pathgraph = SimpleDiGraph(10)
-        pathgraph_closure = transitiveclosure(pathgraph)
-        complete_graph = CompleteDiGraph(9)
-        complete_graph_reduced = transitivereduction(complete_graph)
-        no_edge_graph = SimpleDiGraph(7)
-        self_loop_graph = SimpleDiGraph(1)
-        add_edge!(self_loop_graph, 1, 1)
+        for g in testdigraphs(nullgraph)
+            @test g == @inferred(transitivereduction(g))
+        end
         
-        barbell_graph = SimpleDiGraph(9)
-        for i in 1:4, j in 1:4
-            i == j && continue
-            add_edge!(barbell_graph, i, j)
-        end
-        barbell_graph = SimpleDiGraph(9)
-        for i in 1:4, j in 1:4
-            i == j && continue
-            add_edge!(barbell_graph, i, j)
-        end
-        for i in 5:9, j in 5:9
-            i == j && continue
-            add_edge!(barbell_graph, i, j)
-        end
-        add_edge!(barbell_graph, 1,5);
-
-        # transitive reduction of a the nullgraph is again a nullgraph
-        @test nullgraph == transitivereduction(nullgraph)
-
         # transitive reduction of a path is a path again
-        @test pathgraph == transitivereduction(pathgraph)
-
-        # transitive reduction of a the transtitve closure of a path is the same path again
-        @test pathgraph == transitivereduction(pathgraph_closure)
-        for pathgraph2 in testdigraphs(pathgraph)
-            pathgraph_reduced = @inferred(transitivereduction(pathgraph2))
-            @test pathgraph2 == pathgraph_reduced
+        pathgraph = PathDiGraph(10)
+        for g in testdigraphs(pathgraph)
+            @test g == @inferred(transitivereduction(g))
+        end
+        
+        # transitive reduction of the transitive closure of a path is a path again
+        for g in testdigraphs(pathgraph)
+            gclosure = transitiveclosure(g)
+            @test g == @inferred(transitivereduction(gclosure))
         end
 
         # Transitive reduction of a complete graph should be s simple cycle
-        @test length(strongly_connected_components(complete_graph_reduced)) == 1 &&
-            ne(complete_graph_reduced) == nv(complete_graph)
+        completegraph = CompleteDiGraph(9)
+        for g in testdigraphs(completegraph)
+            greduced = @inferred(transitivereduction(g))
+            @test length(strongly_connected_components(greduced)) == 1 &&
+                ne(greduced) == nv(g)
+        end
 
         # transitive reduction a graph with no edges is the same graph again
-        @test no_edge_graph == transitivereduction(no_edge_graph)
-
+        noedgegraph = SimpleDiGraph(7)
+        for g in testdigraphs(noedgegraph)
+            @test g == @inferred(transitivereduction(g))
+        end
+         
         # transitve reduction should maintain a selfloop only when selflooped==true
-        @test self_loop_graph == transitivereduction(self_loop_graph; selflooped=true)
-        @test self_loop_graph != transitivereduction(self_loop_graph; selflooped=false)
+        selfloopgraph = SimpleDiGraph(1)
+        add_edge!(selfloopgraph, 1, 1)
+        for g in testdigraphs(selfloopgraph)
+            @test g == @inferred(transitivereduction(g; selflooped=true));
+            @test g != @inferred(transitivereduction(g; selflooped=false));
+        end
 
-        # directed barbell should result in two cycles connected by a single edge
-        barbell_graph_reduced = transitivereduction(barbell_graph)
-        scc = strongly_connected_components(barbell_graph_reduced)
-        @test Set(scc) == Set([[1:4;], [5:9;]])
-        @test ne(barbell_graph_reduced) == 10
-        @test length(weakly_connected_components(barbell_graph_reduced)) == 1
+        # transitive should not maintain selfloops for strongly connected components
+        # of size > 1
+        selfloopgraph2 = SimpleDiGraph(2)
+        add_edge!(selfloopgraph2, 1, 1)
+        add_edge!(selfloopgraph2, 1, 2)
+        add_edge!(selfloopgraph2, 2, 1)
+        add_edge!(selfloopgraph2, 2, 2)
+        for g in testdigraphs(selfloopgraph2)
+            @test g != @inferred(transitivereduction(g; selflooped=true));
+        end
+
+        # directed barbell graph should result in two cycles connected by a single edge
+        barbellgraph = SimpleDiGraph(9)
+        for i in 1:4, j in 1:4
+            i == j && continue
+            add_edge!(barbellgraph, i, j)
+            add_edge!(barbellgraph, j, i)
+        end
+        for i in 5:9, j in 5:9
+            i == j && continue
+            add_edge!(barbellgraph, i, j)
+            add_edge!(barbellgraph, j, i)
+        end
+        add_edge!(barbellgraph, 1, 5);
+        for g in testdigraphs(barbellgraph)
+            greduced = @inferred(transitivereduction(g))
+            scc = strongly_connected_components(greduced)
+            @test Set(scc) == Set([[1:4;], [5:9;]])
+            @test ne(greduced) == 10
+            @test length(weakly_connected_components(greduced)) == 1
+        end
     end
-
 end
