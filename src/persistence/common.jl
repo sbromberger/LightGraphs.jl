70382b08cc2975b9fe6d29c09b9b112c620ff03e
Soham Tamba <sohamtamba@gmail.com>
2018-06-04 20:53:51 +0530

    Improve Kruskal and use in-built disjoint set data structure (#896)
    * Improve Kruskal and use in-built disjoint set data structure
    
    * Update kruskal.jl
    
    Changes requested by @somil55

--- src/persistence/common.jl
+++ src/persistence/common.jl
@@ -84,8 +84,13 @@ function savegraph(fn::AbstractString, g::AbstractGraph, gname::AbstractString,
     end
 end
 
+# without graph name
 savegraph(fn::AbstractString, g::AbstractGraph, format::AbstractGraphFormat; compress=true) =
     savegraph(fn, g, "graph", format, compress=compress)
+
+# without format - default to LGFormat()
+savegraph(fn::AbstractString, g::AbstractSimpleGraph; compress=true) = savegraph(fn, g, "graph", LGFormat(), compress=compress)
+
 """
     savegraph(file, g, d, format=LGFormat; compress=true)
 
