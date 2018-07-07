59a70de2c288cf9be73f78d1ecbe73e88d941df8
SohamTamba <sohamtamba@gmail.com>
2018-07-07 08:05:05 +0530

    Tests

--- test/independentset/random_maximal_ind_set.jl
+++ test/independentset/random_maximal_ind_set.jl
@@ -4,17 +4,27 @@
     for g in testgraphs(g3)
         z = @inferred(random_maximal_independent_set(g))
         @test (length(z)== 1 || length(z)== 4)
+
+        z = @inferred(parallel_random_maximal_independent_set(g, 3))
+        @test (length(z)== 1 || length(z)== 4)
     end
     
     g4 = CompleteGraph(5)
     for g in testgraphs(g4)
         z = @inferred(random_maximal_independent_set(g))
         @test length(z)== 1 #Exactly one vertex 
+
+        z = @inferred(parallel_random_maximal_independent_set(g, 4))
+        @test length(z)== 1 
     end
 
     g5 = PathGraph(5)
     for g in testgraphs(g5)
         z = @inferred(random_maximal_independent_set(g))
+        sort!(z)
+        @test (z == [2, 4] || z == [2, 5] || z == [1, 3, 5] || z == [1, 4])
+
+        z = @inferred(parallel_random_maximal_independent_set(g, 5))
         sort!(z)
         @test (z == [2, 4] || z == [2, 5] || z == [1, 3, 5] || z == [1, 4])
     end
--- test/traversals/greedy_color.jl
+++ test/traversals/greedy_color.jl
@@ -7,6 +7,11 @@
             C = @inferred(greedy_color(g, exchange=op_exchange, sort_degree=op_sort))
             @test C.num_colors == 2
         end
+
+        for op_exchange in (true, false)
+            C = @inferred(parallel_random_greedy_color(g, 3, exchange=op_exchange))
+            @test C.num_colors == 2
+        end
     end
     
     g4 = PathGraph(20)
@@ -22,20 +27,30 @@
 
             @test correct
         end
-    end
-    
-    g5 = CompleteGraph(20)
-    for g in testgraphs(g5)
-        for op_exchange in (true, false), op_sort in (true, false)
-            C = @inferred(greedy_color(g, exchange=op_exchange, sort_degree=op_sort))
+
+        for op_exchange in (true, false)
+            C = @inferred(parallel_random_greedy_color(g, 4, exchange=op_exchange))
         
-            @test C.num_colors <= maximum(degree(g))+1
+            @test C.num_colors <= maximum(degree(g))+1 #Propert of greedy coloring
             correct = true
             for e in edges(g) #Condition for valid coloring
                 C.colors[src(e)] == C.colors[dst(e)] && (correct = false)
             end
 
             @test correct
+        end
+    end
+    
+    g5 = CompleteGraph(20)
+    for g in testgraphs(g5)
+        for op_exchange in (true, false), op_sort in (true, false)
+            C = @inferred(greedy_color(g, exchange=op_exchange, sort_degree=op_sort))
+            @test C.num_colors == 20
+        end
+
+        for op_exchange in (true, false)
+            C = @inferred(parallel_random_greedy_color(g, 5, exchange=op_exchange))
+            @test C.num_colors == 20
         end
     end
 
