0a24f19c9c2e09be8f75743f825be45b6678352d
SohamTamba <sohamtamba@gmail.com>
2018-07-19 06:52:58 +0530

    max_set min_set

--- test/utils.jl
+++ test/utils.jl
@@ -18,6 +18,26 @@
     end
 end
 
+@testset "Generate Reduce" begin
+
+    function make_vec(g::AbstractGraph{T}) where T<:Integer
+        return Vector{T}(undef, nv(g))
+    end
+
+    g1 = StarGraph(5)
+
+    for g in testgraphs(g1)
+        s = @inferred(LightGraphs.generate_max_set(g, make_vec, 5))
+        @test length(s) == 5
+    end
+
+    for g in testgraphs(g1)
+        s = @inferred(LightGraphs.generate_max_set(g, make_vec, 5))
+        @test length(s) == 5
+    end
+end
+
+
 @testset "Greedy Contiguous Partition" begin
 
     p = @inferred(LightGraphs.greedy_contiguous_partition([1, 1, 1, 3], 2))
