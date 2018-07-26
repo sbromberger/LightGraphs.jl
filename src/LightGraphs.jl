387f6c2fb31ae7fa73c4f2852d2869e1abf9f25b
James Fairbanks <james.fairbanks@gtri.gatech.edu>
2018-07-26 11:52:20 -0400

    add Statistics: mean to imports

--- src/LightGraphs.jl
+++ src/LightGraphs.jl
@@ -5,6 +5,7 @@ using SimpleTraits
 
 ### Remove the following line once #915 is closed
 using Arpack: eigs
+using Statistics: mean
 
 using CodecZlib: GzipCompressorStream, GzipDecompressorStream
 using DataStructures: IntDisjointSets, PriorityQueue, dequeue!, dequeue_pair!, enqueue!, heappop!, heappush!, in_same_set, peek, union!
