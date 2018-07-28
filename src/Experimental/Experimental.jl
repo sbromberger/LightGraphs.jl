ec89289775d062eba3a04e5bae64b66112ba84ad
Simon Schoelly <sischoel@gmail.com>
2018-07-28 05:14:04 +0100

    tried to rebase

--- src/Experimental/Experimental.jl
+++ src/Experimental/Experimental.jl
@@ -1,7 +1,17 @@
 module Experimental
 
-export description
+using LightGraphs
+using LightGraphs.SimpleGraphs
+
+export description,
+    #isomorphism
+    vf2, IsomorphismProblem, SubgraphIsomorphismProblem, InducedSubgraphIsomorphismProblem,
+    has_iso, all_iso, count_iso,
+    has_induced_subgraphiso, count_induced_subgraphiso, all_induced_subgraphiso,
+    has_subgraphiso, count_subgraphiso, all_subgraphiso
 
 description() = "This module contains experimental graph functions."
+
+include("isomorphism.jl")
 
 end
