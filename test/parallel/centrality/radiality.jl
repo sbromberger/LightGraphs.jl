f05af8db8616119ca30c1d22e433c75663c6916f
SohamTamba <sohamtamba@gmail.com>
2018-08-29 01:35:31 +0530

    MultiThreaded Centrality Measures Implementations

--- test/parallel/centrality/radiality.jl
+++ test/parallel/centrality/radiality.jl
@@ -2,15 +2,19 @@
     gint = loadgraph(joinpath(testdir, "testdata", "graph-50-500.jgz"), "graph-50-500")
     c = vec(readdlm(joinpath(testdir, "testdata", "graph-50-500-rc.txt"), ','))
     for g in testdigraphs(gint)
-        zp = @inferred(Parallel.radiality_centrality(g))
-        @test zp == c
+        zd = @inferred(Parallel.radiality_centrality(g; parallel=:distributed))
+        @test zd == c
+        zt = @inferred(Parallel.radiality_centrality(g; parallel=:threads))
+        @test zt == c
     end
 
     g1 = CycleGraph(4)
     add_vertex!(g1)
     add_edge!(g1, 4, 5)
     for g in testgraphs(g1)
-        zp = @inferred(Parallel.radiality_centrality(g))
-        @test zp ≈ [5 // 6, 3 // 4, 5 // 6, 11 // 12, 2 // 3]
+        zd = @inferred(Parallel.radiality_centrality(g; parallel=:distributed))
+        @test zd ≈ [5 // 6, 3 // 4, 5 // 6, 11 // 12, 2 // 3]
+        zt = @inferred(Parallel.radiality_centrality(g; parallel=:threads))
+        @test zt ≈ [5 // 6, 3 // 4, 5 // 6, 11 // 12, 2 // 3]
     end
 end
