7aa29a38ad2f218e443fbb45e8cb6982c25acecd
Katharine Hyatt <khyatt@flatironinstitute.org>
2018-09-18 15:46:31 -0400

    More examples...

--- src/core.jl
+++ src/core.jl
@@ -16,6 +16,17 @@ is_ordered(e::AbstractEdge) = src(e) <= dst(e)
     add_vertices!(g, n)
 Add `n` new vertices to the graph `g`.
 Return the number of vertices that were added successfully.
+
+# Examples
+```jldoctest
+julia> using LightGraphs
+
+julia> g = SimpleGraph()
+{0, 0} undirected simple Int64 graph
+
+julia> add_vertices!(g, 2)
+2
+```
 """
 add_vertices!(g::AbstractGraph, n::Integer) = sum([add_vertex!(g) for i = 1:n])
 
@@ -24,6 +35,25 @@ add_vertices!(g::AbstractGraph, n::Integer) = sum([add_vertex!(g) for i = 1:n])
 
 Return a vector corresponding to the number of edges which end at each vertex in
 graph `g`. If `v` is specified, only return degrees for vertices in `v`.
+
+# Examples
+```jldoctest
+julia> using LightGraphs
+
+julia> g = DiGraph();
+
+julia> add_vertices!(g, 3);
+
+julia> add_edge!(g, 2, 3);
+
+julia> add_edge!(g, 3, 1);
+
+julia> indegree(g)
+3-element Array{Int64,1}:
+ 1
+ 0
+ 1
+```
 """
 indegree(g::AbstractGraph, v::Integer) = length(inneighbors(g, v))
 indegree(g::AbstractGraph, v::AbstractVector = vertices(g)) = [indegree(g, x) for x in v]
@@ -33,6 +63,25 @@ indegree(g::AbstractGraph, v::AbstractVector = vertices(g)) = [indegree(g, x) fo
 
 Return a vector corresponding to the number of edges which start at each vertex in
 graph `g`. If `v` is specified, only return degrees for vertices in `v`.
+
+# Examples
+```jldoctest
+julia> using LightGraphs
+
+julia> g = DiGraph();
+
+julia> add_vertices!(g, 3);
+
+julia> add_edge!(g, 2, 3);
+
+julia> add_edge!(g, 3, 1);
+
+julia> outdegree(g)
+3-element Array{Int64,1}:
+ 0
+ 1
+ 1
+```
 """
 outdegree(g::AbstractGraph, v::Integer) = length(outneighbors(g, v))
 outdegree(g::AbstractGraph, v::AbstractVector = vertices(g)) = [outdegree(g, x) for x in v]
@@ -43,6 +92,25 @@ Return a vector corresponding to the number of edges which start or end at each
 vertex in graph `g`. If `v` is specified, only return degrees for vertices in `v`.
 For directed graphs, this value equals the incoming plus outgoing edges.
 For undirected graphs, it equals the connected edges.
+
+# Examples
+```jldoctest
+julia> using LightGraphs
+
+julia> g = DiGraph();
+
+julia> add_vertices!(g, 3);
+
+julia> add_edge!(g, 2, 3);
+
+julia> add_edge!(g, 3, 1);
+
+julia> degree(g)
+3-element Array{Int64,1}:
+ 1
+ 1
+ 2
+```
 """
 function degree end
 @traitfn degree(g::::IsDirected, v::Integer) = indegree(g, v) + outdegree(g, v)
@@ -109,10 +177,10 @@ end
 """
     degree_histogram(g, degfn=degree)
 
-Return a Dict with values representing the number of vertices that have degree
+Return a `Dict` with values representing the number of vertices that have degree
 represented by the key.
 
-Degree function (for example, `indegree` or `outdegree`) may be specified by
+Degree function (for example, [`indegree`](@ref) or [`outdegree`](@ref)) may be specified by
 overriding `degfn`.
 """
 function degree_histogram(g::AbstractGraph{T}, degfn=degree) where T
@@ -135,6 +203,30 @@ use [`all_neighbors`](@ref) to list inbound and outbound neighbors.
 
 ### Implementation Notes
 Returns a reference, not a copy. Do not modify result.
+
+# Examples
+```jldoctest
+julia> using LightGraphs
+
+julia> g = DiGraph();
+
+julia> add_vertices!(g, 3);
+
+julia> add_edge!(g, 2, 3);
+
+julia> add_edge!(g, 3, 1);
+
+julia> neighbors(g, 1)
+0-element Array{Int64,1}
+
+julia> neighbors(g, 2)
+1-element Array{Int64,1}:
+ 3
+
+julia> neighbors(g, 3)
+1-element Array{Int64,1}:
+ 1
+```
 """
 neighbors(g::AbstractGraph, v::Integer) = outneighbors(g, v)
 
@@ -146,6 +238,31 @@ and [`inneighbors`](@ref).
 
 ### Implementation Notes
 Returns a reference, not a copy. Do not modify result.
+
+# Examples
+```jldoctest
+julia> using LightGraphs
+
+julia> g = DiGraph();
+
+julia> add_vertices!(g, 3);
+
+julia> add_edge!(g, 2, 3);
+
+julia> add_edge!(g, 3, 1);
+
+julia> all_neighbors(g, 1)
+1-element Array{Int64,1}:
+ 3
+
+julia> all_neighbors(g, 2)
+1-element Array{Int64,1}:
+ 3
+
+julia> all_neighbors(g, 3)
+2-element Array{Int64,1}:
+ 1
+ 2
 """
 function all_neighbors end
 @traitfn all_neighbors(g::::IsDirected, v::Integer) =
@@ -161,6 +278,32 @@ Return the neighbors common to vertices `u` and `v` in `g`.
 
 ### Implementation Notes
 Returns a reference, not a copy. Do not modify result.
+
+# Examples
+```jldoctest
+julia> using LightGraphs
+
+julia> g = SimpleGraph(4);
+
+julia> add_edge!(g, 1, 2);
+
+julia> add_edge!(g, 2, 3);
+
+julia> add_edge!(g, 3, 4);
+
+julia> add_edge!(g, 4, 1);
+
+julia> add_edge!(g, 1, 3);
+
+julia> common_neighbors(g, 1, 3)
+2-element Array{Int64,1}:
+ 2
+ 4
+
+julia> common_neighbors(g, 1, 4)
+1-element Array{Int64,1}:
+ 3
+```
 """
 common_neighbors(g::AbstractGraph, u::Integer, v::Integer) =
     intersect(neighbors(g, u), neighbors(g, v))
@@ -169,6 +312,23 @@ common_neighbors(g::AbstractGraph, u::Integer, v::Integer) =
     has_self_loops(g)
 
 Return true if `g` has any self loops.
+
+# Examples
+```jldoctest
+julia> using LightGraphs
+
+julia> g = SimpleGraph(2);
+
+julia> add_edge!(g, 1, 2);
+
+julia> has_self_loops(g)
+false
+
+julia> add_edge!(g, 1, 1);
+
+julia> has_self_loops(g)
+true
+```
 """
 has_self_loops(g::AbstractGraph) = nv(g) == 0 ? false : any(v -> has_edge(g, v, v), vertices(g))
 
@@ -176,6 +336,23 @@ has_self_loops(g::AbstractGraph) = nv(g) == 0 ? false : any(v -> has_edge(g, v, 
     num_self_loops(g)
 
 Return the number of self loops in `g`.
+
+# Examples
+```jldoctest
+julia> using LightGraphs
+
+julia> g = SimpleGraph(2);
+
+julia> add_edge!(g, 1, 2);
+
+julia> num_self_loops(g)
+0
+
+julia> add_edge!(g, 1, 1);
+
+julia> num_self_loops(g)
+1
+```
 """
 num_self_loops(g::AbstractGraph) = nv(g) == 0 ? 0 : sum(v -> has_edge(g, v, v), vertices(g))
 
