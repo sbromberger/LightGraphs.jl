56c593fc5b274292799cbe7385814a78564a2921
Katharine Hyatt <kshyatt@users.noreply.github.com>
2018-09-19 17:12:57 -0400

    More examples... (#1014)
    * More examples...
    
    * Update core.jl
    
    shortcut to Digraph creation
    
    * Update bipartition.jl

--- src/traversals/bipartition.jl
+++ src/traversals/bipartition.jl
@@ -1,5 +1,5 @@
 """
-    bipartite_map(g)
+    bipartite_map(g) -> Vector{UInt8}
 
 For a bipartite graph `g`, return a vector `c` of size ``|V|`` containing
 the assignment of each vertex to one of the two sets (``c_i == 1`` or c_i == 2``).
@@ -8,6 +8,31 @@ If `g` is not bipartite, return an empty vector.
 ### Implementation Notes
 Note that an empty vector does not necessarily indicate non-bipartiteness.
 An empty graph will return an empty vector but is bipartite.
+
+# Examples
+```jldoctest
+julia> using LightGraphs
+
+julia> g = SimpleGraph(3);
+
+julia> bipartite_map(g)
+3-element Array{UInt8,1}:
+ 0x01
+ 0x01
+ 0x01
+
+julia> add_vertices!(g, 3);
+
+julia> add_edge!(g, 1, 2);
+
+julia> add_edge!(g, 2, 3);
+
+julia> bipartite_map(g)
+3-element Array{UInt8,1}:
+ 0x01
+ 0x02
+ 0x01
+```
 """
 function bipartite_map(g::AbstractGraph{T}) where T
     nvg = nv(g)
@@ -43,5 +68,24 @@ end
     is_bipartite(g)
 
 Return `true` if graph `g` is [bipartite](https://en.wikipedia.org/wiki/Bipartite_graph).
+
+# Examples
+```jldoctest
+julia> using LightGraphs
+
+julia> g = SimpleGraph(3);
+
+julia> add_edge!(g, 1, 2);
+
+julia> add_edge!(g, 2, 3);
+
+julia> is_bipartite(g)
+true
+
+julia> add_edge!(g, 1, 3);
+
+julia> is_bipartite(g)
+false
+```
 """
 is_bipartite(g::AbstractGraph) = length(bipartite_map(g)) == nv(g)
