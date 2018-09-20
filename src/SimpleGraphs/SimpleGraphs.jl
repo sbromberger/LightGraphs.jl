573b2fd6ee9382c0816d688ddf780bfb08ae74c4
Katharine Hyatt <kshyatt@users.noreply.github.com>
2018-09-20 18:32:53 -0400

    More doctests (#1016)

--- src/SimpleGraphs/SimpleGraphs.jl
+++ src/SimpleGraphs/SimpleGraphs.jl
@@ -38,10 +38,10 @@ export AbstractSimpleGraph, AbstractSimpleEdge,
     AbstractSimpleGraph
 
 An abstract type representing a simple graph structure.
-AbstractSimpleGraphs must have the following elements:
-- vertices::UnitRange{Integer}
-- fadjlist::Vector{Vector{Integer}}
-- ne::Integer
+`AbstractSimpleGraph`s must have the following elements:
+  - `vertices::UnitRange{Integer}`
+  - `fadjlist::Vector{Vector{Integer}}`
+  - `ne::Integer`
 """
 abstract type AbstractSimpleGraph{T<:Integer} <: AbstractGraph{T} end
 
@@ -88,8 +88,8 @@ end
 """
     rem_vertex!(g, v)
 
-Remove the vertex `v` from graph `g`. Return false if removal fails
-(e.g., if vertex is not in the graph); true otherwise.
+Remove the vertex `v` from graph `g`. Return `false` if removal fails
+(e.g., if vertex is not in the graph); `true` otherwise.
 
 ### Performance
 Time complexity is ``\\mathcal{O}(k^2)``, where ``k`` is the max of the degrees
@@ -101,6 +101,19 @@ data structures indexed by edges or vertices in the graph, since
 internally the removal is performed swapping the vertices `v`  and ``|V|``,
 and removing the last vertex ``|V|`` from the graph. After removal the
 vertices in `g` will be indexed by ``1:|V|-1``.
+
+# Examples
+```jldoctest
+julia> using LightGraphs
+
+julia> g = SimpleGraph(2);
+
+julia> rem_vertex!(g, 2)
+true
+
+julia> rem_vertex!(g, 2)
+false
+```
 """
 function rem_vertex!(g::AbstractSimpleGraph, v::Integer)
     v in vertices(g) || return false
