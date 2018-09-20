134f3433b74a3793836abfc424beb1690c525376
Katharine Hyatt <khyatt@flatironinstitute.org>
2018-09-20 11:51:09 -0400

    More doctests

--- src/interface.jl
+++ src/interface.jl
@@ -37,6 +37,18 @@ abstract type AbstractGraph{T} end
     src(e)
 
 Return the source vertex of edge `e`.
+
+# Examples
+```jldoctest
+julia> using LightGraphs
+
+julia> g = SimpleGraph(2);
+
+julia> add_edge!(g, 1, 2);
+
+julia> src(first(edges(g)))
+1
+```
 """
 src(e::AbstractEdge) = _NI("src")
 
@@ -44,6 +56,18 @@ src(e::AbstractEdge) = _NI("src")
     dst(e)
 
 Return the destination vertex of edge `e`.
+
+# Examples
+```jldoctest
+julia> using LightGraphs
+
+julia> g = SimpleGraph(2);
+
+julia> add_edge!(g, 1, 2);
+
+julia> dst(first(edges(g)))
+2
+```
 """
 dst(e::AbstractEdge) = _NI("dst")
 
@@ -54,6 +78,18 @@ Tuple(e::AbstractEdge) = _NI("Tuple")
     reverse(e)
 
 Create a new edge from `e` with source and destination vertices reversed.
+
+# Examples
+```jldoctest
+julia> using LightGraphs
+
+julia> g = SimpleDiGraph(2);
+
+julia> add_edge!(g, 1, 2);
+
+julia> reverse(first(edges(g)))
+Edge 2 => 1
+```
 """
 reverse(e::AbstractEdge) = _NI("reverse")
 
@@ -81,6 +117,14 @@ eltype(g::AbstractGraph) = _NI("eltype")
     nv(g)
 
 Return the number of vertices in `g`.
+
+# Examples
+```jldoctest
+julia> using LightGraphs
+
+julia> nv(SimpleGraph(3))
+3
+```
 """
 nv(g::AbstractGraph) = _NI("nv")
 
@@ -88,6 +132,16 @@ nv(g::AbstractGraph) = _NI("nv")
     ne(g)
 
 Return the number of edges in `g`.
+
+# Examples
+```jldoctest
+julia> using LightGraphs
+
+julia> g = PathGraph(3);
+
+julia> ne(g)
+2
+```
 """
 ne(g::AbstractGraph) = _NI("ne")
 
@@ -100,6 +154,17 @@ Return (an iterator to or collection of) the vertices of a graph.
 A returned iterator is valid for one pass over the edges, and
 is invalidated by changes to `g`.
 
+# Examples
+```jldoctest
+julia> using LightGraphs
+
+julia> collect(vertices(SimpleGraph(4)))
+4-element Array{Int64,1}:
+ 1
+ 2
+ 3
+ 4
+```
 """
 vertices(g::AbstractGraph) = _NI("vertices")
 
@@ -107,20 +172,43 @@ vertices(g::AbstractGraph) = _NI("vertices")
     edges(g)
 
 Return (an iterator to or collection of) the edges of a graph.
-For `AbstractSimpleGraph`s it returns a `SimpleEdgeIter`.
+For [`AbstractSimpleGraph`](@ref)s it returns a `SimpleEdgeIter`.
 The expressions `e in edges(g)` and `e ∈ edges(ga)` evaluate as
 calls to [`has_edge`](@ref).
 
 ### Implementation Notes
 A returned iterator is valid for one pass over the edges, and
 is invalidated by changes to `g`.
+
+# Examples
+```jldoctest
+julia> using LightGraphs
+
+julia> g = PathGraph(3);
+
+julia> collect(edges(g))
+2-element Array{LightGraphs.SimpleGraphs.SimpleEdge{Int64},1}:
+ Edge 1 => 2
+ Edge 2 => 3
+```
 """
 edges(g) = _NI("edges")
 
 """
     is_directed(g)
 
-Return true if the graph is a directed graph; false otherwise.
+Return `true` if the graph is a directed graph; `false` otherwise.
+
+# Examples
+```jldoctest
+julia> using LightGraphs
+
+julia> is_directed(SimpleGraph(2))
+false
+
+julia> is_directed(SimpleDiGraph(2))
+true
+```
 """
 is_directed(g) = _NI("is_directed")
 is_directed(::Type{T}) where T = _NI("is_directed")
@@ -129,6 +217,17 @@ is_directed(::Type{T}) where T = _NI("is_directed")
     has_vertex(g, v)
 
 Return true if `v` is a vertex of `g`.
+
+# Examples
+```jldoctest
+julia> using LightGraphs
+
+julia> has_vertex(SimpleGraph(2), 1)
+true
+
+julia> has_vertex(SimpleGraph(2), 3)
+false
+```
 """
 has_vertex(x, v) = _NI("has_vertex")
 
@@ -142,6 +241,21 @@ to a graph, including any data other than source and destination node.
 
 `e ∈ edges(g)` or `e ∈ edges(g)` evaluate as
 calls to `has_edge`, c.f. [`edges`](@ref).
+
+# Examples
+```jldoctest
+julia> using LightGraphs
+
+julia> g = SimpleDiGraph(2);
+
+julia> add_edge!(g, 1, 2);
+
+julia> has_edge(g, 1, 2)
+true
+
+julia> has_edge(g, 2, 1)
+false
+```
 """
 has_edge(g, s, d) = _NI("has_edge")
 has_edge(g, e) = has_edge(g, src(e), dst(e))
