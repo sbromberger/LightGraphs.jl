21fc4ed049c8596696d3774aa315f06d9ac1eb3a
Simon Schoelly <sischoel@gmail.com>
2018-10-08 23:55:17 +0200

    added a karate graph generator

--- src/SimpleGraphs/generators/smallgraphs.jl
+++ src/SimpleGraphs/generators/smallgraphs.jl
@@ -21,6 +21,7 @@ Create a small graph of type `s`. Admissible values for `s` are:
 | :house                    |   A graph mimicing the classic outline of a house. |
 | :housex                   |   A house graph, with two edges crossing the bottom square. |
 | :icosahedral              |   A [Platonic icosahedral   graph](https://en.wikipedia.org/wiki/Platonic_graph). |
+| :karate                   |   A social network graph called [Zachary's karate club](https://en.wikipedia.org/wiki/Zachary%27s_karate_club). |
 | :krackhardtkite           |   A [Krackhardt-Kite social network  graph](http://mathworld.wolfram.com/KrackhardtKite.html). |
 | :moebiuskantor            |   A [Möbius-Kantor graph](http://en.wikipedia.org/wiki/Möbius–Kantor_graph). |
 | :octahedral               |   A [Platonic octahedral graph](https://en.wikipedia.org/wiki/Platonic_graph).
@@ -46,6 +47,7 @@ function smallgraph(s::Symbol)
     :house           => HouseGraph,
     :housex          => HouseXGraph,
     :icosahedral     => IcosahedralGraph,
+    :karate          => KarateGraph,
     :krackhardtkite  => KrackhardtKiteGraph,
     :moebiuskantor   => MoebiusKantorGraph,
     :octahedral      => OctahedralGraph,
@@ -228,6 +230,24 @@ function IcosahedralGraph()
     return SimpleGraph(e)
 end
 
+function KarateGraph()
+    e = SimpleEdge.([
+         (1, 2),   (1, 3),   (1, 4),   (1, 5),   (1, 6),   (1, 7),
+         (1, 8),   (1, 9),   (1, 11),  (1, 12),  (1, 13),  (1, 14),
+         (1, 18),  (1, 20),  (1, 22),  (1, 32),  (2, 3),   (2, 4),
+         (2, 8),   (2, 14),  (2, 18),  (2, 20),  (2, 22),  (2, 31),
+         (3, 4),   (3, 8),   (3, 9),   (3, 10),  (3, 14),  (3, 28),
+         (3, 29),  (3, 33),  (4, 8),   (4, 13) , (4, 14),  (5, 7),
+         (5, 11),  (6, 7),   (6, 11),  (6, 17) , (7, 17),  (9, 31),
+         (9, 33),  (9, 34),  (10, 34), (14, 34), (15, 33), (15, 34),
+         (16, 33), (16, 34), (19, 33), (19, 34), (20, 34), (21, 33),
+         (21, 34), (23, 33), (23, 34), (24, 26), (24, 28), (24, 30),
+         (24, 33), (24, 34), (25, 26), (25, 28), (25, 32), (26, 32),
+         (27, 30), (27, 34), (28, 34), (29, 32), (29, 34), (30, 33),
+         (30, 34), (31, 33), (31, 34), (32, 33), (32, 34), (33, 34),
+    ])
+    return SimpleGraph(e)
+end
 
 function KrackhardtKiteGraph()
     e = SimpleEdge.([
