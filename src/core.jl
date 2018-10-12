d204069960c01655a42524c675388ba80afcff42
Seth Bromberger <sbromberger@users.noreply.github.com>
2018-10-12 10:47:33 -0600

    Update core.jl (#1048)
    clarify `weights` limitations.

--- src/core.jl
+++ src/core.jl
@@ -392,5 +392,9 @@ end
 
 Return the weights of the edges of a graph `g` as a matrix. Defaults
 to [`LightGraphs.DefaultDistance`](@ref).
+
+### Implementation Notes
+In general, referencing the weight of a nonexistent edge is undefined behavior. Do not rely on the `weights` matrix
+as a substitute for the graph's [`adjacency_matrix`](@ref).
 """
 weights(g::AbstractGraph) = DefaultDistance(nv(g))
