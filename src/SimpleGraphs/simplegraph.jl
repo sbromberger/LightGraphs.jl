e3af5b7c041fe8a94b737c031271b3b094086982
Seth Bromberger <sbromberger@users.noreply.github.com>
2018-10-12 09:47:22 -0700

    Update simplegraph.jl

--- src/SimpleGraphs/simplegraph.jl
+++ src/SimpleGraphs/simplegraph.jl
@@ -501,7 +501,7 @@ If `keep_order` is `true`, the vertices in the modified graph appear in the same
 order as they did in the unmodified graph. This might be slower.
 
 ### Implementation Notes
-This function is not (yet) part of the official LightGraphs API and is subject to change/removal between major versions.
+This function is not part of the official LightGraphs API and is subject to change/removal between major versions.
 
 # Examples
 ```jldoctest
