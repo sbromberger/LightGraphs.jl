6121d5579f594b0862aed2767be2ed50f1849dd4
kshyatt <kshyatt@physics.ucsb.edu>
2018-10-14 14:28:31 -0400

    Example for clique_percolation

--- src/community/clique_percolation.jl
+++ src/community/clique_percolation.jl
@@ -7,6 +7,23 @@ The parameter `k` defines the size of the clique to use in percolation.
  
 ### References
 - [Palla G, Derenyi I, Farkas I J, et al.] (https://www.nature.com/articles/nature03607)
+
+# Examples
+```jldoctest
+julia> using LightGraphs
+
+julia> clique_percolation(CliqueGraph(3, 2))
+2-element Array{BitSet,1}:
+ BitSet([4, 5, 6])
+ BitSet([1, 2, 3])
+
+julia> clique_percolation(CliqueGraph(3, 2), k=2)
+1-element Array{BitSet,1}:
+ BitSet([1, 2, 3, 4, 5, 6])
+
+julia> clique_percolation(CliqueGraph(3, 2), k=4)
+0-element Array{BitSet,1}
+```
 """
 function clique_percolation end
  
