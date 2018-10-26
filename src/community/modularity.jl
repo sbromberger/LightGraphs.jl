44739a4d80f6602adf8ab3621ba37607f54980f7
Vincent Gauthier <pub@luxbulb.org>
2018-10-26 17:29:16 +0200

    Update the modularity doc and minor change
    Enhance the documentation with references and example. Add the resolution parameter in the during the computation of the modularity.

--- src/community/modularity.jl
+++ src/community/modularity.jl
@@ -1,15 +1,68 @@
 """
-    modularity(g, c)
+    modularity(g, c, γ=1.0)
 
 Return a value representing Newman's modularity `Q` for the undirected graph
 `g` given the partitioning vector `c`.
+
+```math
+Q = \\frac{1}{2m} \\sum_{c} \\left( e_{c} - \\gamma \\frac{K_c^2}{2m} \\right)
+```
+where:
+- ``m``: m is the total number of edges in the network 
+- ``e_c``: number of edges in community ``c``
+- ``K_c``: is the sum of the degrees of the nodes in community ``c`` 
+
+### Optional Arguments
+- `γ=1.0`: where `γ > 0` is a resolution parameter. When the modularity is used 
+  to find communities structure in networks (i.e with [Louvain's method for 
+  community detection](https://en.wikipedia.org/wiki/Louvain_Modularity)), 
+  higher resolutions lead to more communities, while lower resolutions lead to 
+  fewer communities. Where `γ=1.0` it lead to the traditional definition of 
+  the modularity.
+
+### References
+- M. E. J. Newman and M. Girvan. "Finding and evaluating community structure in networks". 
+  Phys. Rev. E 69, 026113 (2004). [(arXiv)](https://arxiv.org/abs/cond-mat/0308217)
+- Reichardt, J. & Bornholdt, S. "Statistical mechanics of community detection". 
+  Phys. Rev. E 74, 016110 (2006). [(arXiv)](https://arxiv.org/abs/cond-mat/0603718)
+
+# Example 
+```jldoctest
+julia> using LightGraphs
+julia> g = SimpleDiGraph(6)
+{6, 0} directed simple Int64 graph
+
+julia> add_edge!(g, 1, 2)
+true
+
+julia> add_edge!(g, 2, 3)
+true
+
+julia> add_edge!(g, 3, 1)
+true
+
+julia> add_edge!(g, 3, 4)
+true 
+
+julia> add_edge!(g, 4, 5)
+true
+
+julia> add_edge!(g, 5, 6)
+true 
+
+julia> add_edge!(g, 6, 4)
+true
+
+julia> modularity(g, [1, 1, 1, 2, 2, 2])
+0.35714285714285715
+```
 """
 function modularity end
-@traitfn function modularity(g::::(!IsDirected), c::Vector)
+@traitfn function modularity(g::::(!IsDirected), c::Vector, γ=1.0)
     m = 2 * ne(g)
     m == 0 && return 0.
     nc = maximum(c)
-    a = zeros(Int, nc)
+    k = zeros(Int, nc)
     Q = 0
     for u in vertices(g)
         for v in neighbors(g, u)
@@ -19,14 +72,14 @@ function modularity end
                 if c1 == c2
                     Q += 2
                 end
-                a[c1] += 1
-                a[c2] += 1
+                k[c1] += 1
+                k[c2] += 1
             end
         end
     end
     Q = Q * m
     @inbounds for i = 1:nc
-        Q -= a[i] * a[i]
+        Q -= γ * k[i]^2
     end
-    return Q / m / m
+    return Q / m^2
 end
--- test/community/modularity.jl
+++ test/community/modularity.jl
@@ -11,4 +11,20 @@
     for g in testgraphs(gint)
       @test @inferred(modularity(g, c)) == 0
     end
+
+    n = 10
+    gint = SimpleGraph(n)
+    add_edge!(gint, 1, 2)
+    add_edge!(gint, 2, 3)
+    add_edge!(gint, 3, 1)
+    add_edge!(gint, 3, 4)
+    add_edge!(gint, 4, 5)
+    add_edge!(gint, 5, 6)
+    add_edge!(gint, 6, 4)
+    c = [1, 1, 1, 2, 2, 2]
+
+    for g in testgraphs(gint)
+        @test isapprox(modularity(g, c), 0.35714285714285715, atol=1e-3)
+        @test isapprox(modularity(g, c, 0.5), 0.6071428571428571, atol=1e-3)
+    end
 end
