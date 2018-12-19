0730e4e9738194bdb1b53c6cfe393dbf2bbd4d44
Seth Bromberger <sbromberger@users.noreply.github.com>
2018-12-19 09:40:57 -0800

    Update kruskal.jl

--- src/spanningtrees/kruskal.jl
+++ src/spanningtrees/kruskal.jl
@@ -1,5 +1,5 @@
 """
-    kruskal_mst(g, distmx=weights(g); minimum=true)
+    kruskal_mst(g, distmx=weights(g); minimize=true)
 Return a vector of edges representing the minimum (by default) spanning tree of a connected, undirected graph `g` with optional
 distance matrix `distmx` using [Kruskal's algorithm](https://en.wikipedia.org/wiki/Kruskal%27s_algorithm).
 
