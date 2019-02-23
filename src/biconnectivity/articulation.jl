e4702735ccd8f85f3e3f0e1fc95166dfe5cc8fe2
Abhinav Mehndiratta <abhinavmehndiratta@gmail.com>
2019-02-23 22:00:26 +0530

    Drop recursive articultion and use iterative instead

--- src/biconnectivity/articulation.jl
+++ src/biconnectivity/articulation.jl
@@ -1,54 +1,4 @@
 """
-    Articulations{T}
-
-A state type for the depth-first search that finds the articulation points in a graph.
-"""
-mutable struct Articulations{T<:Integer}
-    low::Vector{T}
-    depth::Vector{T}
-    articulation_points::BitVector
-    id::T
-end
-
-function Articulations(g::AbstractGraph)
-    n = nv(g)
-    T = typeof(n)
-    return Articulations(zeros(T, n), zeros(T, n), falses(n), zero(T))
-end
-
-"""
-    visit!(state, g, u, v)
-
-Perform a depth first search storing the depth (in `depth`) and low-points
-(in `low`) of each vertex.
-Call this function repeatedly to complete the DFS (see [`articulation`](@ref) for usage).
-"""
-function visit!(state::Articulations, g::AbstractGraph, u::Integer, v::Integer)
-    children = 0
-    state.id += 1
-    state.depth[v] = state.id
-    state.low[v] = state.depth[v]
-
-    for w in outneighbors(g, v)
-        if state.depth[w] == 0
-            children += 1
-            visit!(state, g, v, w)
-
-            state.low[v] = min(state.low[v], state.low[w])
-            if state.low[w] >= state.depth[v] && u != v
-                state.articulation_points[v] = true
-            end
-
-        elseif w != u
-            state.low[v] = min(state.low[v], state.depth[w])
-        end
-    end
-    if u == v && children > 1
-        state.articulation_points[v] = true
-    end
-end
-
-"""
     articulation(g)
 
 Compute the [articulation points](https://en.wikipedia.org/wiki/Biconnected_component)
@@ -69,22 +19,12 @@ julia> articulation(PathGraph(5))
  4
 ```
 """
-function articulation(g::AbstractGraph)
-    state = Articulations(g)
-    for u in vertices(g)
-        if state.depth[u] == 0
-            visit!(state, g, u, u)
-        end
-    end
-    return findall(state.articulation_points)
-end
-
-function iterative_articulation(g::SimpleGraph)
-    T = eltype(g)
+function articulation(g::SimpleGraph{T}) where T
     s = Vector{Tuple{T, T, T}}()
     is_articulation_pt = falses(nv(g))
     low = zeros(T, nv(g))
     pre = zeros(T, nv(g))
+
     @inbounds for u in vertices(g)
         pre[u] != 0 && continue
         v = u
@@ -93,6 +33,7 @@ function iterative_articulation(g::SimpleGraph)
         w::T = zero(T)
         cnt::T = one(T)
         first_time = true
+
         while !isempty(s) || first_time
             first_time = false
             if  wi < 1
@@ -129,13 +70,17 @@ function iterative_articulation(g::SimpleGraph)
             end
             wi < 1 && continue
         end
+        
         if children > 1
             is_articulation_pt[u] = true
         end
     end
+    
     articulation_points = Vector{T}()
+    
     for u in findall(is_articulation_pt)
         push!(articulation_points, T(u))
     end
+
     return articulation_points
 end
