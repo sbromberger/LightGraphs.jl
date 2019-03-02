a32dc5d4d8ae226593e797c3cf5bbe26fea5e115
Abhinav Mehndiratta <abhinavmehndiratta@gmail.com>
2019-03-02 19:13:52 +0530

    changed documentation, isgraphical works for all degs types (Integer)

--- src/connectivity.jl
+++ src/connectivity.jl
@@ -536,24 +536,30 @@ end
 """
     isgraphical(degs)
 
-Return true if the degree sequence `degs` is graphical
+Return true if the degree sequence `degs` is graphical.
+A sequence of integers is called graphical, if there exists a graph where the degrees of its vertices form that same sequence.
+
+### Performance
+Time complexity: ``\\mathcal{O}(|degs|*\\log(|degs|))``.
+
+### Implementation Notes
 According to Erdös-Gallai theorem, a degree sequence ``\\{d_1, ...,d_n\\}`` (sorted in descending order) is graphic iff the sum of vertex degrees is even and the sequence obeys the property -
 ```math
 \\sum_{i=1}^{r} d_i \\leq r(r-1) + \\sum_{i=r+1}^n min(r,d_i)
 ```
 for each integer r <= n-1
-
-### Performance
-Time complexity: ``\\mathcal{O}(|degs|)``.
 """
-function isgraphical(degs::Vector{Int})
+function isgraphical(degs::Vector{<:Integer})
     iseven(sum(degs)) || return false
     sorted_degs = sort(degs, rev = true)
     n = length(sorted_degs)
     cur_sum = 0
-    mindeg = (i->min(i, sorted_degs[i])).(sorted_degs)
+    mindeg = Vector{UInt64}(undef, n)
+    @inbounds for i = 1:n
+        mindeg[i] = min(i, sorted_degs[i])
+    end
     cum_min = sum(mindeg)
-    for r = 1:(n - 1)
+    @inbounds for r = 1:(n - 1)
         cur_sum += sorted_degs[r]
         cum_min -= mindeg[r]
         cond = cur_sum <= (r * (r - 1) + cum_min)
