3997363b79117b7c851e8663a563761dc7e8743b
eulerkochy <kc99.kol@gmail.com>
2019-03-23 18:06:52 +0530

    iterative dfs for finding bridge

--- src/biconnectivity/bridge.jl
+++ src/biconnectivity/bridge.jl
@@ -1,49 +1,4 @@
 """
-    Bridges{T}
-
-A state type for the depth-first search that finds bridges in a graph.
-"""
-mutable struct Bridges{T<:Integer}
-    low::Vector{T}
-    depth::Vector{T}
-    bridges::Vector{Edge{T}}
-    id::T
-end
-
-function Bridges(g::AbstractGraph)
-    n = nv(g)
-    T = eltype(g)
-    return Bridges(zeros(T, n), zeros(T, n), Edge{T}[], zero(T))
-end
-
-"""
-    visit!(state, g, u, v)
-
-Perform a depth first search storing the depth (in `depth`) and low-points
-(in `low`) of each vertex.
-"""
-function visit!(state::Bridges, g::AbstractGraph, u::Integer, v::Integer)
-    state.id += 1
-    state.depth[v] = state.id
-    state.low[v] = state.depth[v]
-
-    @inbounds for w in outneighbors(g, v)
-        if state.depth[w] == 0
-            visit!(state, g, v, w)
-
-            state.low[v] = min(state.low[v], state.low[w])
-            if state.low[w] > state.depth[v]
-                edge = v < w ? Edge(v, w) : Edge(w, v)
-                push!(state.bridges, edge)
-            end
-
-        elseif w != u
-            state.low[v] = min(state.low[v], state.depth[w])
-        end
-    end
-end
-
-"""
     bridges(g)
 
 Compute the [bridges](https://en.m.wikipedia.org/wiki/Bridge_(graph_theory))
@@ -69,13 +24,56 @@ julia> bridges(PathGraph(5))
 ```
 """
 function bridges end
+@traitfn function bridges(g::AG::(!IsDirected)) where {T, AG<:AbstractGraph{T}}
+    s = Vector{Tuple{T, T, T}}()
+    low = zeros(T, nv(g))
+    pre = zeros(T, nv(g))
+    bridges = Edge{T}[]
+    
+    @inbounds for u in vertices(g)
+        pre[u] != 0 && continue
+        v = u
+        wi::T = zero(T)
+        w::T = zero(T)
+        cnt::T = one(T)
+        first_time = true
 
-@traitfn function bridges(g::AG::(!IsDirected)) where {T<:Integer, AG<:AbstractGraph{T}}
-    state = Bridges(g)
-    for u in vertices(g)
-        if state.depth[u] == 0
-            visit!(state, g, u, u)
+        while !isempty(s) || first_time
+            first_time = false
+            if  wi < 1
+                pre[v] = cnt
+                cnt += 1
+                low[v] = pre[v]
+                v_neighbors = outneighbors(g, v)
+                wi = 1
+            else
+                wi, u, v = pop!(s)
+                v_neighbors = outneighbors(g, v)
+                w = v_neighbors[wi]
+                low[v] = min(low[v], low[w])
+                if low[w] > pre[v]
+                    edge = v < w ? Edge(v, w) : Edge(w, v)
+                    push!(bridges, edge)
+                end
+                wi += 1
+            end
+            while wi <= length(v_neighbors)
+                w = v_neighbors[wi]
+                if pre[w] == 0
+                    push!(s, (wi, u, v))
+                    wi = 0
+                    u = v
+                    v = w
+                    break
+                elseif w != u
+                    low[v] = min(low[v], pre[w])
+                end
+                wi += 1
+            end
+            wi < 1 && continue
         end
+        
     end
-    return state.bridges
+    
+    return bridges
 end
