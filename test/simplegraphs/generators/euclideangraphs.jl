6738b49f67b3f778cdd4c868db95fd357b7ec208
Simon Schoelly <sischoel@gmail.com>
2019-03-24 18:34:33 +0100

    ✅ Improve test coverage of euclidean_graph

--- test/simplegraphs/generators/euclideangraphs.jl
+++ test/simplegraphs/generators/euclideangraphs.jl
@@ -19,4 +19,18 @@
 
     @test_throws DomainError euclidean_graph(points, L=0.01,  bc=:periodic)
     @test_throws ArgumentError euclidean_graph(points, bc=:badbc)
+
+    # In our algorithm we ensure, that the resulting graph has the same
+    # number of vertices as we have points. For this we have a special case,
+    # where we have to insert vertices if the vertices with the highest indices are
+    # isolated. This test ensures that this case is covered.
+    @testset "Euclidean graph with vertex of highest index isolated" begin
+        point1 = [1.0, 0.0, 0.0]
+        point2 = [0.0, 1.0, 0.0]
+        point3 = [0.0, 0.0, 100.0]
+        matrix = hcat(point1, point2, point3)
+        g = euclidean_graph(matrix, cutoff= 5.0)
+        @test has_edge(g, 1, 2) && ne(g) == 1
+        @test nv(g) == 3
+    end
 end
