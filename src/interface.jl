beb69ea94115f5532a0f3fc6aeb4acc9a4fa683a
Mathieu Besançon <mathieu.besancon@gmail.com>
2019-06-04 19:36:48 +0200

    details in doc

--- src/interface.jl
+++ src/interface.jl
@@ -195,15 +195,19 @@ julia> collect(edges(g))
 edges(g) = _NI("edges")
 
 """
-    is_directed(g)
+    is_directed(G)
 
-Return `true` if the graph is a directed graph; `false` otherwise.
+Return `true` if the graph type `G` is a directed graph; `false` otherwise.
 New graph types must implement `is_directed(::Type{<:G})`.
+The method can also be called with `is_directed(g::G)`
 # Examples
 ```jldoctest
 julia> using LightGraphs
 
 julia> is_directed(SimpleGraph(2))
+false
+
+julia> is_directed(SimpleGraph)
 false
 
 julia> is_directed(SimpleDiGraph(2))
