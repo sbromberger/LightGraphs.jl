86fa923af20aba159bc9b8a80018e6ff75a6428e
Simon Schoelly <sischoel@gmail.com>
2019-06-23 16:16:13 +0200

    Remove print

--- test/simplegraphs/simplegraphs.jl
+++ test/simplegraphs/simplegraphs.jl
@@ -396,7 +396,6 @@ import Random
             @test_throws DomainError SimpleDiGraphFromIterator(edge_list_2)
         end
     end
-    println("Finished unning SimpleGraphFromIterator")
 
     # test for rem_vertices!
     let
