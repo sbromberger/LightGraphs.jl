402db4c7eadafa3a102a7960f8a96e84d5004b0c
Mathieu Besançon <mathieu.besancon@gmail.com>
2019-07-09 20:43:56 +0200

    split deprecation tests

--- test/simplegraphs/runtests.jl
+++ test/simplegraphs/runtests.jl
@@ -96,30 +96,3 @@ const simple_tests = [
         include(tp)
     end
 end
-
-@testset "Generator deprecations" begin
-    types_0 = [BullGraph, ChvatalGraph, CubicalGraph, DesarguesGraph,
-        DiamondGraph, DodecahedralGraph, FruchtGraph, HeawoodGraph,
-        HouseGraph, HouseXGraph, IcosahedralGraph, KarateGraph, KrackhardtKiteGraph,
-        MoebiusKantorGraph, OctahedralGraph, PappusGraph, PetersenGraph,
-        SedgewickMazeGraph, TetrahedralGraph, TruncatedCubeGraph,
-        TruncatedTetrahedronGraph, TruncatedTetrahedronDiGraph, TutteGraph]
-    types_1param = [CompleteGraph, CompleteDiGraph,
-            StarGraph, StarDigraph, PathGraph,
-            PathDiGraph, CycleGraph, CycleDiGraph, WheelGraph, WheelDiGraph,
-            BinaryTree, Doublebinary_tree, RoachGraph,
-            LadderGraph, Circularladder_graph]
-    types_2params = [CompleteBipartiteGraph, LollipopGraph, BarbellGraph,
-        TuranGraph, CliqueGraph]
-    for G in types_0
-        @test_deprecated G()
-    end
-    for G in types_1param
-        @test_deprecated G(5)
-    end
-    for G in types_2params
-        @test_deprecated G(5, 2)
-    end
-    @test_deprecated CompleteMultipartiteGraph([3, 5])
-    @test_deprecated Grid([3, 5])
-end
