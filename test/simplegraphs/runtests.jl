733a438170fe88119e489b117d39af58b353066c
Mathieu Besançon <mathieu.besancon@gmail.com>
2019-07-09 20:24:15 +0200

    fixing name

--- test/simplegraphs/runtests.jl
+++ test/simplegraphs/runtests.jl
@@ -80,7 +80,7 @@ end
 
 const simplegraphtestdir = dirname(@__FILE__)
 
-const tests = [
+const simple_tests = [
     "simplegraphs",
     "simpleedge",
     "simpleedgeiter",
@@ -91,7 +91,7 @@ const tests = [
 ]
 
 @testset "LightGraphs.SimpleGraphs" begin
-    for t in tests
+    for t in simple_tests
         tp = joinpath(simplegraphtestdir, "$(t).jl")
         include(tp)
     end
