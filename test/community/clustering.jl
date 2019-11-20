7ee3bc581720c57ec2a33757131e57cadfe29b96
Seth Bromberger <sbromberger@users.noreply.github.com>
2019-11-20 05:11:56 -0800

    fixes #1265 (#1266)
    * fixes #1265
    
    * test coverage

--- test/community/clustering.jl
+++ test/community/clustering.jl
@@ -8,4 +8,8 @@
         @test @inferred(triangles(g)) == fill(36, 10)
         @test @inferred(triangles(g, 1)) == 36
     end
+    # 1265 / 1266
+    g = complete_graph(3)
+    add_edge!(g, 2, 2)
+    @test @inferred(triangles(g)) == fill(1, 3)
 end
