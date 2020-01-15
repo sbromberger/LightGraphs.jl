21ae65339692ac612eb1bf29da6293e666b04367
Lucas <halow443@gmail.com>
2020-01-15 14:48:09 -0300

    changing comment

--- src/spanningtrees/boruvka.jl
+++ src/spanningtrees/boruvka.jl
@@ -5,8 +5,7 @@ optimum (minimum, by default) spanning tree of a connected, undirected graph
 `g` with optional matrix `distmx` that provides distinct edge weights, and
 `weights` is the sum of all the edges in the solution by using
 [Boruvka's algorithm](https://en.wikipedia.org/wiki/Bor%C5%AFvka%27s_algorithm).
-The algorithm correctly gives a minimum/maximum spanning tree if all edges have
-different weights.
+The algorithm requires that all edges have different weights to correctly generate a minimun/maximum spanning tree
 ### Optional Arguments
 - `minimize=true`: if set to `false`, calculate the maximum spanning tree.
 """
