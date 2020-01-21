c60afc3b72f3ac39578381e693c6d30a476450b3
Arib Alam <aribalam64@gmail.com>
2020-01-22 00:35:09 +0530

    added test cases according to graph functions

--- test/traversals/bfs.jl
+++ test/traversals/bfs.jl
@@ -1,57 +1,18 @@
 import LightGraphs: tree
 @testset "BFS" begin
+
     g5 = SimpleDiGraph(4)
     add_edge!(g5, 1, 2); add_edge!(g5, 2, 3); add_edge!(g5, 1, 3); add_edge!(g5, 3, 4)
     g6 = smallgraph(:house)
 
-    @testset "Simple DiGraph with 4 vertices" begin
+    @testset "bfs_tree and bfs_parents" begin
         for g in testdigraphs(g5)
             z = @inferred(bfs_tree(g, 1))
             t = bfs_parents(g, 1)
             @test t == [1, 1, 1, 3]
             @test nv(z) == 4 && ne(z) == 3 && !has_edge(z, 2, 3)
         end
-    end
 
-    @testset "geodesic distances using House Graph" begin
-        for g in testgraphs(g6)
-            @test @inferred(gdistances(g, 2)) == @inferred(gdistances(g, 2; sort_alg = MergeSort)) == [1, 0, 2, 1, 2]
-            @test @inferred(gdistances(g, [1, 2])) == [0, 0, 1, 1, 2]
-            @test @inferred(gdistances(g, [])) == fill(typemax(eltype(g)), 5)
-        end
-    end
-
-    @testset "is_bipartite with Simple Graph with 5 vertices" begin
-        gx = SimpleGraph(5)
-        add_edge!(gx, 1, 2); add_edge!(gx, 1, 4)
-        add_edge!(gx, 2, 3); add_edge!(gx, 2, 5)
-        add_edge!(gx, 3, 4)
-
-        for g in testgraphs(gx)
-            @test @inferred(is_bipartite(g))
-        end
-    end
-
-
-    # import LightGraphs: TreeBFSVisitorVector, bfs_tree!, tree
-
-    function istree(parents::Vector{T}, maxdepth, n::T) where T<:Integer
-        flag = true
-        for i in one(T):n
-            s = i
-            depth = 0
-            while parents[s] > 0 && parents[s] != s
-                s = parents[s]
-                depth += 1
-                if depth > maxdepth
-                    return false
-                end
-            end
-        end
-        return flag
-    end
-
-    @testset "bfs_tree and bfs_parents with House graph" begin
         for g in testgraphs(g6)
             n = nv(g)
             parents = bfs_parents(g, 1)
@@ -65,7 +26,26 @@ import LightGraphs: tree
         end
     end
 
-    @testset "has_path using House Graph" begin
+    @testset "gdistances" begin
+        for g in testgraphs(g6)
+            @test @inferred(gdistances(g, 2)) == @inferred(gdistances(g, 2; sort_alg = MergeSort)) == [1, 0, 2, 1, 2]
+            @test @inferred(gdistances(g, [1, 2])) == [0, 0, 1, 1, 2]
+            @test @inferred(gdistances(g, [])) == fill(typemax(eltype(g)), 5)
+        end
+    end
+
+    @testset "is_bipartite" begin
+        gx = SimpleGraph(5)
+        add_edge!(gx, 1, 2); add_edge!(gx, 1, 4)
+        add_edge!(gx, 2, 3); add_edge!(gx, 2, 5)
+        add_edge!(gx, 3, 4)
+
+        for g in testgraphs(gx)
+            @test @inferred(is_bipartite(g))
+        end
+    end
+
+    @testset "has_path" begin
         gx = SimpleGraph(6)
         d = nv(gx)
         for (i, j) in [(1, 2), (2, 3), (2, 4), (4, 5), (3, 5)]
@@ -88,7 +68,25 @@ import LightGraphs: tree
             @test has_path(g, 1, 1) # inseparable 
             @test !has_path(g, 1, 2; exclude_vertices=[2])
             @test !has_path(g, 1, 2; exclude_vertices=[1])
+        end 
+    end
+    
+    # import LightGraphs: TreeBFSVisitorVector, bfs_tree!, tree
+
+    function istree(parents::Vector{T}, maxdepth, n::T) where T<:Integer
+        flag = true
+        for i in one(T):n
+            s = i
+            depth = 0
+            while parents[s] > 0 && parents[s] != s
+                s = parents[s]
+                depth += 1
+                if depth > maxdepth
+                    return false
+                end
+            end
         end
+        return flag
     end
     
 end
--- test/traversals/dfs.jl
+++ test/traversals/dfs.jl
@@ -1,31 +1,34 @@
 @testset "DFS" begin
 
-    @testset "Simple DiGraph with 4 vertices" begin
-      g5 = SimpleDiGraph(4)
-      add_edge!(g5, 1, 2); add_edge!(g5, 2, 3); add_edge!(g5, 1, 3); add_edge!(g5, 3, 4)
-      for g in testdigraphs(g5)
-        z = @inferred(dfs_tree(g, 1))
-        @test ne(z) == 3 && nv(z) == 4
-        @test !has_edge(z, 1, 3)
+  g5 = SimpleDiGraph(4)
+  add_edge!(g5, 1, 2); add_edge!(g5, 2, 3); add_edge!(g5, 1, 3); add_edge!(g5, 3, 4)
+  gx = cycle_digraph(3)
 
-        @test @inferred(topological_sort_by_dfs(g)) == [1, 2, 3, 4]
-        @test !is_cyclic(g)
-      end
+  @testset "dfs_tree" begin
+    for g in testdigraphs(g5)
+      z = @inferred(dfs_tree(g, 1))
+      @test ne(z) == 3 && nv(z) == 4
+      @test !has_edge(z, 1, 3)
+      @test !is_cyclic(g)
+    end
+  end
+
+  @testset "topological_sort_by_dfs" begin
+    for g in testdigraphs(g5)
+      @test @inferred(topological_sort_by_dfs(g)) == [1, 2, 3, 4]
     end
 
-    @testset "Cycle DiGraph with 3 vertices" begin
-      gx = cycle_digraph(3)
-      for g in testdigraphs(gx)
-        @test @inferred(is_cyclic(g))
-        @test_throws ErrorException topological_sort_by_dfs(g)
-      end
+    for g in testdigraphs(gx)
+      @test @inferred(is_cyclic(g))
+      @test_throws ErrorException topological_sort_by_dfs(g)      
     end
+  end
 
-    @testset "Path Graph with 2 vertices" begin
-      for g in testgraphs(path_graph(2))
-          @test @inferred(is_cyclic(g))
-          @test @inferred(!is_cyclic(zero(g)))
-      end
+  @testset "is_cyclic" begin
+    for g in testgraphs(path_graph(2))
+      @test @inferred(is_cyclic(g))
+      @test @inferred(!is_cyclic(zero(g)))
     end
+  end
 
 end
