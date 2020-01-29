cde20cb0998722ab4db52d01697b2c4af9f73d1f
Seth Bromberger <github@bromberger.com>
2020-01-29 11:50:41 -0800

    comments

--- src/degeneracy.jl
+++ src/degeneracy.jl
@@ -37,32 +37,41 @@ julia> core_number(g)
 """
 function core_number(g::AbstractGraph{T}) where T
     has_self_loops(g) && throw(ArgumentError("graph must not have self-loops"))
-    n = nv(g)
-    deg = T.(degree(g)) # degree should really return T.
-    maxdeg = maximum(deg)
-    bin = zeros(T, maxdeg+1)
-    vert = zeros(T, n)
-    pos = zeros(T, n)
+    n = nv(g)    
+    deg = T.(degree(g)) # this will contain core number for each vertex of graph
+    maxdeg = maximum(deg) # maximum degree of a vertex in graph
+    bin = zeros(T, maxdeg+1) # used for bin-sort and storing starting positions of bins
+    vert = zeros(T, n) # contains the set of vertices, sorted by their degrees
+    pos = zeros(T, n) # contains positions of vertices in array vert
+
+    # count number of vertices will be in each bin
     for v = 1:n
         bin[deg[v]+1] += one(T)
     end
+    # from bin sizes determine starting positions of bins in array vert 
     start = one(T)
     for d = zero(T):maxdeg
         num = bin[d+1]
         bin[d+1] = start
         start += num
     end
+    # sort the vertices in increasing order of their degrees and store in array vert
     for v in vertices(g)
         pos[v] = bin[deg[v]+1]
         vert[pos[v]] = v
         bin[deg[v]+1] += one(T)
     end
+
+    # recover starting positions of the bins
     for d = maxdeg:-1:one(T)
        bin[d+1] = bin[d]
     end
     bin[1] = one(T)
+
+    # cores decomposition
     for i = 1:n
         v = vert[i]
+        # for each neighbor u of vertex v with higher degree we have to decrease its degree and move it for one bin to the left
         for u in all_neighbors(g, v)
             if deg[u] > deg[v]
                 du = deg[u]
@@ -80,6 +89,7 @@ function core_number(g::AbstractGraph{T}) where T
             end
         end
     end
+
     return deg
 end
 
