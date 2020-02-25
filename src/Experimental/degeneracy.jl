002fd3b22f9e898b3d79403a9cb1bb98f4a2f25b
Abhinav Mehndiratta <abhinavmehndiratta@gmail.com>
2020-02-25 17:34:47 +0530

    bug fixes

--- src/Experimental/degeneracy.jl
+++ src/Experimental/degeneracy.jl
@@ -12,56 +12,46 @@ Time complexity is ``\\mathcal{O}(K_{max}*|V| + |E|)``.
 """
 function PKC(g, frac = 0.95)
     deg = Atomic{Int64}.(degree(g))
-    buff = [Vector{Int64}(undef, nv(g)) for _ in 1:nthreads()]
+    buff = [Vector{Int64}() for _ in 1:nthreads()]
+    e = zeros(Int64, nthreads())
     visited = 0
-    len_buff = zeros(Int64, nthreads())
     level = 0
     while visited < nv(g)*frac
-        process_level(g, deg, buff, level, len_buff)       
-        for tid in 1:nthreads()
-            visited += len_buff[tid]
-            len_buff[tid] = 0
-        end
+        visited += process_level(g, deg, buff, e, level)
         level += 1
     end
     if visited < nv(g)
-        reduce_nvg = nv(g) - visited
-        g_small, mapIndexToVtx = subgraph(g, reduce_nvg, level, deg)
+        g_small, mapIndexToVtx = subgraph(g, nv(g)-visited, level, deg)
         newdeg = Atomic{Int64}.(degree(g_small))
-        while visited < nv(g)        
-            process_level(g_small, newdeg, buff, level, len_buff)
-            for tid in 1:nthreads()
-                visited += len_buff[tid]
-                len_buff[tid] = 0
-            end
+        while visited < nv(g)
+            visited += process_level(g_small, newdeg, buff, e, level)
             level += 1
         end
         for i in 1:nv(g_small)
             deg[mapIndexToVtx[i]] = newdeg[i]
         end
     end
-    return map(x -> x[], deg)
+    return map(i -> i[], deg)
 end
 
-function process_level(g, deg, buff, level, len_buff)
+function process_level(g, deg, buff, e, level)
+    e .= 0
     @threads for v in 1:nv(g)
         tid = threadid()
         if deg[v][] == level
-            len_buff[tid] += 1
-            buff[tid][len_buff[tid]] = v
+            push!(buff[tid], v)
+            e[tid] += 1
         end
     end
     @threads for tid in 1:nthreads()
-        s = 1
-        while s <= len_buff[tid]
-            v = buff[tid][s]
-            s += 1
+        while !isempty(buff[tid])
+            v = popfirst!(buff[tid])
             for u in all_neighbors(g, v)
                 if deg[u][] > level
                     a = atomic_sub!(deg[u], 1)
                     if a == level+1
-                        len_buff[tid] += 1
-                        buff[tid][len_buff[tid]] = u
+                        push!(buff[tid], u)
+                        e[tid] += 1
                     end
                     if a <= level
                         atomic_add!(deg[u], 1)
@@ -70,6 +60,7 @@ function process_level(g, deg, buff, level, len_buff)
             end
         end
     end
+    return sum(e)
 end
 
 function subgraph(g, nvg_small, level, deg)
@@ -80,14 +71,12 @@ function subgraph(g, nvg_small, level, deg)
             in_gsmall[v] = true
         end
     end
-    vlist = findall(in_gsmall)
+    vmap = findall(in_gsmall)
     newvid = Vector{Int64}(undef, nv(g))
-    vmap = Vector{Int64}(undef, nvg_small)
-    for (i, v) in enumerate(vlist)
-        newvid[v] = i
-        vmap[i] = v
+    for i in 1:length(vmap)
+        newvid[vmap[i]] = i
     end
-    @threads for s in vlist
+    @threads for s in vmap
         for d in all_neighbors(g, s)
             if in_gsmall[d]
                 add_edge!(g_small, newvid[s], newvid[d])
@@ -98,13 +87,12 @@ function subgraph(g, nvg_small, level, deg)
 end
 
 #parallel graph
-struct SmallGraph
+struct SmallGraph <: AbstractGraph{Int64}
     adjlist::Vector{Vector{Int64}}
     nvg::Int64
     SmallGraph(nvertices::Int64) = new([Int64[] for _ in 1:nvertices], nvertices)
 end
 is_directed(::Type{<:SmallGraph}) = false
-is_directed(::SmallGraph) = false
 nv(g::SmallGraph) = g.nvg
 degree(g::SmallGraph) = length.(g.adjlist)
 add_edge!(g_small::SmallGraph, u::Int64, v::Int64) = push!(g_small.adjlist[u], v)
