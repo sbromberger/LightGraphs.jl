f142b3734d30091de289172b7ab8902395cd2639
alimamdouh212 <33188255+alimamdouh212@users.noreply.github.com>
2020-02-27 15:42:03 +0200

    Update Dominator_Tree.jl

--- src/digraph/Dominator_Tree.jl
+++ src/digraph/Dominator_Tree.jl
@@ -190,7 +190,7 @@ function dominator_Tree(g::AG, source) where {T, AG<:DiGraph{T}}
   end
 
 
-function produce_eval_link(n<:Int32,accumfun::Function,T::DataType)
+function produce_eval_link(n::Int32,accumfun::Function,T::DataType)
 
 
 
