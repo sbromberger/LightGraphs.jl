1186bc8c421e064a1b8606b6adc60d50f80fdf61
Ryan Birmingham <birm@rbirm.us>
2020-02-27 16:08:45 -0500

    whitespace to standard

--- test/community/cliques.jl
+++ test/community/cliques.jl
@@ -7,53 +7,53 @@
 
 @testset "Cliques" begin
     function setofsets(array_of_arrays)
-      Set(map(Set, array_of_arrays))
+        Set(map(Set, array_of_arrays))
     end
 
     function test_cliques(graph, expected)
-      # Make test results insensitive to ordering
-      setofsets(@inferred(maximal_cliques(graph))) == setofsets(expected)
+        # Make test results insensitive to ordering
+        setofsets(@inferred(maximal_cliques(graph))) == setofsets(expected)
     end
 
     @testset "simple maximal clique cases" begin
-      gx = SimpleGraph(3)
-      add_edge!(gx, 1, 2)
-      @testset "$g simple single maximal clique" for g in testgraphs(gx)
-        @test test_cliques(g, Array[[1, 2], [3]])
-      end
-      add_edge!(gx, 2, 3)
-      @testset "$g" for g in testgraphs(gx)
-        @test test_cliques(g, Array[[1, 2], [2, 3]])
-      end
+        gx = SimpleGraph(3)
+        add_edge!(gx, 1, 2)
+        @testset "$g simple single maximal clique" for g in testgraphs(gx)
+            @test test_cliques(g, Array[[1, 2], [3]])
+        end
+        add_edge!(gx, 2, 3)
+        @testset "$g" for g in testgraphs(gx)
+            @test test_cliques(g, Array[[1, 2], [2, 3]])
+        end
     end
     @testset "pivotdonenbrs defined" begin
-      h = SimpleGraph(6)
-      add_edge!(h, 1, 2)
-      add_edge!(h, 1, 3)
-      add_edge!(h, 1, 4)
-      add_edge!(h, 2, 5)
-      add_edge!(h, 2, 6)
-      add_edge!(h, 3, 4)
-      add_edge!(h, 3, 6)
-      add_edge!(h, 5, 6)
+        h = SimpleGraph(6)
+        add_edge!(h, 1, 2)
+        add_edge!(h, 1, 3)
+        add_edge!(h, 1, 4)
+        add_edge!(h, 2, 5)
+        add_edge!(h, 2, 6)
+        add_edge!(h, 3, 4)
+        add_edge!(h, 3, 6)
+        add_edge!(h, 5, 6)
 
-      @testset "$g" for g in testgraphs(h)
-        @test !isempty(@inferred(maximal_cliques(g)))
-      end
+        @testset "$g" for g in testgraphs(h)
+            @test !isempty(@inferred(maximal_cliques(g)))
+        end
     end
 
     # test for extra cliques bug
     @testset "does not find extra cliques" begin
-      h = SimpleGraph(7)
-      add_edge!(h, 1, 3)
-      add_edge!(h, 2, 6)
-      add_edge!(h, 3, 5)
-      add_edge!(h, 3, 6)
-      add_edge!(h, 4, 5)
-      add_edge!(h, 4, 7)
-      add_edge!(h, 5, 7)
-      @testset "$g" for g in testgraphs(h)
-        @test test_cliques(h, Array[[7, 4, 5], [2, 6], [3, 5], [3, 6], [3, 1]])
-      end
+        h = SimpleGraph(7)
+        add_edge!(h, 1, 3)
+        add_edge!(h, 2, 6)
+        add_edge!(h, 3, 5)
+        add_edge!(h, 3, 6)
+        add_edge!(h, 4, 5)
+        add_edge!(h, 4, 7)
+        add_edge!(h, 5, 7)
+        @testset "$g" for g in testgraphs(h)
+            @test test_cliques(h, Array[[7, 4, 5], [2, 6], [3, 5], [3, 6], [3, 1]])
+        end
     end
 end
--- test/community/modularity.jl
+++ test/community/modularity.jl
@@ -1,29 +1,29 @@
 @testset "Modularity" begin
 
     @testset "undirected cases" begin
-      n = 10
-      m = n * (n - 1) / 2
-      c = ones(Int, n)
-      gint = complete_graph(n)
-      @testset "$g" for g in testgraphs(gint)
-        @test @inferred(modularity(g, c)) == 0
-      end
+        n = 10
+        m = n * (n - 1) / 2
+        c = ones(Int, n)
+        gint = complete_graph(n)
+        @testset "$g" for g in testgraphs(gint)
+            @test @inferred(modularity(g, c)) == 0
+        end
 
-      gint = SimpleGraph(n)
-      @testset "$g" for g in testgraphs(gint)
-        @test @inferred(modularity(g, c)) == 0
-      end
+        gint = SimpleGraph(n)
+        @testset "$g" for g in testgraphs(gint)
+            @test @inferred(modularity(g, c)) == 0
+        end
 
-      barbell = blockdiag(complete_graph(3), complete_graph(3))
-      add_edge!(barbell, 1, 4)
-      c = [1, 1, 1, 2, 2, 2]
+        barbell = blockdiag(complete_graph(3), complete_graph(3))
+        add_edge!(barbell, 1, 4)
+        c = [1, 1, 1, 2, 2, 2]
 
-      @testset "$g" for g in testgraphs(barbell)
-          Q1 = @inferred(modularity(g, c))
-          @test isapprox(Q1, 0.35714285714285715, atol=1e-3)
-          Q2 = @inferred(modularity(g, c, γ=0.5))
-          @test isapprox(Q2, 0.6071428571428571, atol=1e-3)
-      end
+        @testset "$g" for g in testgraphs(barbell)
+            Q1 = @inferred(modularity(g, c))
+            @test isapprox(Q1, 0.35714285714285715, atol=1e-3)
+            Q2 = @inferred(modularity(g, c, γ=0.5))
+            @test isapprox(Q2, 0.6071428571428571, atol=1e-3)
+        end
     end
 
     @testset "directed cases" begin
@@ -51,44 +51,44 @@
     end
 
     @testset "weighted and undirected cases" begin
-      triangle = SimpleGraph(3)
-      add_edge!(triangle, 1, 2)
-      add_edge!(triangle, 2, 3)
-      add_edge!(triangle, 3, 1)
+        triangle = SimpleGraph(3)
+        add_edge!(triangle, 1, 2)
+        add_edge!(triangle, 2, 3)
+        add_edge!(triangle, 3, 1)
 
-      barbell = blockdiag(triangle, triangle)
-      add_edge!(barbell, 1, 4) # this edge has a weight of 5
-      c = [1, 1, 1, 2, 2, 2]
-      d = [[0  1  1  5  0  0]
-           [1  0  1  0  0  0]
-           [1  1  0  0  0  0]
-           [5  0  0  0  1  1]
-           [0  0  0  1  0  1]
-           [0  0  0  1  1  0]]
-      @testset "$g" for g in testgraphs(barbell)
-          Q = @inferred(modularity(g, c, distmx=d))
-          @test isapprox(Q, 0.045454545454545456, atol=1e-3)
-      end
+        barbell = blockdiag(triangle, triangle)
+        add_edge!(barbell, 1, 4) # this edge has a weight of 5
+        c = [1, 1, 1, 2, 2, 2]
+        d = [[0  1  1  5  0  0]
+             [1  0  1  0  0  0]
+             [1  1  0  0  0  0]
+             [5  0  0  0  1  1]
+             [0  0  0  1  0  1]
+             [0  0  0  1  1  0]]
+        @testset "$g" for g in testgraphs(barbell)
+            Q = @inferred(modularity(g, c, distmx=d))
+            @test isapprox(Q, 0.045454545454545456, atol=1e-3)
+        end
     end
 
     @testset "weighted and directed cases" begin
-      triangle = SimpleDiGraph(3)
-      add_edge!(triangle, 1, 2)
-      add_edge!(triangle, 2, 3)
-      add_edge!(triangle, 3, 1)
+        triangle = SimpleDiGraph(3)
+        add_edge!(triangle, 1, 2)
+        add_edge!(triangle, 2, 3)
+        add_edge!(triangle, 3, 1)
 
-      barbell = blockdiag(triangle, triangle)
-      add_edge!(barbell, 1, 4) # this edge has a weight of 5
-      c = [1, 1, 1, 2, 2, 2]
-      d = [[0  1  0  5  0  0]
-           [0  0  1  0  0  0]
-           [1  0  0  0  0  0]
-           [0  0  0  0  1  0]
-           [0  0  0  0  0  1]
-           [0  0  0  1  0  0]]
-      @testset "$g" for g in testdigraphs(barbell)
-          Q = @inferred(modularity(g, c, distmx=d))
-          @test isapprox(Q, 0.1487603305785124, atol=1e-3)
-      end
+        barbell = blockdiag(triangle, triangle)
+        add_edge!(barbell, 1, 4) # this edge has a weight of 5
+        c = [1, 1, 1, 2, 2, 2]
+        d = [[0  1  0  5  0  0]
+             [0  0  1  0  0  0]
+             [1  0  0  0  0  0]
+             [0  0  0  0  1  0]
+             [0  0  0  0  0  1]
+             [0  0  0  1  0  0]]
+        @testset "$g" for g in testdigraphs(barbell)
+            Q = @inferred(modularity(g, c, distmx=d))
+            @test isapprox(Q, 0.1487603305785124, atol=1e-3)
+        end
     end
 end
