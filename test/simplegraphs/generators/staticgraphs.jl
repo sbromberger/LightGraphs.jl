40dd25d8b976d5b46a011ae7fcbbb010c6f4c46d
alimamdouh212 <33188255+alimamdouh212@users.noreply.github.com>
2020-03-01 19:37:38 +0200

    Update staticgraphs.jl

--- test/simplegraphs/generators/staticgraphs.jl
+++ test/simplegraphs/generators/staticgraphs.jl
@@ -538,16 +538,17 @@
     end
    @testset "Friendship Graphs" begin
         # the Friendship Graphs is connected graph consist of n cycle of lenght 3 share only one node 
-        for n in [10, 15, 20]
+        for n in [-5,0,10, 15, 20]
          g=friendship_graph(n)
          @test length(connected_components(g)) == 1
          @test articulation(g) == [1]
-         compents=biconnected_components(g)
-         @test length(compents) == n
+         cc=biconnected_components(g)
+         @test length(cc) == max(1,n)
          for i in 1:n
-            @test length(compents[i]) == 3 # the only biconnected simple graph of 3 edges is c3 
+            @test length(cc[i]) == 3 # the only biconnected simple graph of 3 edges is c3 
          end
         end
+        
     end
     
 end
