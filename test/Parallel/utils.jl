5723aa4bf9a0a2bab259c5926f30ad98351d20ce
Seth Bromberger <github@bromberger.com>
2020-03-04 19:11:44 -0800

    removed experimental shortest paths

---
+++ test/Parallel/utils.jl
@@ -0,0 +1,20 @@
+@testset "Parallel.Generate Reduce" begin
+
+    function make_vec(g::AbstractGraph{T}) where T<:Integer
+        return Vector{T}(undef, nv(g))
+    end
+
+    function comp_vec(x::Vector, y::Vector)
+        return length(x) < length(y)
+    end
+
+    g1 = star_graph(5)
+
+    for parallel in [:distributed, :threads]
+        for g in testgraphs(g1)
+
+            s = @inferred(LightGraphs.Parallel.generate_reduce(g, make_vec, comp_vec, 5; parallel=parallel))
+            @test length(s) == 5
+        end
+    end
+end
