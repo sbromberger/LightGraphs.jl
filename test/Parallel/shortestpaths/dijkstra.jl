3eaaf530038ba8d52d6418d5225bbe779798d294
Seth Bromberger <github@bromberger.com>
2020-03-04 19:11:44 -0800

    removed experimental shortest paths

---
+++ test/Parallel/shortestpaths/dijkstra.jl
@@ -0,0 +1,95 @@
+@testset "Parallel.Dijkstra" begin
+    g4 = path_digraph(5)
+    d1 = float([0 1 2 3 4; 5 0 6 7 8; 9 10 0 11 12; 13 14 15 0 16; 17 18 19 20 0])
+    d2 = sparse(float([0 1 2 3 4; 5 0 6 7 8; 9 10 0 11 12; 13 14 15 0 16; 17 18 19 20 0]))
+    #Testing multisource On undirected Graph
+    g3 = path_graph(5)
+    d = [0 1 2 3 4; 1 0 1 0 1; 2 1 0 11 12; 3 0 11 0 5; 4 1 19 5 0]
+
+    for g in testgraphs(g3)
+        z  = shortest_paths(g, d, FloydWarshall())
+        zp = @inferred(parallel_shortest_paths(g, collect(1:5), d, Dijkstra()))
+        @test all(isapprox(dists(z), dists(zp)))
+
+        for i in 1:5
+            state = shortest_paths(g, i, Dijkstra(all_paths=true));
+            for j in 1:5
+                if parents(zp)[i, j] != 0
+                    @test parents(zp)[i, j] in state.predecessors[j]
+                end
+            end
+        end
+
+        z  = shortest_paths(g, FloydWarshall())
+        zp = @inferred(parallel_shortest_paths(g, Dijkstra()))
+        @test all(isapprox(dists(z), dists(zp)))
+
+        for i in 1:5
+            state = shortest_paths(g, i, Dijkstra(all_paths=true))
+            for j in 1:5
+                if parents(zp)[i, j] != 0
+                    @test parents(zp)[i, j] in state.predecessors[j]
+                end
+            end
+        end
+
+        z  = shortest_paths(g, FloydWarshall())
+        zp = @inferred(parallel_shortest_paths(g, [1, 2], Dijkstra()))
+        @test all(isapprox(dists(z)[1:2, :], dists(zp)))
+
+        for i in 1:2
+            state = shortest_paths(g, i, Dijkstra(all_paths=true))
+            for j in 1:5
+                if parents(zp)[i, j] != 0
+                    @test parents(zp)[i, j] in state.predecessors[j]
+                end
+            end
+        end
+    end
+
+
+    #Testing multisource On directed Graph
+    g3 = path_digraph(5)
+    d = float([0 1 2 3 4; 5 0 6 7 8; 9 10 0 11 12; 13 14 15 0 16; 17 18 19 20 0])
+
+    for g in testdigraphs(g3)
+        z  = shortest_paths(g, d, FloydWarshall())
+        zp = @inferred(parallel_shortest_paths(g, collect(1:5), d, Dijkstra()))
+        @test all(isapprox(dists(z), dists(zp)))
+
+        for i in 1:5
+            state = shortest_paths(g, i, Dijkstra(all_paths=true))
+            for j in 1:5
+                if parents(z)[i, j] != 0
+                    @test parents(zp)[i, j] in state.predecessors[j]
+                end
+            end
+        end
+
+        z  = shortest_paths(g, FloydWarshall())
+        zp = @inferred(parallel_shortest_paths(g, Dijkstra()))
+        @test all(isapprox(dists(z), dists(zp)))
+
+        for i in 1:5
+            state = shortest_paths(g, i, Dijkstra(all_paths=true))
+            for j in 1:5
+                if parents(zp)[i, j] != 0
+                    @test parents(zp)[i, j] in state.predecessors[j]
+                end
+            end
+        end
+
+        z  = shortest_paths(g, FloydWarshall())
+        zp = @inferred(parallel_shortest_paths(g, [1, 2], Dijkstra()))
+        @test all(isapprox(dists(z)[1:2, :], dists(zp)))
+
+        for i in 1:2
+            state = shortest_paths(g, i, Dijkstra(all_paths=true))
+            for j in 1:5
+                if parents(zp)[i, j] != 0
+                    @test parents(zp)[i, j] in state.predecessors[j]
+                end
+            end
+        end
+    end
+end
---
+++ test/Parallel/shortestpaths/floyd-warshall.jl
@@ -0,0 +1,27 @@
+@testset "Parallel.Floyd Warshall" begin
+    g3 = path_graph(5)
+    d = [0 1 2 3 4; 5 0 6 7 8; 9 10 0 11 12; 13 14 15 0 16; 17 18 19 20 0]
+    for g in testgraphs(g3)
+        z = @inferred(parallel_shortest_paths(g, d, FloydWarshall()))
+        @test z.dists[3, :][:] == [7, 6, 0, 11, 27]
+        @test z.parents[3, :][:] == [2, 3, 0, 3, 4]
+
+        @test @inferred(paths(z))[2][2] == []
+        @test @inferred(paths(z))[2][4] == paths(z, 2)[4] == paths(z, 2, 4) == [2, 3, 4]
+    end
+    g4 = path_digraph(4)
+    d = ones(4, 4)
+    for g in testdigraphs(g4)
+        z = @inferred(parallel_shortest_paths(g, d, FloydWarshall()))
+        @test length(paths(z, 4, 3)) == 0
+        @test length(paths(z, 4, 1)) == 0
+        @test length(paths(z, 2, 3)) == 2
+    end 
+
+    g5 = DiGraph([1 1 1 0 1; 0 1 0 1 1; 0 1 1 0 0; 1 0 1 1 0; 0 0 0 1 1])
+    d = [0 3 8 0 -4; 0 0 0 1 7; 0 4 0 0 0; 2 0 -5 0 0; 0 0 0 6 0]
+    for g in testdigraphs(g5)
+        z = @inferred(parallel_shortest_paths(g, d, FloydWarshall()))
+        @test z.dists == [0 1 -3 2 -4; 3 0 -4 1 -1; 7 4 0 5 3; 2 -1 -5 0 -2; 8 5 1 6 0]
+    end 
+end
