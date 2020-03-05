143f1d107ced646a0c855d07edff551b40d43759
Seth Bromberger <github@bromberger.com>
2020-03-04 19:06:14 -0800

    shortest paths / parallel shortest paths

--- src/Parallel/shortestpaths/dijkstra.jl
+++ src/Parallel/shortestpaths/dijkstra.jl
@@ -1,38 +1,48 @@
 """
-    struct Parallel.MultipleDijkstraState{T, U}
+    struct ParallelDijkstra <: ShortestPathAlgorithm end
 
-An [`AbstractPathState`](@ref) designed for Parallel.dijkstra_shortest_paths calculation.
+A struct representing a parallel implementation of the Dijkstra shortest-paths algorithm.
 """
-struct MultipleDijkstraState{T <: Real,U <: Integer} <: AbstractPathState
+struct ParallelDijkstra <: ShortestPathAlgorithm end
+
+"""
+    struct ParallelDijkstraResult{T, U}
+
+A [`ShortestPathResult`](@ref) designed for parallel Dijkstra shortest paths calculation.
+"""
+struct ParallelDijkstraResult{T <: Real,U <: Integer} <: ShortestPathResult
     dists::Matrix{T}
     parents::Matrix{U}
+    pathcounts::Matrix{U}
 end
 
-"""
-    Parallel.dijkstra_shortest_paths(g, sources=vertices(g), distmx=weights(g))
-
-Compute the shortest paths between all pairs of vertices in graph `g` by running
-[`dijkstra_shortest_paths`] for every vertex and using an optional list of source vertex `sources` and
-an optional distance matrix `distmx`. Return a [`Parallel.MultipleDijkstraState`](@ref) with relevant
-traversal information.
-"""
-function dijkstra_shortest_paths(g::AbstractGraph{U},
-    sources::AbstractVector=vertices(g),
-    distmx::AbstractMatrix{T}=weights(g)) where T <: Real where U
+# """
+#     Parallel.dijkstra_shortest_paths(g, sources=vertices(g), distmx=weights(g))
+#
+# Compute the shortest paths between all pairs of vertices in graph `g` by running
+# [`dijkstra_shortest_paths`] for every vertex and using an optional list of source vertex `sources` and
+# an optional distance matrix `distmx`. Return a [`Parallel.MultipleDijkstraState`](@ref) with relevant
+# traversal information.
+# """
+function shortest_paths(g::AbstractGraph{U},
+    sources::AbstractVector, distmx::AbstractMatrix{T}, ::ParallelDijkstra) where {T<:Real, U}
 
     n_v = nv(g)
     r_v = length(sources)
 
     # TODO: remove `Int` once julialang/#23029 / #23032 are resolved
-    dists   = SharedMatrix{T}(Int(r_v), Int(n_v))
-    parents = SharedMatrix{U}(Int(r_v), Int(n_v))
+    dists      = SharedMatrix{T}(Int(r_v), Int(n_v))
+    parents    = SharedMatrix{U}(Int(r_v), Int(n_v))
+    pathcounts = SharedMatrix{U}(Int(r_v), Int(n_v))
 
     @sync @distributed for i in 1:r_v
-        state = LightGraphs.dijkstra_shortest_paths(g, sources[i], distmx)
+        state = shortest_paths(g, sources[i], distmx, Dijkstra())
         dists[i, :] = state.dists
         parents[i, :] = state.parents
+        pathcounts[i, :] = state.pathcounts
     end
 
-    result = MultipleDijkstraState(sdata(dists), sdata(parents))
-    return result
+    return ParallelDijkstraResult(sdata(dists), sdata(parents), sdata(pathcounts))
 end
+
+shortest_paths(g::AbstractGraph, s::Integer, distmx::AbstractMatrix, alg::ParallelDijkstra) = shortest_paths(g, [s], distmx, ds)
---
+++ src/ShortestPaths/bellman-ford.jl
@@ -0,0 +1,75 @@
+# Parts of this code were taken / derived from Graphs.jl. See LICENSE for
+# licensing details.
+
+# The Bellman Ford algorithm for single-source shortest path
+
+
+"""
+    struct BellmanFord <: ShortestPathAlgorithm
+
+The structure used to configure and specify that [`shortest_paths`](@ref)
+should use the [Bellman-Ford algorithm](http://en.wikipedia.org/wiki/Bellman–Ford_algorithm).
+No fields are specified or required.
+
+### Implementation Notes
+`BellmanFord` supports the following shortest-path functionality:
+- negative distance matrices / weights
+- (optional) multiple sources
+- all destinations
+"""
+struct BellmanFord <: ShortestPathAlgorithm end
+struct BellmanFordResult{T, U<:Integer} <: ShortestPathResult
+    parents::Vector{U}
+    dists::Vector{T}
+end
+
+function shortest_paths(
+    graph::AbstractGraph{U},
+    sources::AbstractVector{<:Integer},
+    distmx::AbstractMatrix{T},
+    ::BellmanFord
+   ) where {T, U<:Integer}
+
+    nvg = nv(graph)
+    active = falses(nvg)
+    active[sources] .= true
+    dists = fill(typemax(T), nvg)
+    parents = zeros(U, nvg)
+    dists[sources] .= 0
+    no_changes = false
+    new_active = falses(nvg)
+
+    @inbounds for i in vertices(graph)
+        no_changes = true
+        new_active .= false
+        for u in vertices(graph)[active]
+            for v in outneighbors(graph, u)
+                relax_dist = distmx[u, v] + dists[u]
+                if dists[v] > relax_dist
+                    dists[v] = relax_dist
+                    parents[v] = u
+                    no_changes = false
+                    new_active[v] = true
+                end
+            end
+        end
+        no_changes && break
+        active, new_active = new_active, active
+    end
+    no_changes || throw(NegativeCycleError())
+    return BellmanFordResult(parents, dists)
+end
+
+shortest_paths(g::AbstractGraph, v::Integer, distmx::AbstractMatrix, alg::BellmanFord) =
+    shortest_paths(g, [v], distmx, alg)
+
+has_negative_weight_cycle(g::AbstractGraph, ::BellmanFord) = false
+
+function has_negative_weight_cycle(g::AbstractGraph, distmx::AbstractMatrix, alg::BellmanFord)
+    try
+        shortest_paths(g, vertices(g), distmx, alg)
+    catch e
+        isa(e, ShortestPaths.NegativeCycleError) && return true
+    end
+    return false
+end
