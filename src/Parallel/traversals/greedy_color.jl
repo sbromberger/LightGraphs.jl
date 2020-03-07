1b2dff3308d3882c8543c16b1172f62764ce7fc5
Seth Bromberger <github@bromberger.com>
2020-03-07 08:20:16 -0800

    deprecated rest of Parallel/traversals

--- src/Parallel/traversals/greedy_color.jl
+++ src/Parallel/traversals/greedy_color.jl
@@ -1,11 +1,7 @@
 # TODO 2.0.0: Remove this file
 function random_greedy_color(g::AbstractGraph{T}, reps::Integer) where {T <: Integer}
-    best = @distributed (LightGraphs.Traversals.best_color) for i in 1:reps
-        seq = shuffle(vertices(g))
-        LightGraphs.perm_greedy_color(g, seq)
-    end
-
-    return convert(LightGraphs.Traversals.Coloring{T}, best)
+    Base.depwarn("`Parallel.random_greedy_color` is deprecated. Equivalent functionality has been moved to `LightGraphs.Traversals.greedy_color`.", :random_greedy_color)
+    LightGraphs.Traversals.greedy_color(g, LightGraphs.Traversals.ThreadedRandomColoring(niter=reps))
 end
 
 greedy_color(g::AbstractGraph{U}; sort_degree::Bool=false, reps::Integer=1) where {U <: Integer} =
