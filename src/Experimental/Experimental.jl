d7ec05bfe5d5e434b7da5514b02b2044f73a1cf7
ali <alimamdouh1809@gmail.com>
2020-03-15 19:26:39 +0200

    fix monor error

--- src/Experimental/Experimental.jl
+++ src/Experimental/Experimental.jl
@@ -8,7 +8,7 @@ export description,
     VF2, vf2, IsomorphismProblem, SubgraphIsomorphismProblem, InducedSubgraphIsomorphismProblem,
     could_have_isomorph, has_isomorph, all_isomorph, count_isomorph,
     has_induced_subgraphisomorph, count_induced_subgraphisomorph, all_induced_subgraphisomorph,
-    has_subgraphisomorph, count_subgraphisomorph, all_subgraphisomorph, dominator_tree
+    has_subgraphisomorph, count_subgraphisomorph, all_subgraphisomorph, dominator_tree,
 
     ShortestPaths
 description() = "This module contains experimental graph functions."
