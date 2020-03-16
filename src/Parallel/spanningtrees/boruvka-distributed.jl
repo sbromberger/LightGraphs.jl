4c63f02e21a1fd1c3c7346a23e6ea10f51d85883
hanstananda <hanstananda@gmail.com>
2020-03-17 02:39:40 +1300

    resolve comments part #1

--- src/Parallel/spanningtrees/boruvka-distributed.jl
+++ src/Parallel/spanningtrees/boruvka-distributed.jl
@@ -5,70 +5,71 @@ boruvka_multithread_lightgraph:
 - Date: 2020-03-15
 =#
 @everywhere using LightGraphs, SimpleWeightedGraphs
-using CPUTime
 using DelimitedFiles
 @everywhere using Base
 @everywhere using DataStructures
 using SharedArrays
+using Distributed
 
 """
     boruvka_mst_distributed(g)
 
-Return a tuple `(mst, weights)` where `mst` is a vector of edges representing the
+Return a tuple `(mst, weight)` where `mst` is a vector of edges representing the
 optimum (minimum, by default) spanning tree of a connected, undirected graph
-`g` , and `weights` is the sum of all the edges in the solution by using
+`g` , and `weight` is the sum of all the edges in the solution by using
 [Boruvka's algorithm](https://en.wikipedia.org/wiki/Bor%C5%AFvka%27s_algorithm).
 The algorithm requires that all edges have different weights to correctly generate a minimun/maximum spanning tree
 ### Optional parameter(s):
 `maxItr`: Used to limit maximum number of iterations the algorithm will be performed. The default is log2(numVertex).
 """
-function boruvka_mst_distributed(g::SimpleWeightedGraph, maxItr = convert(Int64, round(log2(nv(g))+1, digits=0)))
-    connected_vs = IntDisjointSets(nv(g))
-    joined_nodes = Dict{Int, Vector{Int}}(i=>[i] for i in 1:nv(g))
-    MAX_WEIGHT=2000000000.0
-    minCost = SharedArray{Float64}(nv(g)+1)
-    minNodeTgt = SharedArray{Int}(nv(g)+1)
-    minNodeSrc = SharedArray{Int}(nv(g)+1)
+function boruvka_mst_distributed(g::SimpleWeightedGraph, max_iter = round(Int64, log2(nv(g)) + 1))
+    nvg = nv(g)
+    connected_vs = IntDisjointSets(nvg)
+    joined_nodes = Dict{Int, Vector{Int}}(i=>[i] for i in 1:nvg)
+    MAX_WEIGHT = Inf
+    cheapest = SharedVector{Float64}(nvg+1)
+    cheapest_target_node = SharedVector{Int}(nvg+1)
+    cheapest_source_node = SharedVector{Int}(nvg+1)
     mst = Vector{edgetype(g)}()
-    sizehint!(mst, nv(g) - 1)
-    res =0
-    i=1
-    println("Max iteration: ", maxItr)
-    while(i<maxItr && length(mst)< nv(g)-1)
-        i+=1
-        initMinCostArray(g, minNodeSrc, minNodeTgt, minCost, MAX_WEIGHT)
-        findMinCostVertex(g, minNodeSrc, minNodeTgt, minCost, joined_nodes, connected_vs)
-        res+= contractVertex(g, minNodeSrc, minNodeTgt, minCost, joined_nodes, connected_vs,
+    sizehint!(mst, nvg - 1)
+    weight = zero(1)
+    current_iteration = 1
+    println("Max iteration: ", max_iter)
+    while(current_iteration< max_iter && length(mst) < nvg - 1)
+        current_iteration += 1
+        initcheapestarray(g, cheapest_source_node, cheapest_target_node, cheapest, MAX_WEIGHT)
+        findcheapestvertex(g, cheapest_source_node, cheapest_target_node, cheapest, joined_nodes, connected_vs)
+        weight += contractvertex(g, cheapest_source_node, cheapest_target_node, cheapest, joined_nodes, connected_vs,
                 mst, MAX_WEIGHT)
     end
 
-    return (mst=mst, weight=res)
+    return (mst=mst, weight=weight)
 end
 
-function initMinCostArray(
+function initcheapestarray(
         g::SimpleWeightedGraph,
-        minNodeSrc::SharedArray{Int,1},
-        minNodeTgt::SharedArray{Int,1},
-        minCost::SharedArray{Float64,1},
+        cheapest_source_node::SharedVector{Int},
+        cheapest_target_node::SharedVector{Int},
+        cheapest::SharedVector{Float64},
         MAX_WEIGHT::Float64
     )
-    for i = 1:nv(g)
-        minCost[i] = MAX_WEIGHT
-        minNodeTgt[i] = i
-        minNodeSrc[i] = i
+    for i in vertices(g)
+        cheapest[i] = MAX_WEIGHT
+        cheapest_target_node[i] = i
+        cheapest_source_node[i] = i
     end
 end
 
-function findMinCostVertex(
+function findcheapestvertex(
         g::SimpleWeightedGraph,
-        minNodeSrc::SharedArray{Int},
-        minNodeTgt::SharedArray{Int},
-        minCost::SharedArray{Float64},
+        cheapest_source_node::SharedVector{Int},
+        cheapest_target_node::SharedVector{Int},
+        cheapest::SharedVector{Float64},
         joined_nodes::Dict{Int, Vector{Int}},
         connected_vs::IntDisjointSets,
     )
-    sets = Vector{Int}(first.(keys(joined_nodes)))
-    @sync @distributed for i in sets
+    source_vertices = Vector{Int}(first.(keys(joined_nodes)))
+    @sync @distributed for i in source_vertices
         # println("Accessing set ",i, " with sources ", joined_nodes[i])
         for src in joined_nodes[i]
             for dst in neighbors(g, src)
@@ -77,10 +78,10 @@ function findMinCostVertex(
                 root_src = find_root(connected_vs, src)
                 root_dst = find_root(connected_vs, dst)
                 if root_src != root_dst
-                    if(minCost[root_src] > weight )
-                        minCost[root_src] = weight
-                        minNodeTgt[root_src] = dst
-                        minNodeSrc[root_src] = src
+                    if(cheapest[root_src] > weight )
+                        cheapest[root_src] = weight
+                        cheapest_target_node[root_src] = dst
+                        cheapest_source_node[root_src] = src
                     end
                 end
             end
@@ -89,34 +90,34 @@ function findMinCostVertex(
     end
 end
 
-function contractVertex(
+function contractvertex(
         g::SimpleWeightedGraph,
-        minNodeSrc::SharedArray{Int},
-        minNodeTgt::SharedArray{Int},
-        minCost::SharedArray{Float64},
+        cheapest_source_node::SharedVector{Int},
+        cheapest_target_node::SharedVector{Int},
+        cheapest::SharedVector{Float64},
         joined_nodes::Dict{Int, Vector{Int}},
         connected_vs::IntDisjointSets,
         mst::Vector,
         MAX_WEIGHT::Float64
-    )::Int
-    res = 0
-    for i in 1:nv(g)
-        if(minCost[i]!= MAX_WEIGHT && !in_same_set(connected_vs, minNodeSrc[i], minNodeTgt[i]))
+    )::Float64
+    res = zero(1)
+    for i in vertices(g)
+        if(abs( cheapest[i]- MAX_WEIGHT)> eps() && !in_same_set(connected_vs, cheapest_source_node[i], cheapest_target_node[i]))
             # Connect the vertices, add mst to answer
-            set1 = find_root(connected_vs, minNodeSrc[i])
-            set2 = find_root(connected_vs, minNodeTgt[i])
-            union!(connected_vs, minNodeSrc[i], minNodeTgt[i])
-            res+=minCost[i]
-            push!(mst, SimpleWeightedEdge(minNodeSrc[i], minNodeTgt[i], minCost[i]))
+            set1 = find_root(connected_vs, cheapest_source_node[i])
+            set2 = find_root(connected_vs, cheapest_target_node[i])
+            union!(connected_vs, cheapest_source_node[i], cheapest_target_node[i])
+            res += cheapest[i]
+            push!(mst, SimpleWeightedEdge(cheapest_source_node[i], cheapest_target_node[i], cheapest[i]))
             # Merge Vertices that has been connected together
-            merge_target = find_root(connected_vs, minNodeSrc[i])
-            if merge_target!=set1
+            merge_target = find_root(connected_vs, cheapest_source_node[i])
+            if merge_target != set1
                 for j in joined_nodes[set1]
                     push!(joined_nodes[merge_target],j)
                 end
                 delete!(joined_nodes,set1)
             end
-            if merge_target!=set2
+            if merge_target != set2
                 for j in joined_nodes[set2]
                     push!(joined_nodes[merge_target],j)
                 end
--- src/Parallel/spanningtrees/boruvka-multithread.jl
+++ src/Parallel/spanningtrees/boruvka-multithread.jl
@@ -8,61 +8,62 @@ using Base.Threads
 """
     boruvka_mst_multithread(g)
 
-Return a tuple `(mst, weights)` where `mst` is a vector of edges representing the
+Return a tuple `(mst, weight)` where `mst` is a vector of edges representing the
 optimum (minimum, by default) spanning tree of a connected, undirected graph
-`g` , and `weights` is the sum of all the edges in the solution by using
+`g` , and `weight` is the sum of all the edges in the solution by using
 [Boruvka's algorithm](https://en.wikipedia.org/wiki/Bor%C5%AFvka%27s_algorithm).
 The algorithm requires that all edges have different weights to correctly generate a minimun/maximum spanning tree
 ### Optional parameter(s):
 `maxItr`: Used to limit maximum number of iterations the algorithm will be performed. The default is log2(numVertex).
 """
-function boruvka_mst_multithread(g::SimpleWeightedGraph, maxItr = convert(Int64, round(log2(nv(g))+1, digits=0)))
-    connected_vs = IntDisjointSets(nv(g))
-    joined_nodes = Dict{Int, Vector{Int}}(i=>[i] for i in 1:nv(g))
-    MAX_WEIGHT=2000000000.0
-    minCost = fill(MAX_WEIGHT, nv(g)+1)
-    minNodeTgt = Vector{Int}(1:nv(g)+1)
-    minNodeSrc = Vector{Int}(1:nv(g)+1)
+function boruvka_mst_multithread(g::SimpleWeightedGraph, max_iter = round(Int64, log2(nv(g)) + 1))
+    nvg = nv(g)
+    connected_vs = IntDisjointSets(nvg)
+    joined_nodes = Dict{Int, Vector{Int}}(i=>[i] for i in 1:nvg)
+    MAX_WEIGHT = Inf
+    cheapest = fill(MAX_WEIGHT, nvg+1)
+    cheapest_target_node = Vector{Int}(1:nvg+1)
+    cheapest_source_node = Vector{Int}(1:nvg+1)
     mst = Vector{edgetype(g)}()
-    sizehint!(mst, nv(g) - 1)
-    res =0
-    i=1
-    println("Max iteration: ", maxItr)
-    while(i<maxItr && length(mst)< nv(g)-1)
-        i+=1
-        initMinCostArray(g, minNodeSrc, minNodeTgt, minCost, MAX_WEIGHT)
-        findMinCostVertex(g, minNodeSrc, minNodeTgt, minCost, joined_nodes, connected_vs)
-        res+= contractVertex(g, minNodeSrc, minNodeTgt, minCost, joined_nodes, connected_vs,
+    sizehint!(mst, nvg - 1)
+    weight = zero(1)
+    current_iteration = 1
+    println("Max iteration: ", max_iter)
+    while(current_iteration< max_iter && length(mst) < nvg - 1)
+        current_iteration += 1
+        initcheapestarray(g, cheapest_source_node, cheapest_target_node, cheapest, MAX_WEIGHT)
+        findcheapestvertex(g, cheapest_source_node, cheapest_target_node, cheapest, joined_nodes, connected_vs)
+        weight += contractvertex(g, cheapest_source_node, cheapest_target_node, cheapest, joined_nodes, connected_vs,
                 mst, MAX_WEIGHT)
     end
 
-    return (mst=mst, weight=res)
+    return (mst=mst, weight=weight)
 end
 
-function initMinCostArray(
+function initcheapestarray(
         g::SimpleWeightedGraph,
-        minNodeSrc::Vector{Int},
-        minNodeTgt::Vector{Int},
-        minCost::Vector{Float64},
+        cheapest_source_node::Vector{Int},
+        cheapest_target_node::Vector{Int},
+        cheapest::Vector{Float64},
         MAX_WEIGHT::Float64
     )
-    for i = 1:nv(g)
-        minCost[i] = MAX_WEIGHT
-        minNodeTgt[i] = i
-        minNodeSrc[i] = i
+    for i in vertices(g)
+        cheapest[i] = MAX_WEIGHT
+        cheapest_target_node[i] = i
+        cheapest_source_node[i] = i
     end
 end
 
-function findMinCostVertex(
+function findcheapestvertex(
         g::SimpleWeightedGraph,
-        minNodeSrc::Vector{Int},
-        minNodeTgt::Vector{Int},
-        minCost::Vector{Float64},
+        cheapest_source_node::Vector{Int},
+        cheapest_target_node::Vector{Int},
+        cheapest::Vector{Float64},
         joined_nodes::Dict{Int, Vector{Int}},
         connected_vs::IntDisjointSets,
     )
-    sets = Vector{Int}(first.(keys(joined_nodes)))
-    @threads for i in sets
+    source_vertices = Vector{Int}(first.(keys(joined_nodes)))
+    @threads for i in source_vertices
         # println("Accessing set ",i, " with sources ", joined_nodes[i])
         for src in joined_nodes[i]
             for dst in neighbors(g, src)
@@ -71,10 +72,10 @@ function findMinCostVertex(
                 root_src = find_root(connected_vs, src)
                 root_dst = find_root(connected_vs, dst)
                 if root_src != root_dst
-                    if(minCost[root_src] > weight )
-                        minCost[root_src] = weight
-                        minNodeTgt[root_src] = dst
-                        minNodeSrc[root_src] = src
+                    if(cheapest[root_src] > weight )
+                        cheapest[root_src] = weight
+                        cheapest_target_node[root_src] = dst
+                        cheapest_source_node[root_src] = src
                     end
                 end
             end
@@ -83,27 +84,27 @@ function findMinCostVertex(
     end
 end
 
-function contractVertex(
+function contractvertex(
         g::SimpleWeightedGraph,
-        minNodeSrc::Vector{Int},
-        minNodeTgt::Vector{Int},
-        minCost::Vector{Float64},
+        cheapest_source_node::Vector{Int},
+        cheapest_target_node::Vector{Int},
+        cheapest::Vector{Float64},
         joined_nodes::Dict{Int, Vector{Int}},
         connected_vs::IntDisjointSets,
         mst::Vector,
         MAX_WEIGHT::Float64
-    )::Int
+    )::Float64
     res = 0
-    for i in 1:nv(g)
-        if(minCost[i]!= MAX_WEIGHT && !in_same_set(connected_vs, minNodeSrc[i], minNodeTgt[i]))
+    for i in vertices(g)
+        if(cheapest[i]!= MAX_WEIGHT && !in_same_set(connected_vs, cheapest_source_node[i], cheapest_target_node[i]))
             # Connect the vertices, add mst to answer
-            set1 = find_root(connected_vs, minNodeSrc[i])
-            set2 = find_root(connected_vs, minNodeTgt[i])
-            union!(connected_vs, minNodeSrc[i], minNodeTgt[i])
-            res+=minCost[i]
-            push!(mst, SimpleWeightedEdge(minNodeSrc[i], minNodeTgt[i], minCost[i]))
+            set1 = find_root(connected_vs, cheapest_source_node[i])
+            set2 = find_root(connected_vs, cheapest_target_node[i])
+            union!(connected_vs, cheapest_source_node[i], cheapest_target_node[i])
+            res+=cheapest[i]
+            push!(mst, SimpleWeightedEdge(cheapest_source_node[i], cheapest_target_node[i], cheapest[i]))
             # Merge Vertices that has been connected together
-            merge_target = find_root(connected_vs, minNodeSrc[i])
+            merge_target = find_root(connected_vs, cheapest_source_node[i])
             if merge_target!=set1
                 for j in joined_nodes[set1]
                     push!(joined_nodes[merge_target],j)
