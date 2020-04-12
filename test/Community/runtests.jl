ac8348faccbe6527da9376616a57db9d7d1aad31
Seth Bromberger <github@bromberger.com>
2020-04-12 14:15:19 -0700

    Community with 2.0-like dispatch

---
+++ test/Community/runtests.jl
@@ -0,0 +1,21 @@
+using LightGraphs.Community
+
+const LCOM = LightGraphs.Community
+
+const communitytestdir = dirname(@__FILE__)
+
+communitytests = [
+	"cliques.jl",
+	"clustering.jl",
+	"communities.jl",
+	"core-periphery.jl",
+	"modularity.jl",
+]
+
+@testset "LightGraphs.Community" begin
+    for t in communitytests
+        tp = joinpath(communitytestdir, "$t")
+        include(tp)
+    end
+end
+
