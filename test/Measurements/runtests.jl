6048260aafff2a47b7399041e695e97c8f98cb78
Seth Bromberger <github@bromberger.com>
2020-04-14 14:20:33 -0700

    test coverage

--- test/Measurements/runtests.jl
+++ test/Measurements/runtests.jl
@@ -1,3 +1,5 @@
+using LightGraphs.ShortestPaths
+
 const LMS = LightGraphs.Measurements
 @testset "Measurements" begin
     g4 = path_digraph(5)
@@ -47,15 +49,26 @@ const LMS = LightGraphs.Measurements
         @testset "LMS.eccentricity" begin
             y = @inferred(LMS.eccentricity(g, vertices(g), distmx2))
             x = @inferred(LMS.eccentricity(g, 1, distmx2))
+            w = @inferred(LMS.eccentricity(g, vertices(g), distmx2, ShortestPaths.Dijkstra()))
+            v = @inferred(LMS.eccentricity(g, distmx2, ShortestPaths.Dijkstra()))
+            u = @inferred(LMS.eccentricity(g, 1, distmx2, ShortestPaths.Dijkstra()))
             tz = @inferred(LMS.eccentricity(g, distmx2, LMS.Threaded()))
             ty = @inferred(LMS.eccentricity(g, vertices(g), distmx2, LMS.Threaded()))
-            @test z == y == tz == ty == [6.2, 4.2, 6.1]
-            @test y[1] == x
+            tw = @inferred(LMS.eccentricity(g, vertices(g), distmx2, ShortestPaths.Dijkstra(), LMS.Threaded()))
+            tv = @inferred(LMS.eccentricity(g, distmx2, ShortestPaths.Dijkstra(), LMS.Threaded()))
+            @test z == y == w == v == tz == ty == tw == tv == [6.2, 4.2, 6.1]
+            @test y[1] == x == u
             y2 = @inferred(LMS.eccentricity(g, vertices(g)))
             x2 = @inferred(LMS.eccentricity(g, 1))
+            w2 = @inferred(LMS.eccentricity(g, ShortestPaths.BFS()))
+            v2 = @inferred(LMS.eccentricity(g, vertices(g), ShortestPaths.BFS()))
+            u2 = @inferred(LMS.eccentricity(g, 1, ShortestPaths.BFS()))
+
             ty2 = @inferred(LMS.eccentricity(g, vertices(g), LMS.Threaded()))
-            @test z2 == y2 == ty2 == [2, 1, 1]
-            @test y2[1] == x2
+            tw2 = @inferred(LMS.eccentricity(g, ShortestPaths.BFS(), LMS.Threaded()))
+            tv2 = @inferred(LMS.eccentricity(g, vertices(g), ShortestPaths.BFS(), LMS.Threaded()))
+            @test z2 == y2 == w2 == v2 == ty2 == tw2 == tv2 == [2, 1, 1]
+            @test y2[1] == x2 == u2
         end
         @testset "diameter" begin
             @test @inferred(LMS.diameter(z)) == LMS.diameter(g, distmx2) == 6.2
