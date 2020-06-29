743ec560f28a22c38bebee33d0cdf2fc78f302bb
Abhinav Mehndiratta <abhinavmehndiratta@gmail.com>
2020-06-29 23:12:14 +0530

    changes and bug fixes in VertexSubsets (#1443)

--- src/Parallel/dominatingset/minimal_dom_set.jl
+++ src/Parallel/dominatingset/minimal_dom_set.jl
@@ -1,6 +1,6 @@
 function dominating_set(g::AbstractGraph{T}, reps::Integer, alg::MinimalDominatingSet; parallel=:threads, seed=-1) where T <: Integer
     Base.depwarn("`dominating_set` is deprecated. Equivalent functionality has been moved to `LightGraphs.VertexSubsets.dominating_set`.", :dominating_set)
     rng = LightGraphs.getRNG(seed)
-    alg = parallel == :threads ?  LightGraphs.VertexSubsets.ThreadedMinimalSubset(reps, rng=rng) : LightGraphs.VertexSubsets.ParallelMinimalSubset(reps, rng=rng)
+    alg = parallel == :threads ?  LightGraphs.VertexSubsets.ThreadedRandomSubset(reps, rng=rng) : LightGraphs.VertexSubsets.DistributedRandomSubset(reps, rng=rng)
     LightGraphs.VertexSubsets.dominating_set(g, alg)
 end
--- src/VertexSubsets/VertexSubsets.jl
+++ src/VertexSubsets/VertexSubsets.jl
@@ -16,8 +16,6 @@ abstract type VertexSubset end
 
 Return the vertex cover of graph `g` using [`VertexCover`](@ref) algorithm `alg` as a vector
 of vertices in the cover.
-
-An vertex is said to be covered if it has at least one adjacent edge end-point in the vertex cover.
 """
 function vertex_cover end
 
@@ -27,7 +25,7 @@ function vertex_cover end
 Using a [`VertexSubset`](@ref) algorithm, find a set of vertices that consitute a dominating set (all vertices
 in `g` are either adjacent to a vertex in the set or is a vertex in the set) and it is not possible to delete a
 vertex from the set without sacrificing the dominating property.
-#
+
 ### Implementation Notes
 A vertex is said to be dominated if it is in the dominating set or adjacent to a vertex
 in the dominating set.
@@ -41,25 +39,86 @@ Obtain an [independent set](https://en.wikipedia.org/wiki/Independent_set_(graph
 `g` using the specifed [`VertexSubset`](@ref) algorithm.
 
 ### Implementation Notes
-A vertex is said to be valid if it is not in the independent set or adjacent to any vertex
+A vertex is said to be independent if it is not in the independent set or adjacent to any vertex
 in the independent set.
 """
 function indepndent_set end
 
+"""
+    struct DegreeSubset <: VertexSubset
+
+A struct representing a degree-based greedy algorithm to calculate the
+approximate vertex cover, minimal dominating set or maximal independent set.
+"""
+struct DegreeSubset <: VertexSubset end
+
+"""
+    struct RandomSubset <: VertexSubset
+
+A struct representing an algorithm to calculate the approximate vertex cover,
+minimal dominating set or maximal independent set.
+
+### Optional Arguments
+- `rng<:AbstractRNG`: override default random number generator (`GLOBAL_RNG`).
+"""
+struct RandomSubset{R<:AbstractRNG} <: VertexSubset
+    rng::R
+end
+RandomSubset(;rng=GLOBAL_RNG) = RandomSubset(rng)
+
+"""
+    struct DistributedRandomSubset <: VertexSubset
+
+A struct representing an algorithm to calculate the approximate vertex cover,
+minimal dominating set or maximal independent set of a graph performing `reps` times
+in parallel, returning the minimum (in case of vertex cover and dominating set)
+or maximum (in case of independent set) result.
+
+### Required Fields
+`reps::Integer`: the number of times to perform the calculation.
+
+### Optional Arguments
+- `rng<:AbstractRNG`: override default random number generator (`GLOBAL_RNG`).
+"""
+struct DistributedRandomSubset{R<:AbstractRNG} <: VertexSubset
+    reps::Int
+    rng::R
+end
+DistributedRandomSubset(reps::Int; rng=GLOBAL_RNG) = DistributedRandomSubset(reps, rng)
+
+"""
+    struct ThreadedRandomSubset <: VertexSubset
+
+A struct representing an algorithm to calculate the approximate vertex cover,
+minimal dominating set or maximal independent set of a graph performing `reps` times
+in parallel using threads, returning the minimum (in case of vertex cover and dominating set)
+or maximum (in case of independent set) result.
+
+### Required Fields
+`reps::Integer`: the number of times to perform the calculation.
+
+### Optional Arguments
+- `rng<:AbstractRNG`: override default random number generator (`GLOBAL_RNG`).
+"""
+struct ThreadedRandomSubset{R<:AbstractRNG} <: VertexSubset
+    reps::Int
+    rng::R
+end
+ThreadedRandomSubset(reps::Int; rng=GLOBAL_RNG) = ThreadedRandomSubset(reps, rng)
+
 include("degree_vertex_cover.jl")
 include("random_vertex_cover.jl")
-include("parallel-random_vertex_cover.jl")
 include("threaded-random_vertex_cover.jl")
+include("distributed-random_vertex_cover.jl")
 
 include("degree_dom_set.jl")
-include("minimal_dom_set.jl")
-include("parallel-minimal_dom_set.jl")
-include("threaded-minimal_dom_set.jl")
+include("random_dom_set.jl")
+include("threaded-random_dom_set.jl")
+include("distributed-random_dom_set.jl")
 
 include("degree_ind_set.jl")
-include("maximal_ind_set.jl")
-include("parallel-maximal_ind_set.jl")
-include("threaded-maximal_ind_set.jl")
-
+include("random_ind_set.jl")
+include("threaded-random_ind_set.jl")
+include("distributed-random_ind_set.jl")
 
 end # module
