74c0c0b4ae9697eaeeacf8f2c7aa7ff46c1b2e9a
Abhinav Mehndiratta <abhinavmehndiratta@gmail.com>
2020-06-30 13:42:35 +0530

    modify tests

--- test/Community/triangle_count.jl
+++ test/Community/triangle_count.jl
@@ -1,12 +1,12 @@
 @testset "Triangle Count" begin
     g10 = SimpleGraph(SGGEN.Complete(5))
     @testset "$g" for g in testgraphs(g10)
-        @test LCOM.triangle_count(g) == 10
-        @test LCOM.triangle_count(g, LCOM.ThreadedTriangleCount()) == 10
+        @test LCOM.triangle_count(g, LCOM.DODG()) == 10
+        @test LCOM.triangle_count(g, LCOM.ThreadedDODG()) == 10
     end
     rem_edge!(g10, 1, 2)
     @testset "$g" for g in testgraphs(g10)
-        @test LCOM.triangle_count(g) == 7
-        @test LCOM.triangle_count(g, LCOM.ThreadedTriangleCount()) == 7
+        @test LCOM.triangle_count(g, LCOM.DODG()) == 7
+        @test LCOM.triangle_count(g, LCOM.ThreadedDODG()) == 7
     end
 end
