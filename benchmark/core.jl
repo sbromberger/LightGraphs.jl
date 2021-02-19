e2319325f63569ddd29b93ac67e7bc5abd7d0c79
Ryan Birmingham <birm@rbirm.us>
2021-02-19 12:14:30 -0500

    reorganise core benchmark file

--- benchmark/core.jl
+++ benchmark/core.jl
@@ -1,25 +1,5 @@
 suite["core"] = BenchmarkGroup(["nv", "edges", "has_edge"])
 
-
-# nv
-suite["core"]["nv"] = BenchmarkGroup(["graphs", "digraphs"])
-suite["core"]["nv"]["graphs"] = @benchmarkable [nv(g) for (n,g) in $GRAPHS]
-suite["core"]["nv"]["digraphs"] = @benchmarkable [nv(g) for (n,g) in $DIGRAPHS]
-
-# iterate edges
-function iter_edges(g::AbstractGraph)
-    i = 0
-    for e in edges(g)
-        i += 1
-    end
-    return i
-end
-
-suite["core"]["edges"] = BenchmarkGroup(["graphs", "digraphs"])
-suite["core"]["edges"]["graphs"] = @benchmarkable [iter_edges(g) for (n,g) in $GRAPHS]
-suite["core"]["edges"]["digraphs"] = @benchmarkable [iter_edges(g) for (n,g) in $DIGRAPHS]
-
-# has edge
 function all_has_edge(g::AbstractGraph)
     nvg = nv(g)
     srcs = rand([1:nvg;], cld(nvg, 4))
@@ -33,6 +13,25 @@ function all_has_edge(g::AbstractGraph)
     return i
 end
 
+function iter_edges(g::AbstractGraph)
+    i = 0
+    for e in edges(g)
+        i += 1
+    end
+    return i
+end
+
+# nv
+suite["core"]["nv"] = BenchmarkGroup(["graphs", "digraphs"])
+suite["core"]["nv"]["graphs"] = @benchmarkable [nv(g) for (n,g) in $GRAPHS]
+suite["core"]["nv"]["digraphs"] = @benchmarkable [nv(g) for (n,g) in $DIGRAPHS]
+
+# iter edges
+suite["core"]["edges"] = BenchmarkGroup(["graphs", "digraphs"])
+suite["core"]["edges"]["graphs"] = @benchmarkable [iter_edges(g) for (n,g) in $GRAPHS]
+suite["core"]["edges"]["digraphs"] = @benchmarkable [iter_edges(g) for (n,g) in $DIGRAPHS]
+
+# has edge
 suite["core"]["has_edge"] = BenchmarkGroup(["graphs", "digraphs"])
-suite["core"]["has_edge"]["graphs"] = @benchmark [all_has_edge(g) for (n,g) in $GRAPHS]
-suite["core"]["has_edge"]["digraphs"] = @benchmark [all_has_edge(g) for (n,g) in $DIGRAPHS]
+suite["core"]["has_edge"]["graphs"] = @benchmarkable [all_has_edge(g) for (n,g) in $GRAPHS]
+suite["core"]["has_edge"]["digraphs"] = @benchmarkable [all_has_edge(g) for (n,g) in $DIGRAPHS]
