f324304f37feeeb64bd93ca4cd35fb3fe4f9cc4a
vboussange <boutor2@gmail.com>
2021-03-11 16:20:20 +0100

    Added tests and corrected s_metric and corresponding documentation

--- src/community/s_metric.jl
+++ src/community/s_metric.jl
@@ -3,11 +3,13 @@
 
 Return the normalised s-metric of `g`.
 
-The s-metric is defined as the sum of the product of degrees between pair of nodes
+The s-metric is defined as the sum of the product of degrees between pair of vertices
 for every edge in `g`. [Ref](https://arxiv.org/abs/cond-mat/0501169)
+In directed graphs, the paired values are the out-degree of source vertices
+and the in-degree of destination vertices.
 It is normalised by the maximum s_metric obtained from the family of graph
-with similar degree distribution.
-s_max is computed from an approximation formula as in https://journals.aps.org/pre/pdf/10.1103/PhysRevE.75.046102
+with similar degree distribution. s_max is computed from an approximation
+formula as in https://journals.aps.org/pre/pdf/10.1103/PhysRevE.75.046102
 If `norm=false`, no normalisation is performed.
 
 # Examples
@@ -22,7 +24,7 @@ julia> s_metric(star_graph(4))
 function s_metric(g::AbstractGraph{T};norm=true) where T
     s = zero(T)
     for e in edges(g)
-        s += degree(g,src(e)) * degree(g,dst(e))
+        s += outdegree(g,src(e)) * indegree(g,dst(e))
     end
     if norm
         sm = sum(degree(g).^3)/2
