34ad732f522191bb7cee1461789c660010c5d714
vboussange <boutor2@gmail.com>
2021-03-30 13:48:22 +0200

    updated spaces

--- src/community/rich_club.jl
+++ src/community/rich_club.jl
@@ -1,5 +1,5 @@
 """
-    rich_club(g,k)
+    rich_club(g, k)
 
 Return the non-normalised [rich-club coefficient](https://en.wikipedia.org/wiki/Rich-club_coefficient) of graph `g`,
 with degree cut-off `k`.
@@ -7,14 +7,14 @@ with degree cut-off `k`.
 ```jldoctest
 julia> using LightGraphs
 julia> g = star_graph(5)
-julia> rich_club(g,1)
+julia> rich_club(g, 1)
 0.4
 ```
 """
-function rich_club(g::AbstractGraph{T},k::Int) where T
+function rich_club(g::AbstractGraph{T}, k::Int) where T
     E = zero(T)
     for e in edges(g)
-        if (outdegree(g,src(e)) >= k) && (indegree(g,dst(e)) >= k )
+        if (outdegree(g, src(e)) >= k) && (indegree(g, dst(e)) >= k )
             E +=1
         end
     end
