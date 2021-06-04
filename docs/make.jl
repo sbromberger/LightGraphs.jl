5d2b80ab3d33d134c88312a1b9518ec3682c44ad
Seth Bromberger <sbromberger@users.noreply.github.com>
2021-06-04 08:49:36 -0400

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -19,7 +19,7 @@ makedocs(
         "Making and Modifying Graphs"       => "generators.md",
         "Reading / Writing Graphs"          => "persistence.md",
         "Operators"                         => "operators.md",
-        "Core functions"                    => "core.md",
+        "Core Functions"                    => "core.md",
         "Plotting Graphs"                   => "plotting.md",
         "Path and Traversal"                => "pathing.md",
         "Coloring"                          => "coloring.md",
